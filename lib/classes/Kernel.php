b31050891f10418db8e1cdd6b54bdf1b00438e43
Nicolas Le Goff <legoff.n@gmail.com>
2011-12-13 12:19:19 +0100

    modif kernel

--- lib/classes/Kernel.php
+++ lib/classes/Kernel.php
@@ -10,21 +10,15 @@
  */
 
 /**
- * Access Control List class
+ * Kernel
  *
  * @license     http://opensource.org/licenses/gpl-3.0 GPLv3
  * @link        www.phraseanet.com
  */
 
-
 namespace Phrasea;
 
-
 require_once __DIR__ . '/../vendor/Silex/vendor/pimple/lib/Pimple.php';
-require_once __DIR__ . '/../vendor/Twig/lib/Twig/Autoloader.php';
-require_once __DIR__ . '/../vendor/Twig-extensions/lib/Twig/Extensions/Autoloader.php';
-require_once __DIR__ . '/../vendor/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
-
 
 class Kernel extends \Pimple
 {
@@ -33,22 +27,6 @@ class Kernel extends \Pimple
   {
     //autoload
     $this->bootstrap();
-    
-    /**
-     * Load entity manager
-     */
-    $this['EM'] = $this->share(function()
-            {
-              return static::getEntityManager();
-            });
-    /**
-     * Load registry
-     */
-    $this['registry'] = $this->share(function()
-            {
-              return new \registry(new \cache_opcode_adapter(crc32(__DIR__)));
-            });
-
 
     /**
      * load Gatekeeper
@@ -74,51 +52,36 @@ class Kernel extends \Pimple
             {
               $app['request'] = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
             });
-
+            
   }
 
-  protected function phrasea_autoload($class_name)
+  protected function phraseaAutoload($class_name)
   {
-     if (file_exists(__CUSTOMDIR__ . '/classes/'
+    if (file_exists(__DIR__ .'/../../config/classes/'
                     . str_replace('_', '/', $class_name) . '.class.php'))
     {
-      require_once __CUSTOMDIR__ . '/classes/'
+      require_once __DIR__ .'/../../config/classes/'
               . str_replace('_', '/', $class_name) . '.class.php';
     }
-    elseif (file_exists(__LIBDIR__ . '/classes/'
+    elseif (file_exists(__DIR__ . '/../classes/'
                     . str_replace('_', '/', $class_name) . '.class.php'))
     {
-      require_once __LIBDIR__ . '/classes/'
+      require_once __DIR__ . '/../classes/'
               . str_replace('_', '/', $class_name) . '.class.php';
     }
   }
 
-  
   protected function bootstrap()
   {
-    $this->define_dirs();
-    $loader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
-    spl_autoload_register(array($this, 'phrasea_autoload'));
-    \Twig_Autoloader::register();
-    \Twig_Extensions_Autoloader::register();
-    require_once __DIR__ . '/../vendor/Silex/autoload.php';
-    $loader->registerNamespaces(array(
-        'Symfony\\Component\\Yaml' => __LIBDIR__ . '/vendor/symfony/src',
-        'Symfony\\Component\\Console' => __LIBDIR__ . '/vendor/symfony/src',
-        'Phrasea' => __LIBDIR__ . '/classes',
-    ));
+    require_once __DIR__ . '/../vendor/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
     
-  }
- 
-  private function define_dirs()
-  {
-    $root = dirname(dirname(dirname(__FILE__)));
-    if (!defined('__LIBDIR__'))
-      define('__LIBDIR__', $root . '/lib');
-    if (!defined('__CUSTOMDIR__'))
-      define('__CUSTOMDIR__', $root . '/config');
+    $loader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
+    
+    spl_autoload_register(array($this, 'phraseaAutoload'));
+    
+    $loader->registerNamespaces(array(
+        'Phrasea' => __DIR__. '/../classes',
+    ));
   }
 
 }
-
-?>
