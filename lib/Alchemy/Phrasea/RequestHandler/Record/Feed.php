6d195d532b27a404a13bad37912b7de22d1d0680
Nicolas Le Goff <legoff.n@gmail.com>
2011-12-14 15:59:12 +0100

    add new record RequestHandler

---
+++ lib/Alchemy/Phrasea/RequestHandler/Record/Feed.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * This file is part of Phraseanet
+ *
+ * (c) 2005-2010 Alchemy
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Alchemy\Phrasea\RequestHandler\Record;
+
+/**
+ *
+ * @package
+ * @license     http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link        www.phraseanet.com
+ */
+
+use Alchemy\Phrasea\RequestHandler\RecordsAbstract as RecordHandler;
+use Symfony\Component\HttpFoundation\Request;
+
+class Feed extends RecordHandler
+{
+
+  /**
+   *
+   * @var Array
+   */
+  protected $required_sbas_rights = array('bas_chupub');
+  /**
+   *
+   * @var boolean
+   */
+  protected $works_on_unique_sbas = true;
+  
+  /**
+   *
+   * @var boolean
+   */
+  protected $flatten_groupings = true;
+
+  /**
+   *
+   * @param Request $request
+   * @return RecordsFeed 
+   */
+  public function __construct(Request $request)
+  {
+    $appbox = \appbox::get_instance();
+
+    parent::__construct($request);
+
+    if ($this->is_single_grouping())
+    {
+      $record = array_pop($this->selection->get_elements());
+      foreach ($record->get_children() as $child)
+      {
+        $this->selection->add_element($child);
+      }
+    }
+
+    return $this;
+  }
+
+}
