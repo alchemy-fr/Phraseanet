3340c8fdfd8ab21097ab6622af9ae000c25e5d86
Romain Neutron <imprec@gmail.com>
2011-12-15 11:57:46 +0100

    Start implementing entities

--- lib/classes/basketCollection.class.php
+++ lib/classes/basketCollection.class.php
@@ -41,36 +41,48 @@ class basketCollection
     $baskets = array();
     $baskets['baskets'] = $baskets['recept'] = $baskets['regroup'] = array();
 
-    $sql = 'SELECT s.ssel_id, s.usr_id as owner, v.id as validate_id,
-              s.temporaryType, s.pushFrom, v.expires_on FROM ssel s
-            LEFT JOIN validate v
-              ON (v.ssel_id = s.ssel_id AND v.usr_id = :usr_id_v)
-            WHERE (s.usr_id = :usr_id_o OR v.id IS NOT NULL)';
 
-    $stmt = $appbox->get_connection()->prepare($sql);
-    $stmt->execute(array(':usr_id_o' => $usr_id, ':usr_id_v' => $usr_id));
-    $rs = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    $stmt->closeCursor();
+    $kernel = bootstrap::getKernel();
+    $em = $kernel->getEntityManager();
 
-    foreach ($rs as $row)
-    {
-        $is_mine = ($row['owner'] == $usr_id);
+    $baskets['baskets'] = $em->getRepository('Entities\Basket')
+            ->findBy(array('usr_id' => $usr_id, 'pusher_id' => null));
+    
+    $baskets['regroup'] = $em->getRepository('Entities\WorkZone')
+            ->findBy(array('usr_id' => $usr_id));
 
-        $expires_on_obj = new DateTime($row['expires_on']);
-        $expires_on = $expires_on_obj->format('U');
+//    $sql = 'SELECT s.ssel_id, s.usr_id as owner, v.id as validate_id,
+//              s.temporaryType, s.pushFrom, v.expires_on FROM ssel s
+//            LEFT JOIN validate v
+//              ON (v.ssel_id = s.ssel_id AND v.usr_id = :usr_id_v)
+//            WHERE (s.usr_id = :usr_id_o OR v.id IS NOT NULL)';
+//
+//    $stmt = $appbox->get_connection()->prepare($sql);
+//    $stmt->execute(array(':usr_id_o' => $usr_id, ':usr_id_v' => $usr_id));
+//    $rs = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//    $stmt->closeCursor();
 
-        if ($row['validate_id'] != null && !$is_mine && $expires_on < $current_timestamp)
-          continue;
-
-        if ($row['temporaryType'] == '1')
-          $baskets['regroup'][] = basket_adapter::getInstance($appbox, $row['ssel_id'], $usr_id);
-        elseif (!is_null($row['validate_id']))
-          $baskets['baskets'][] = basket_adapter::getInstance($appbox, $row['ssel_id'], $usr_id);
-        elseif ((int) $row['pushFrom'] > 0)
-          $baskets['recept'][] = basket_adapter::getInstance($appbox, $row['ssel_id'], $usr_id);
-        else
-          $baskets['baskets'][] = basket_adapter::getInstance($appbox, $row['ssel_id'], $usr_id);
-    }
+//    foreach ($rs as $row)
+//    {
+//        $is_mine = ($row['owner'] == $usr_id);
+//
+//        $expires_on_obj = new DateTime($row['expires_on']);
+//        $expires_on = $expires_on_obj->format('U');
+//
+//        if ($row['validate_id'] != null && !$is_mine && $expires_on < $current_timestamp)
+//          continue;
+//
+//        if ($row['temporaryType'] == '1')
+//          $baskets['regroup'][] = basket_adapter::getInstance($appbox, $row['ssel_id'], $usr_id);
+//        elseif (!is_null($row['validate_id']))
+//        {
+////          $baskets['baskets'][] = basket_adapter::getInstance($appbox, $row['ssel_id'], $usr_id);
+//        }
+//        elseif ((int) $row['pushFrom'] > 0)
+//          $baskets['recept'][] = basket_adapter::getInstance($appbox, $row['ssel_id'], $usr_id);
+////        else
+////          $baskets['baskets'][] = basket_adapter::getInstance($appbox, $row['ssel_id'], $usr_id);
+//    }
 
     $to_remove = array_intersect(array('recept', 'regroup', 'baskets'), $except);
 
@@ -89,6 +101,7 @@ class basketCollection
       uasort($baskets['regroup'], array('basketCollection', 'story_date_sort'));
       uasort($baskets['recept'], array('basketCollection', 'story_date_sort'));
     }
+
 
     $this->baskets = $baskets;
 
@@ -119,13 +132,11 @@ class basketCollection
   function story_date_sort($a, $b)
   {
     if (!$a->create || !$b->create)
-
       return 0;
 
     $comp = strcasecmp($a->create, $b->create);
 
     if ($comp == 0)
-
       return 0;
 
     return $comp < 0 ? -1 : 1;
@@ -133,14 +144,13 @@ class basketCollection
 
   function story_name_sort($a, $b)
   {
-    if (!$a->get_name() || !$b->get_name())
+    if (!$a->getName() || !$b->getName())
     {
       return 0;
     }
-    $comp = strcasecmp($a->get_name(), $b->get_name());
+    $comp = strcasecmp($a->getName(), $b->getName());
 
     if ($comp == 0)
-
       return 0;
 
     return $comp < 0 ? -1 : 1;
@@ -158,7 +168,7 @@ class basketCollection
     $stmt->closeCursor();
 
     $baskets = array();
-    foreach($rs as $row)
+    foreach ($rs as $row)
     {
       $baskets[] = basket_adapter::getInstance($appbox, $row['ssel_id'], $session->get_usr_id());
     }
