e2ab10b47e48386ff4cd3b021274f0a8277d5644
Romain Neutron <imprec@gmail.com>
2011-12-16 17:56:12 +0100

    Add new modal box

---
+++ www/prod/jquery.dialogs.js
@@ -0,0 +1,32 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$(document).ready(function(){
+  
+  $('a.dialog').live('click', function(event){
+    
+    var $this = $(this);
+    
+    $.ajax({
+      type: "GET",
+      url: $this.attr('href'),
+      dataType: 'html',
+      beforeSend:function(){
+        
+      },
+      success: function(data){
+
+        $('#DIALOG').attr('title', $this.attr('title'))
+                    .append(data).dialog("open");
+
+        return;
+      }
+    });
+    
+    return false;
+  });
+  
+});
