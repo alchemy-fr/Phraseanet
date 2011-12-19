df080113653b2a8622fb0765cab8b4c3a9e00fd3
Romain Neutron <imprec@gmail.com>
2011-12-19 17:40:32 +0100

    Basket Controller

--- lib/Alchemy/Phrasea/Controller/Prod/Basket.php
+++ lib/Alchemy/Phrasea/Controller/Prod/Basket.php
@@ -33,21 +33,200 @@ class Basket implements ControllerProviderInterface
 
   public function connect(Application $app)
   {
+    $basket_controller = $this;
+
     $controllers = new ControllerCollection();
 
     $controllers->post('/', function(Application $app)
             {
+              $request = $app['request'];
+
+              /* @var $request \Symfony\Component\HttpFoundation\Request */
+
               $em = $app['Core']->getEntityManager();
 
               $Basket = new \Entities\Basket();
-              $Basket->setName($app['request']->get('name'));
-              $Basket->setUser($app['Core']->getAuthenticatedUser());
-              $Basket->setDescription($app['request']->get('desc'));
+
+              $Basket->setName($request->get('name', ''));
+              $Basket->setOwner($app['Core']->getAuthenticatedUser());
+              $Basket->setDescription($request->get('desc'));
 
               $em->persist($Basket);
               $em->flush();
 
-              return new RedirectResponse(sprintf('/%d/', $Basket->getId()));
+              if ($request->getRequestFormat() == 'json')
+              {
+                $data = array('basket' => array('id' => $Basket->getId()));
+
+                $datas = $app['Core']['Serializer']->serialize($data, 'json');
+
+                return new Response($datas, 200, array('Content-type' => 'application/json'));
+              }
+              else
+              {
+                return new RedirectResponse(sprintf('/%d/', $Basket->getId()));
+              }
+            });
+
+    $controllers->post('/{basket_id}/delete/', function(Application $app, $basket_id) use ($basket_controller)
+            {
+              $basket = $basket_controller->getUserBasket($app['Core'], $basket_id);
+
+              $em->remove($basket);
+              $em->flush();
+
+              $data = array(
+                  'success' => true
+                  , 'message' => _('Basket has been deleted')
+              );
+
+              if ($request->getRequestFormat() == 'json')
+              {
+
+                $datas = $app['Core']['Serializer']->serialize($data, 'json');
+
+                return new Response($datas, 200, array('Content-type' => 'application/json'));
+              }
+              else
+              {
+                return new RedirectResponse('/');
+              }
+            });
+
+
+
+    $controllers->post('/{basket_id}/update/', function(Application $app, $basket_id) use ($basket_controller)
+            {
+              $basket = $basket_controller->getUserBasket($app['Core'], $basket_id);
+
+              $basket->setName($request->get('name'));
+              $basket->setDescription($request->get('description'));
+
+              $em->merge($basket);
+              $em->flush();
+
+              $data = array(
+                  'success' => true
+                  , 'message' => _('Basket has been updated')
+                  , 'basket' => array('id' => $basket->getId())
+              );
+
+              if ($request->getRequestFormat() == 'json')
+              {
+
+                $datas = $app['Core']['Serializer']->serialize($data, 'json');
+
+                return new Response($datas, 200, array('Content-type' => 'application/json'));
+              }
+              else
+              {
+                return new RedirectResponse('/');
+              }
+            });
+
+
+    $controllers->get('/{basket_id}/update/', function(Application $app, $basket_id) use ($basket_controller)
+            {
+              $basket = $basket_controller->getUserBasket($app['Core'], $basket_id);
+
+              $twig = new \supertwig();
+
+              return new Response(
+                              $twig->render(
+                                      'prod/Baskets/Update.html.twig'
+                                      , array('basket' => $basket)
+                              )
+              );
+            });
+
+
+    $controllers->post('/{basket_id}/archive/', function(Application $app, $basket_id) use ($basket_controller)
+            {
+              $basket = $basket_controller->getUserBasket($app['Core'], $basket_id);
+
+              $basket->setArchived(!!$request->get('archive'));
+
+              $em->merge($basket);
+              $em->flush();
+
+              $data = array(
+                  'success' => true
+                  , 'message' => _('Basket has been deleted')
+              );
+
+              if ($request->getRequestFormat() == 'json')
+              {
+
+                $datas = $app['Core']['Serializer']->serialize($data, 'json');
+
+                return new Response($datas, 200, array('Content-type' => 'application/json'));
+              }
+              else
+              {
+                return new RedirectResponse('/');
+              }
+            });
+
+    $controllers->post('/{basket_id}/addElements/', function(Application $app, $basket_id) use ($basket_controller)
+            {
+
+              $request = $app['request'];
+
+              $basket = $basket_controller->getUserBasket($app['Core'], $basket_id);
+
+              $user = $app['Core']->getAuthenticatedUser();
+              /* @var $user \User_Adapter */
+
+              foreach (explode(';', $request->get('lst')) as $sbas_rec)
+              {
+                $sbas_rec = explode('_', $sbas_rec);
+
+                if (count($sbas_rec) !== 2)
+                  continue;
+
+                try
+                {
+                  $record = new \record_adapter($sbas_rec[0], $sbas_rec[1]);
+
+                  if (!$user->ACL()->has_access_to_base($record->get_base_id())
+                          && !$user->ACL()->has_hd_grant($record)
+                          && !$user->ACL()->has_preview_grant($record))
+                  {
+                    continue;
+                  }
+
+                  $basket_element = new \Entities\BasketElement();
+                  $basket_element->setRecord($record);
+
+                  $em->persist($basket_element);
+
+                  $basket->addBasketElement($basket_element);
+                }
+                catch (\Exception_NotFound $e)
+                {
+                  
+                }
+              }
+
+              $em->merge($basket);
+              $em->flush();
+
+              $data = array(
+                  'success' => true
+                  , 'message' => _('Basket has been deleted')
+              );
+
+              if ($request->getRequestFormat() == 'json')
+              {
+
+                $datas = $app['Core']['Serializer']->serialize($data, 'json');
+
+                return new Response($datas, 200, array('Content-type' => 'application/json'));
+              }
+              else
+              {
+                return new RedirectResponse('/');
+              }
             });
 
     $controllers->get('/create/', function(Application $app)
@@ -57,23 +236,16 @@ class Basket implements ControllerProviderInterface
               return new Response($twig->render('prod/Baskets/Create.html.twig', array()));
             });
 
-
-    $controllers->get('/{basket_id}/', function($basket_id) use ($app)
+    $controllers->get('/{basket_id}/', function(Application $app, $basket_id) use ($basket_controller)
             {
-
-              $identifier = $app['request']->get('basket_id');
-
-              if (null === $identifier)
-              {
-                throw new \Exception_BadRequest('No basket_id');
-              }
-
               $em = $app['Core']->getEntityManager();
+      
+              $basket = $basket_controller->getUserBasket($app['Core'], $basket_id);
 
-              $repository = $em->getRepository('Entities\Baskets');
+              $basket->setIsRead(true);
 
-              /* @var $basket Entities\Basket */
-              $basket = $repository->find($identifier);
+              $em->merge($basket);
+              $em->flush();
 
               $twig = new \supertwig();
 
@@ -83,6 +255,35 @@ class Basket implements ControllerProviderInterface
             })->assert('basket_id', '\d+');
 
     return $controllers;
+  }
+
+  /**
+   *
+   * @param \Alchemy\Phrasea\Core $core
+   * @param int $basket_id
+   * @return \Entities\Basket 
+   */
+  public function getUserBasket(\Alchemy\Phrasea\Core $core, $basket_id)
+  {
+    $em = $core->getEntityManager();
+
+    /* @var $em \Doctrine\ORM\EntityManager */
+    $repository = $em->getRepository('Entities\Basket');
+
+    $basket = $repository->find($basket_id);
+
+    /* @var $basket Entities\Basket */
+    if (null === $basket)
+    {
+      throw new \Exception_NotFound(_('Basket is not found'));
+    }
+
+    if ($basket->getowner()->get_id() != $core->getAuthenticatedUser()->get_id())
+    {
+      throw new \Exception_Forbidden(_('You have not access to this basket'));
+    }
+
+    return $basket;
   }
 
 }
