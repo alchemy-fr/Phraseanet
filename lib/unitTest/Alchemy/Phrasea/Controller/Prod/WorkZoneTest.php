010724d86bdea2044f4a73d3a0c13dddc1bc41ab
Nicolas Le Goff <legoff@alchemy.fr>
2011-12-30 14:29:08 +0100

    move and refactor attach & detach story testes

--- lib/unitTest/Alchemy/Phrasea/Controller/Prod/WorkZoneTest.php
+++ lib/unitTest/Alchemy/Phrasea/Controller/Prod/WorkZoneTest.php
@@ -25,7 +25,8 @@ class WorkZoneTest extends PhraseanetWebTestCaseAuthenticatedAbstract
 
   protected $client;
   protected static $need_records = 1;
-
+  protected static $need_story = true;
+  
   public function setUp()
   {
     parent::setUp();
@@ -52,4 +53,116 @@ class WorkZoneTest extends PhraseanetWebTestCaseAuthenticatedAbstract
     $this->assertEquals(200, $response->getStatusCode());
   }
 
+  
+  public function testAttachStoryToWZ()
+  {
+    $story = self::$story_1;
+    /* @var $story \Record_Adapter */
+    $route = sprintf("/WorkZone/attachStories/");
+    try
+    {
+      $this->client->request('POST', $route);
+      $this->fail("we can attach none stories to WZ");
+    }
+    catch (\Exception $e)
+    {
+      
+    }
+
+    try
+    {
+      $this->client->request('POST', $route, array('stories'=> $story->get_serialize_key()));
+
+      $response = $this->client->getResponse();
+    }
+    catch (\Exception $e)
+    {
+      $this->fail($e->getMessage());
+    }
+
+    $this->assertEquals(302, $response->getStatusCode());
+
+    $em = self::$core->getEntityManager();
+    /* @var $em \Doctrine\ORM\EntityManager */    
+    $query = $em->createQuery(
+            'SELECT COUNT(w.id) FROM \Entities\StoryWZ w'
+    );
+
+    $count = $query->getSingleScalarResult();
+
+    $this->assertEquals(1, $count);
+    
+    $query = $em->createQuery(
+            'SELECT w FROM \Entities\StoryWZ w'
+    );
+    
+    $storyWZ = $query->getResult();
+    $em->remove(array_shift($storyWZ));
+    
+    $em->flush();
+ 
+    
+    //attach JSON
+    $this->client->request('POST', $route, array('stories'=> $story->get_serialize_key()), array(), array(
+        "HTTP_ACCEPT" => "application/json")
+    );
+    
+    $response = $this->client->getResponse();
+    
+    $this->assertEquals(200, $response->getStatusCode());
+  }
+
+  public function testDetachStoryFromWZ()
+  {
+    $story = self::$story_1;
+
+    $route = sprintf("/WorkZone/detachStory/%s/%s/", $story->get_sbas_id(), $story->get_record_id());
+    //story not yet Attched
+
+    try
+    {
+      $this->client->request('POST', $route);
+      $this->fail("Story should not be found");
+    }
+    catch (\Exception $e)
+    {
+      
+    }
+
+    //attach
+    $attachRoute = sprintf("/WorkZone/attachStories/");
+    $this->client->request('POST', $attachRoute, array('stories'=> $story->get_serialize_key()));
+
+    $query = self::$core->getEntityManager()->createQuery(
+            'SELECT COUNT(w.id) FROM \Entities\StoryWZ w'
+    );
+
+    $count = $query->getSingleScalarResult();
+
+    $this->assertEquals(1, $count);
+
+
+    //detach
+    $this->client->request('POST', $route);
+    $response = $this->client->getResponse();
+    $this->assertEquals(302, $response->getStatusCode());
+
+    $query = self::$core->getEntityManager()->createQuery(
+            'SELECT COUNT(w.id) FROM \Entities\StoryWZ w'
+    );
+
+    $count = $query->getSingleScalarResult();
+
+    $this->assertEquals(0, $count);
+
+    //attach
+    $this->client->request('POST', $attachRoute, array('stories'=> $story->get_serialize_key()));
+
+    //detach JSON
+    $this->client->request('POST', $route, array(), array(), array(
+        "HTTP_ACCEPT" => "application/json")
+    );
+    $response = $this->client->getResponse();
+    $this->assertEquals(200, $response->getStatusCode());
+  }
 }
