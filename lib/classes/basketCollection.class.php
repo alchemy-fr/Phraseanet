d4250bcae8f04a2c2e3159e3b94c14b500bf6b4e
Romain Neutron <imprec@gmail.com>
2012-01-04 15:15:38 +0100

    Fix #150 : missing notifications when receiving a validation

--- lib/classes/basketCollection.class.php
+++ lib/classes/basketCollection.class.php
@@ -151,9 +151,8 @@ class basketCollection
     $appbox = appbox::get_instance();
     $conn = $appbox->get_connection();
     $session = $appbox->get_session();
-    $sql = 'SELECT n.ssel_id FROM sselnew n, ssel s 
-            WHERE s.ssel_id = n.ssel_id AND n.usr_id = :usr_id 
-              AND n.usr_id = s.usr_id';
+    $sql = 'SELECT n.ssel_id FROM sselnew n 
+            WHERE n.usr_id = :usr_id ';
     $stmt = $conn->prepare($sql);
     $stmt->execute(array(':usr_id' => $session->get_usr_id()));
     $rs = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -162,7 +161,17 @@ class basketCollection
     $baskets = array();
     foreach($rs as $row)
     {
-      $baskets[] = basket_adapter::getInstance($appbox, $row['ssel_id'], $session->get_usr_id());
+      try
+      {
+        $baskets[] = basket_adapter::getInstance($appbox, $row['ssel_id'], $session->get_usr_id());
+      }
+      catch(Exception $e)
+      {
+        $sql = 'DELETE FROM sselnew WHERE ssel_id = :ssel_id AND usr_id = :usr_id';
+        $stmt = $conn->prepare($sql);
+        $stmt->execute(array(':usr_id' => $session->get_usr_id(), ':ssel_id' => $row['ssel_id']));
+        $stmt->closeCursor();
+      }
     }
 
     return $baskets;
