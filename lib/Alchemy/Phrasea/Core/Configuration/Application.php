80bc7e7217d24381c2fc3c8c84aa4c6c3bc0d9b1
Nicolas Le Goff <legoff@alchemy.fr>
2012-01-06 16:36:39 +0100

    add Configuration\Application::getServiceFile method

--- lib/Alchemy/Phrasea/Core/Configuration/Application.php
+++ lib/Alchemy/Phrasea/Core/Configuration/Application.php
@@ -89,5 +89,15 @@ class Application implements Specification
   {
     return new \SplFileObject($this->getConfigurationPathName());
   }
+  
+    /**
+   * Return the main configuration file 
+   * 
+   * @return \SplFileObject
+   */
+  public function getServiceFile()
+  {
+    return new \SplFileObject(__DIR__ . '/../../../../../config/service.yml');
+  }
 
 }
