e631a4598e7cbeda0921913630c59c60ed888a69
Romain Neutron <imprec@gmail.com>
2012-01-12 15:31:59 +0100

    Move user search in push controller

--- lib/Alchemy/Phrasea/Helper/Record/Push.php
+++ lib/Alchemy/Phrasea/Helper/Record/Push.php
@@ -32,20 +32,4 @@ class Push extends RecordHelper
   
   protected $required_rights = array('canpush');
   
-  public function search($search)
-  {
-    $query = new \User_Query(appbox::get_instance());
-    
-    $result = $query->on_bases_where_i_am($this->core->getAuthenticatedUser(), array('canpush'))
-          ->like(\User_Query::LIKE_FIRSTNAME, $search)
-          ->like(\User_Query::LIKE_LASTNAME, $search)
-          ->like(\User_Query::LIKE_LOGIN, $search)
-          ->like_match(\User_Query::LIKE_MATCH_OR)
-                  ->include_phantoms()
-            ->limit(0, 50)
-          ->execute()->get_results();
-    
-    
-  }
-  
 }
