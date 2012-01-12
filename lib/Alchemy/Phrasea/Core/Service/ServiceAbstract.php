500314aad207d56b9fc1ef68af6dc9edbaad61bb
Nicolas Le Goff <legoff@alchemy.fr>
2012-01-12 18:52:30 +0100

    add configuration access ans external service access from service

--- lib/Alchemy/Phrasea/Core/Service/ServiceAbstract.php
+++ lib/Alchemy/Phrasea/Core/Service/ServiceAbstract.php
@@ -11,24 +11,43 @@
 
 namespace Alchemy\Phrasea\Core\Service;
 
+use Alchemy\Phrasea\Core;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
+
 /**
  *
  * @package
  * @license     http://opensource.org/licenses/gpl-3.0 GPLv3
  * @link        www.phraseanet.com
  */
-
 class ServiceAbstract
 {
+
   protected $name;
   protected $options;
-  
+  protected $configuration;
+
   public function __construct($name, Array $options)
   {
     $this->name = $name;
     $this->options = $options;
+
+    $spec = new Core\Configuration\Application();
+    $parser = new Core\Configuration\Parser\Yaml();
+    $handler = new Core\Configuration\Handler($spec, $parser);
+
+    $this->configuration = new Core\Configuration($handler);
   }
-  
+
+  /**
+   *
+   * @return Core\Configuration 
+   */
+  protected function getConfiguration()
+  {
+    return $this->configuration;
+  }
+
   /**
    *
    * @return string
@@ -46,13 +65,30 @@ class ServiceAbstract
   {
     return $this->options;
   }
-  
+
   /**
    *
    * @return string
    */
   public function getVersion()
   {
-    return 'Unknow';
+    return '';
+  }
+
+  /**
+   *
+   * @param type $serviceName
+   * @param type $serviceScope
+   * @return ServiceInterface
+   */
+  protected function findService($serviceName, $serviceScope, ParameterBag $configuration, $namespace = null)
+  {
+    
+    return Core\ServiceBuilder::build(
+                      $serviceName
+                    , $serviceScope
+                    , $configuration
+                    , $namespace
+    );
   }
 }
