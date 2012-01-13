c8e0f0764b6a7699a4f04530f1a4105bef495f61
Romain Neutron <imprec@gmail.com>
2012-01-13 12:12:46 +0100

    UsrLists fixtures

---
+++ lib/conf.d/PhraseaFixture/UsrLists/UsrList.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of Phraseanet
+ *
+ * (c) 2005-2010 Alchemy
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PhraseaFixture\UsrLists;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+
+/**
+ *
+ * @package
+ * @license     http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link        www.phraseanet.com
+ */
+class UsrList extends ListAbstract implements FixtureInterface
+{
+
+  /**
+   *
+   * @var \Entities\UsrList
+   */
+  public $list;
+
+  public function load($manager)
+  {
+    $list = new \Entities\UsrList();
+
+    $owner = $this->getReference('one-listowner');
+
+    $list->setName('new list');
+    $list->addUsrListOwner($owner);
+    
+    /* @var $owner \Entities\UsrListOwner */
+    $owner->setList($list);
+    
+    $manager->persist($list);
+    $manager->merge($owner);
+    $manager->flush();
+
+    $this->list = $list;
+
+    $this->addReference('one-list', $list);
+  }
+
+}
---
+++ lib/conf.d/PhraseaFixture/UsrLists/UsrListEntry.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ * This file is part of Phraseanet
+ *
+ * (c) 2005-2010 Alchemy
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * 
+ * @package
+ * @license     http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link        www.phraseanet.com
+ */
+
+namespace PhraseaFixture\UsrLists;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+
+/**
+ *
+ * @package
+ * @license     http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link        www.phraseanet.com
+ */
+class UsrListEntry extends ListAbstract implements FixtureInterface
+{
+
+  /**
+   *
+   * @var \Entities\UsrListEntry
+   */
+  public $entry;
+
+  public function load($manager)
+  {
+    $entry = new \Entities\UsrListEntry();
+
+    if (null === $this->user)
+    {
+      throw new \LogicException('Fill a user to store a new basket');
+    }
+    
+    $list = $this->getReference('one-list');
+    
+    $entry->setUser($this->user);
+    $entry->setList($list);
+    
+    /* @var $list \Entities\UsrList */
+    $list->addUsrListEntry($entry);
+    
+    $manager->persist($entry);
+    $manager->flush();
+
+    $this->entry = $entry;
+
+    $this->addReference('one-entry', $entry);
+  }
+
+}
