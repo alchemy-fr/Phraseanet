60b9d45837e899a1fd7a0a74c26bfed1636a5cab
Nicolas Le Goff <legoff@alchemy.fr>
2012-01-17 16:38:00 +0100

    add moveCollection tests

--- lib/unitTest/Alchemy/Phrasea/Controller/Prod/MoveCollectionTest.php
+++ lib/unitTest/Alchemy/Phrasea/Controller/Prod/MoveCollectionTest.php
@@ -17,6 +17,7 @@ class ControllerMoveCollectionTest extends \PhraseanetWebTestCaseAuthenticatedAb
    * As controllers use WebTestCase, it requires a client 
    */
   protected $client;
+
   /**
    * If the controller tests require some records, specify it her
    * 
@@ -26,7 +27,7 @@ class ControllerMoveCollectionTest extends \PhraseanetWebTestCaseAuthenticatedAb
    * $need_records = 2; 
    * 
    */
-  protected static $need_records = false;
+  protected static $need_records = 1;
 
   /**
    * The application loader
@@ -35,7 +36,7 @@ class ControllerMoveCollectionTest extends \PhraseanetWebTestCaseAuthenticatedAb
   {
     return require __DIR__ . '/../../../../../Alchemy/Phrasea/Application/Prod.php';
   }
-  
+
   public function setUp()
   {
     parent::setUp();
@@ -52,9 +53,21 @@ class ControllerMoveCollectionTest extends \PhraseanetWebTestCaseAuthenticatedAb
    */
   public function testRouteSlash()
   {
-    $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-    );
+    $this->client->request('POST', '/records/movecollection/', array('lst' => self::$record_1->get_serialize_key()));
+
+    $response = $this->client->getResponse();
+
+    $this->assertTrue($response->isOk());
+  }
+
+  public function testApply()
+  {
+
+    $this->client->request('POST', '/records/movecollection/apply/', array('lst' => self::$record_1->get_serialize_key(), 'base_id' => self::$collection->get_base_id()));
+
+    $response = $this->client->getResponse();
+
+    $this->assertTrue($response->isOk());
   }
 
 }
