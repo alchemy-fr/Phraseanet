443482f9714c62eaa41c088ebde2a95e83fd0128
Romain Neutron <imprec@gmail.com>
2012-01-26 16:44:33 +0100

    Added some vocabulary edit tests

--- lib/unitTest/Alchemy/Phrasea/Controller/Prod/EditTest.php
+++ lib/unitTest/Alchemy/Phrasea/Controller/Prod/EditTest.php
@@ -61,5 +61,39 @@ class ControllerEditTest extends \PhraseanetWebTestCaseAuthenticatedAbstract
 
     $this->assertTrue($response->isOk());
   }
+  
+
+  public function testVocabulary()
+  {
+    $this->client->request('GET', '/records/edit/vocabulary/Zanzibar/');
+
+    $response = $this->client->getResponse();
+    $this->assertTrue($response->isOk());
+    $datas = json_decode($response->getContent());
+    $this->assertFalse($datas->success);
+    
+    $this->client->request('GET', '/records/edit/vocabulary/User/');
+
+    $response = $this->client->getResponse();
+    $this->assertTrue($response->isOk());
+    $datas = json_decode($response->getContent());
+    $this->assertFalse($datas->success);
+    
+    $params = array('sbas_id'=>self::$collection->get_sbas_id());
+    $this->client->request('GET', '/records/edit/vocabulary/Zanzibar/', $params);
+
+    $response = $this->client->getResponse();
+    $this->assertTrue($response->isOk());
+    $datas = json_decode($response->getContent());
+    $this->assertFalse($datas->success);
+    
+    $params = array('sbas_id'=>self::$collection->get_sbas_id());
+    $this->client->request('GET', '/records/edit/vocabulary/User/', $params);
+
+    $response = $this->client->getResponse();
+    $this->assertTrue($response->isOk());
+    $datas = json_decode($response->getContent());
+    $this->assertTrue($datas->success);
+  }
 
 }
