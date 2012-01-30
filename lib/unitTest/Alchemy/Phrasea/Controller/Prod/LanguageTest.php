98abdcb1d3ca4a3dcb87c1fbba6f729ae488bb63
Nicolas Le Goff <legoff@alchemy.fr>
2012-01-30 17:22:36 +0100

    fix build

--- lib/unitTest/Alchemy/Phrasea/Controller/Prod/LanguageTest.php
+++ lib/unitTest/Alchemy/Phrasea/Controller/Prod/LanguageTest.php
@@ -17,7 +17,7 @@ require_once __DIR__ . '/../../../../PhraseanetWebTestCaseAbstract.class.inc';
  * @license     http://opensource.org/licenses/gpl-3.0 GPLv3
  * @link        www.phraseanet.com
  */
-class ControllerBasketTest extends PhraseanetWebTestCaseAbstract
+class ControllerLanguageTest extends PhraseanetWebTestCaseAbstract
 {
 
   protected $client;
