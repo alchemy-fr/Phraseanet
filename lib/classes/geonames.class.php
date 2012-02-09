841d0eac49a8f7dd3321132aa722e4cb9a1dbe5d
Romain Neutron <imprec@gmail.com>
2012-02-09 19:07:16 +0100

    Add some cache to geonames

--- lib/classes/geonames.class.php
+++ lib/classes/geonames.class.php
@@ -1,23 +1,47 @@
 <?php
 
-class geonames
+class geonames implements cache_cacheableInterface
 {
+
   protected static $NamesFromId = array();
-
   protected static $CountryFromId = array();
-
   protected static $CountryCodeFromId = array();
-
   protected static $GeonameFromIp = array();
-
   protected static $Searches = array();
+
+  const CACHE_NAMESFROMID       = 'NAMESFROMID';
+  const CACHE_COUNTRYFROMID     = 'COUNTRYFROMID';
+  const CACHE_COUNTRYCODEFROMID = 'COUNTRYCODEFROMID';
+  const CACHE_GEONAMEFROMIP     = 'GEONAMEFROMIP';
+  const CACHE_SEARCH            = 'SEARCH';
 
   public function name_from_id($geonameid)
   {
+    if (trim($geonameid) === '')
+    {
+      return null;
+    }
+
+    if (!ctype_digit($geonameid))
+    {
+      return null;
+    }
+
+    $cache_id = self::CACHE_NAMESFROMID . '_' . $geonameid;
+
+    try
+    {
+      return $this->get_data_from_cache($cache_id);
+    }
+    catch (\Exception $e)
+    {
+
+    }
+
     $registry = registry::get_instance();
-    $url = $registry->get('GV_i18n_service', 'http://localization.webservice.alchemyasp.com/')
-            . 'get_name.php?geonameid='
-            . $geonameid;
+    $url      = $registry->get('GV_i18n_service', 'http://localization.webservice.alchemyasp.com/')
+      . 'get_name.php?geonameid='
+      . $geonameid;
 
     $ret = '';
 
@@ -33,20 +57,33 @@ class geonames
       }
     }
 
+    $this->set_data_to_cache($ret, $cache_id);
+
     return $ret;
   }
 
   public function get_country($geonameid)
   {
-    if (trim($geonameid) === '' || trim($geonameid) <= 0)
+    if (trim($geonameid) === '' || !ctype_digit($geonameid))
     {
       return '';
     }
 
+    $cache_id = self::CACHE_COUNTRYFROMID . '_' . $geonameid;
+
+    try
+    {
+      return $this->get_data_from_cache($cache_id);
+    }
+    catch (\Exception $e)
+    {
+
+    }
+
     $registry = registry::get_instance();
-    $url = $registry->get('GV_i18n_service', 'http://localization.webservice.alchemyasp.com/')
-            . 'get_name.php?geonameid='
-            . $geonameid;
+    $url      = $registry->get('GV_i18n_service', 'http://localization.webservice.alchemyasp.com/')
+      . 'get_name.php?geonameid='
+      . $geonameid;
 
     $ret = '';
     $xml = http_query::getUrl($url);
@@ -60,15 +97,38 @@ class geonames
       }
     }
 
+    $this->set_data_to_cache($ret, $cache_id);
+
     return $ret;
   }
 
   public function get_country_code($geonameid)
   {
+    if (trim($geonameid) === '')
+    {
+      return null;
+    }
+
+    if (!ctype_digit($geonameid))
+    {
+      return null;
+    }
+
+    $cache_id = self::CACHE_COUNTRYCODEFROMID . '_' . $geonameid;
+
+    try
+    {
+      return $this->get_data_from_cache($cache_id);
+    }
+    catch (\Exception $e)
+    {
+
+    }
+
     $registry = registry::get_instance();
-    $url = $registry->get('GV_i18n_service', 'http://localization.webservice.alchemyasp.com/')
-            . 'get_name.php?geonameid='
-            . $geonameid;
+    $url      = $registry->get('GV_i18n_service', 'http://localization.webservice.alchemyasp.com/')
+      . 'get_name.php?geonameid='
+      . $geonameid;
 
     $ret = '';
 
@@ -83,6 +143,8 @@ class geonames
       }
     }
 
+    $this->set_data_to_cache($ret, $cache_id);
+
     return $ret;
   }
 
@@ -94,7 +156,7 @@ class geonames
   protected static function highlight($title, $length)
   {
     return '<span class="highlight">' . mb_substr($title, 0, $length) . '</span>'
-            . mb_substr($title, $length);
+      . mb_substr($title, $length);
   }
 
   public function find_city($cityName)
@@ -103,13 +165,24 @@ class geonames
     $cityName = self::clean_input($cityName);
 
     if (strlen($cityName) === 0)
-
       return $output;
 
+    $cache_id = self::CACHE_SEARCH . '_' . $cityName;
+
+    try
+    {
+      return $this->get_data_from_cache($cache_id);
+    }
+    catch (\Exception $e)
+    {
+
+    }
+
+
     $registry = registry::get_instance();
-    $url = $registry->get('GV_i18n_service', 'http://localization.webservice.alchemyasp.com/')
-            . 'find_city.php?city='
-            . urlencode($cityName) . '&maxResult=30';
+    $url      = $registry->get('GV_i18n_service', 'http://localization.webservice.alchemyasp.com/')
+      . 'find_city.php?city='
+      . urlencode($cityName) . '&maxResult=30';
 
     $sxe = simplexml_load_string(http_query::getUrl($url));
 
@@ -122,19 +195,21 @@ class geonames
       $country_highlight = (string) $geoname->country;
       if (trim($geoname->country_match) !== '')
       {
-        $length = mb_strlen($geoname->country_match);
+        $length            = mb_strlen($geoname->country_match);
         $country_highlight = self::highlight($geoname->country, $length);
       }
 
       $output[] = array(
-          'title_highlighted' => $title_highlight
-          , 'title' => (string) $geoname->title
-          , 'country_highlighted' => $country_highlight
-          , 'country' => (string) $geoname->country
-          , 'geoname_id' => (int) $geoname->geonameid
-          , 'region' => (string) $geoname->region
+        'title_highlighted'   => $title_highlight
+        , 'title'               => (string) $geoname->title
+        , 'country_highlighted' => $country_highlight
+        , 'country'             => (string) $geoname->country
+        , 'geoname_id'          => (int) $geoname->geonameid
+        , 'region'              => (string) $geoname->region
       );
     }
+
+    $this->set_data_to_cache($output, $cache_id);
 
     return $output;
   }
@@ -144,22 +219,38 @@ class geonames
   public function find_geoname_from_ip($ip)
   {
     if (array_key_exists($ip, $this->cache_ips))
-
       return $this->cache_ips[$ip];
 
+
+    if (trim($ip) === '')
+    {
+      return null;
+    }
+
+    $cache_id = self::CACHE_GEONAMEFROMIP . '_' . $ip;
+
+    try
+    {
+      return $this->get_data_from_cache($cache_id);
+    }
+    catch (\Exception $e)
+    {
+
+    }
+
     $output = array(
-        'city' => '',
-        'country_code' => '',
-        'country' => '',
-        'fips' => '',
-        'longitude' => '',
-        'latitude' => ''
+      'city'         => '',
+      'country_code' => '',
+      'country'      => '',
+      'fips'         => '',
+      'longitude'    => '',
+      'latitude'     => ''
     );
 
     $registry = registry::get_instance();
-    $url = $registry->get('GV_i18n_service', 'http://localization.webservice.alchemyasp.com/')
-            . 'geoip.php?ip='
-            . urlencode($ip);
+    $url      = $registry->get('GV_i18n_service', 'http://localization.webservice.alchemyasp.com/')
+      . 'geoip.php?ip='
+      . urlencode($ip);
 
     $xml = http_query::getUrl($url);
     if ($xml)
@@ -167,18 +258,45 @@ class geonames
       $sxe = simplexml_load_string($xml);
       if ($sxe && $sxe->geoname)
       {
-        $output['city'] = (string) $sxe->geoname->city;
+        $output['city']         = (string) $sxe->geoname->city;
         $output['country_code'] = (string) $sxe->geoname->country_code;
-        $output['country'] = (string) $sxe->geoname->country;
-        $output['fips'] = (string) $sxe->geoname->fips;
-        $output['longitude'] = (string) $sxe->geoname->longitude;
-        $output['latitude'] = (string) $sxe->geoname->latitude;
+        $output['country']      = (string) $sxe->geoname->country;
+        $output['fips']         = (string) $sxe->geoname->fips;
+        $output['longitude']    = (string) $sxe->geoname->longitude;
+        $output['latitude']     = (string) $sxe->geoname->latitude;
       }
     }
     $this->cache_ips[$ip] = $output;
 
+    $this->set_data_to_cache($output, $cache_id);
 
     return $output;
+  }
+
+  public function get_cache_key($option = null)
+  {
+    return 'geonames_' . ($option ? '_' . $option : '');
+  }
+
+  public function get_data_from_cache($option = null)
+  {
+    $appbox = appbox::get_instance();
+    $datas =  $appbox->get_data_from_cache($this->get_cache_key($option));
+
+    echo "got form cache\n";
+    return $datas;
+    }
+
+  public function set_data_to_cache($value, $option = null, $duration = 0)
+  {
+    $appbox = appbox::get_instance();
+    return $appbox->set_data_to_cache($value, $this->get_cache_key($option), $duration);
+  }
+
+  public function delete_data_from_cache($option = null)
+  {
+    $appbox = appbox::get_instance();
+    return $appbox->delete_data_from_cache($this->get_cache_key($option));
   }
 
 }
