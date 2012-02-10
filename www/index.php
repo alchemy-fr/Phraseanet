cb1235a329b08391c0df8fd8ef07ef50924ec67c
Romain Neutron <imprec@gmail.com>
2012-02-10 17:26:04 +0100

    Fix bootstraping

--- www/index.php
+++ www/index.php
@@ -17,7 +17,7 @@
  */
 
 
-require_once __DIR__ . "/../lib/classes/bootstrap.class.php";
+require_once __DIR__ . "/../lib/bootstrap.php";
 
 $app = require __DIR__ . '/../lib/Alchemy/Phrasea/Application/Root.php';
 
