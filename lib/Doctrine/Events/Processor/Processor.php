d94ce73f5c892760718b353db0684ceed94717dc
Nicolas Le Goff <legoff@alchemy.fr>
2012-02-15 19:31:24 +0100

    add doctrine even listener for clearing the cache

---
+++ lib/Doctrine/Events/Processor/Processor.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of Phraseanet
+ *
+ * (c) 2005-2010 Alchemy
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Events\Processor;
+
+use Doctrine\Common\EventArgs;
+/**
+ *
+ * @package
+ * @license     http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link        www.phraseanet.com
+ */
+
+interface Processor
+{
+  public function process(EventArgs $args);
+}
