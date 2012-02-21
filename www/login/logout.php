ef08ff70595889aaf3e4254ae8323ab2b79e5aae
Romain Neutron <imprec@gmail.com>
2012-02-21 15:29:56 +0100

    Refactor Appbox

--- www/login/logout.php
+++ www/login/logout.php
@@ -15,7 +15,9 @@
  * @license     http://opensource.org/licenses/gpl-3.0 GPLv3
  * @link        www.phraseanet.com
  */
-require_once __DIR__ . "/../../lib/bootstrap.php";
+
+/* @var $Core \Alchemy\Phrasea\Core */
+$Core = require_once __DIR__ . "/../../lib/bootstrap.php";
 
 
 $request = http_request::getInstance();
@@ -23,7 +25,7 @@ $parm = $request->get_parms("app");
 
 try
 {
-  $appbox = appbox::get_instance();
+  $appbox = appbox::get_instance($Core);
   $session = $appbox->get_session();
 
   $session->logout();
