e7df74ebfe38855ce016b0f7a1a539bf2e90a1e5
Romain Neutron <imprec@gmail.com>
2012-03-30 13:51:28 +0200

    Use Composer for dependencies instead of git submodules

--- vendors.php
+++ vendors.php
@@ -12,10 +12,26 @@
 /**
  * Get all dependencies needed for Phraseanet
  */
-
 chdir(__DIR__);
 
 set_time_limit(0);
+
+$composer = __DIR__ . '/composer.phar';
+
+if (!file_exists($composer))
+{
+  system('curl -s http://getcomposer.org/installer | php');
+  system('chmod +x ' . $composer);
+  system($composer . ' install');
+}
+
+if (!is_executable($composer))
+{
+  system('chmod +x ' . $composer);
+}
+
+system($composer . ' self-update');
+system($composer . ' update');
 
 system('git submodule init');
 system('git submodule update');
@@ -25,10 +41,9 @@ $iterator = new RecursiveDirectoryIterator(__DIR__ . '/lib/vendor/');
 foreach ($iterator as $file)
 {
   /* @var $file SplFileInfo */
-  if($file->isDir())
+  if ($file->isDir())
   {
-    $cmd = sprintf('cd %s && git submodule init && git submodule update',
-            escapeshellarg($file->getPathname()));
+    $cmd = sprintf('cd %s && git submodule init && git submodule update', escapeshellarg($file->getPathname()));
     system($cmd);
   }
 }
