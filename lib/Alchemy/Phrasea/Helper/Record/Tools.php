bb3451a27ff6b5ed1a352d93d493e162c351ed93
Nicolas Le Goff <legoff.n@gmail.com>
2012-04-05 19:23:39 +0200

    add tools helper

---
+++ lib/Alchemy/Phrasea/Helper/Record/Tools.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of Phraseanet
+ *
+ * (c) 2005-2010 Alchemy
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Alchemy\Phrasea\Helper\Record;
+
+
+use Alchemy\Phrasea\Core;
+use Symfony\Component\HttpFoundation\Request;
+use Alchemy\Phrasea\Helper\Record\Helper as RecordHelper;
+
+/**
+ *
+ *
+ * @license     http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link        www.phraseanet.com
+ */
+class Tools extends RecordHelper
+{
+
+}
