c201ad301a9a18ea909bd77486e2188c41b35262
Nicolas Le Goff <legoff.n@gmail.com>
2012-04-24 17:47:04 +0200

    add api/ route which provide informations on supported api versions

---
+++ www/api/index.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of Phraseanet
+ *
+ * (c) 2005-2010 Alchemy
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ *
+ * @package
+ * @license     http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link        www.phraseanet.com
+ */
+
+require_once __DIR__ . '/../../lib/bootstrap.php';
+
+$app = require __DIR__ . '/../../lib/Alchemy/Phrasea/Application/ApiVersion.php';
+
+$app->run();
