135234640b6df996b1f6df4de2463b8e3c8b822d
Romain Neutron <imprec@gmail.com>
2012-04-25 00:04:19 +0200

    Add missing license header

--- lib/Alchemy/Phrasea/Media/Subdef/Provider.php
+++ lib/Alchemy/Phrasea/Media/Subdef/Provider.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Phraseanet
+ *
+ * (c) 2005-2010 Alchemy
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Alchemy\Phrasea\Media\Subdef;
 
 abstract class Provider implements Subdef
