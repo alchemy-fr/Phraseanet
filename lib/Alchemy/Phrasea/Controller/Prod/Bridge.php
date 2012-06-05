5450e9ca732232d9efa23b55f041f8c5ecc1e86f
Ysolyne Gresille <ygresille@alchemy.fr>
2012-06-05 10:44:52 +0200

    view bugfixes + update skins for Bridge

--- lib/Alchemy/Phrasea/Controller/Prod/Bridge.php
+++ lib/Alchemy/Phrasea/Controller/Prod/Bridge.php
@@ -98,7 +98,6 @@ class Bridge implements ControllerProviderInterface
 
                     $connector->reconnect();
                 } catch (\Exception $e) {
-                    var_dump(get_class($e), $e->getFile(), $e->getLine(), $e->getTraceAsString());
                     $error_message = $e->getMessage();
                 }
 
