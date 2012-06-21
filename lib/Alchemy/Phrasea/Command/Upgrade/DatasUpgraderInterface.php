283891b35d61539ac82b5eefccf979dd20353f81
Romain Neutron <imprec@gmail.com>
2012-06-21 09:30:02 +0200

    Add license header

--- lib/Alchemy/Phrasea/Command/Upgrade/DatasUpgraderInterface.php
+++ lib/Alchemy/Phrasea/Command/Upgrade/DatasUpgraderInterface.php
@@ -1,10 +1,22 @@
 <?php
 
+/*
+ * This file is part of Phraseanet
+ *
+ * (c) 2005-2012 Alchemy
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Alchemy\Phrasea\Command\Upgrade;
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ * The data upgrader interface
+ */
 interface DatasUpgraderInterface
 {
 
