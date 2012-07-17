3a8be265f7e42b9f90895f23ead93f6e7b81ac55
Nicolas Le Goff <legoff.n@gmail.com>
2012-07-17 18:56:21 +0200

    fix unit tests

--- lib/Alchemy/Phrasea/Controller/Root/Account.php
+++ lib/Alchemy/Phrasea/Controller/Root/Account.php
@@ -405,10 +405,10 @@ class Account implements ControllerProviderInterface
             }
         }
 
-        return new Response($app['Core']['Twig']->render('account/reset-email.html.twig'), array(
-                'noticeMsg' => $noticeMsg,
-                'updateMsg'  => $updateMsg,
-            ));
+        return new Response($app['Core']['Twig']->render('account/reset-email.html.twig', array(
+                    'noticeMsg' => $noticeMsg,
+                    'updateMsg' => $updateMsg,
+                )));
     }
 
     /**
