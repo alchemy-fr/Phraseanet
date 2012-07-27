742216c41dc8714f7f1af193beb95b2e7579880a
Romain Neutron <imprec@gmail.com>
2012-07-27 15:57:39 +0200

    Fix tests

--- tests/Alchemy/Phrasea/Command/CommandTest.php
+++ tests/Alchemy/Phrasea/Command/CommandTest.php
@@ -14,17 +14,6 @@ class CommandTest extends \PHPUnit_Framework_TestCase
     protected function setUp()
     {
         $this->object = new AbstractCommandTester('name');
-        $this->logger = new \Monolog\Logger('test');
-    }
-
-    /**
-     * @covers Alchemy\Phrasea\Command\Command::setLogger
-     * @covers Alchemy\Phrasea\Command\Command::getLogger
-     */
-    public function testSetLogger()
-    {
-        $this->object->setLogger($this->logger);
-        $this->assertEquals($this->logger, $this->object->getLogger());
     }
 
     /**
