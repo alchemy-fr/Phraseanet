afdeb226739e2fe68e7864f6c7d91c5e37a90682
Romain Neutron <imprec@gmail.com>
2012-08-28 11:41:26 +0200

    [SearchEngine] Add unit tests

--- tests/Alchemy/Phrasea/SearchEngine/SearchEngineAbstractTest.php
+++ tests/Alchemy/Phrasea/SearchEngine/SearchEngineAbstractTest.php
@@ -4,21 +4,24 @@ namespace Alchemy\Phrasea\SearchEngine;
 
 require_once __DIR__ . '/../../../PhraseanetPHPUnitAuthenticatedAbstract.class.inc';
 
-abstract class SearchEngineAbstractTest extends \PhraseanetPHPUnitAuthenticatedAbstract
+abstract class SearchEngineAbstractTest extends \PhraseanetPHPUnitAbstract
 {
-
     protected static $searchEngine;
 
     public function setUp()
     {
         parent::setUp();
+        $appbox = \appbox::get_instance(\bootstrap::getCore());
+        foreach($appbox->get_databoxes() as $databox) {
+            break;
+        }
 
-        if(!self::$searchEngine instanceof SearchEngineInterface) {
+        if ( ! self::$searchEngine instanceof SearchEngineInterface) {
             $this->markTestSkipped('Unable to initialize search Engine');
         }
 
         $options = new SearchEngineOptions();
-        $options->onCollections(self::$user->ACL()->get_granted_base());
+        $options->onCollections($databox->get_collections());
 
         self::$searchEngine->setOptions($options);
     }
@@ -32,75 +35,235 @@ abstract class SearchEngineAbstractTest extends \PhraseanetPHPUnitAuthenticatedA
     {
         $record = self::$records['record_24'];
 
+        self::$searchEngine->removeRecord($record);
+        $this->updateIndex();
+
         $results = self::$searchEngine->query('recordid=' . $record->get_record_id(), 0, 1);
 
         $this->assertEquals(0, $results->total());
 
         self::$searchEngine->addRecord($record);
         $this->updateIndex();
-        sleep(1);
-        self::$searchEngine->query('recordid=' . $record->get_record_id(), 0, 1);
 
         $results = self::$searchEngine->query('recordid=' . $record->get_record_id(), 0, 1);
 
         $this->assertEquals(1, $results->total());
+
+        self::$searchEngine->removeRecord($record);
+        $this->updateIndex();
     }
 
+    public function testUpdateRecord()
+    {
+        $record = self::$records['record_24'];
 
-//
-//    public function testUpdateRecord()
-//    {
-//        $record = self::$records['record_24'];
-//
-//        self::$searchEngine->addRecord($record);
-//        $this->updateIndex();
-//
-//        $results = self::$searchEngine->query('boomboklot', 0, 1);
-//        $this->assertEquals(0, $results->total());
-//
-//        $toupdate = array();
-//
-//        foreach ($record->get_databox()->get_meta_structure()->get_elements() as $field) {
-//            try {
-//                $values = $record->get_caption()->get_field($field->get_name())->get_values();
-//                $value = array_pop($values);
-//                $meta_id = $value->getId();
-//            } catch (\Exception $e) {
-//                $meta_id = null;
-//            }
-//
-//            $toupdate[$field->get_id()] = array(
-//                'meta_id'        => $meta_id
-//                , 'meta_struct_id' => $field->get_id()
-//                , 'value'          => 'boomboklot '
-//            );
-//            break;
-//        }
-//
-//        $record->set_metadatas($toupdate);
-//
-//        self::$searchEngine->updateRecord($record);
-//        $this->updateIndex();
-//
-//        $results = self::$searchEngine->query('boomboklot', 0, 1);
-//        $this->assertEquals(1, $results->total());
-//    }
-//
-//    public function testDeleteRecord()
-//    {
-//        $record = self::$records['record_24'];
-//
-//        self::$searchEngine->addRecord($record);
-//        $this->updateIndex();
-//
-//        $results = self::$searchEngine->query('recordid='.$record->get_record_id(), 0, 1);
-//        $this->assertEquals(1, $results->total());
-//
-//        self::$searchEngine->removeRecord($record);
-//        $this->updateIndex();
-//
-//        $results = self::$searchEngine->query('recordid='.$record->get_record_id(), 0, 1);
-//        $this->assertEquals(0, $results->total());
-//    }
+        self::$searchEngine->addRecord($record);
+        $this->updateIndex();
+
+        $query_string = 'boomboklot' . $record->get_record_id() ;
+
+        $results = self::$searchEngine->query($query_string, 0, 1);
+        $this->assertEquals(0, $results->total());
+
+        $toupdate = array();
+
+        foreach ($record->get_databox()->get_meta_structure()->get_elements() as $field) {
+            try {
+                $values = $record->get_caption()->get_field($field->get_name())->get_values();
+                $value = array_pop($values);
+                $meta_id = $value->getId();
+            } catch (\Exception $e) {
+                $meta_id = null;
+            }
+
+            $toupdate[$field->get_id()] = array(
+                'meta_id'        => $meta_id
+                , 'meta_struct_id' => $field->get_id()
+                , 'value'          => $query_string
+            );
+            break;
+        }
+
+        $record->set_metadatas($toupdate);
+
+        self::$searchEngine->updateRecord($record);
+        $this->updateIndex();
+
+        sleep(1);
+
+        $results = self::$searchEngine->query($query_string, 0, 1);
+
+        $this->assertEquals(1, $results->total());
+
+        self::$searchEngine->removeRecord($record);
+        $this->updateIndex();
+    }
+
+    public function testUpdateNonIndexableRecord()
+    {
+        $record = self::$records['record_24'];
+
+        self::$searchEngine->addRecord($record);
+        $this->updateIndex();
+
+        $query_string = 'boomboklot_no_index_' . $record->get_record_id() . '_';
+
+        $results = self::$searchEngine->query($query_string, 0, 1);
+        $this->assertEquals(0, $results->total());
+
+        $toupdate = array();
+
+        foreach ($record->get_databox()->get_meta_structure()->get_elements() as $field) {
+            if ($field->is_indexable()) {
+                continue;
+            }
+
+            try {
+                $values = $record->get_caption()->get_field($field->get_name())->get_values();
+                $value = array_pop($values);
+                $meta_id = $value->getId();
+            } catch (\Exception $e) {
+                $meta_id = null;
+            }
+
+            $toupdate[$field->get_id()] = array(
+                'meta_id'        => $meta_id
+                , 'meta_struct_id' => $field->get_id()
+                , 'value'          => $query_string
+            );
+            break;
+        }
+
+        if ( ! $toupdate) {
+            $this->markTestSkipped('No non-indexable field found');
+        }
+
+        $record->set_metadatas($toupdate);
+
+        self::$searchEngine->updateRecord($record);
+        $this->updateIndex();
+
+        $results = self::$searchEngine->query($query_string, 0, 1);
+        $this->assertEquals(0, $results->total());
+
+        self::$searchEngine->removeRecord($record);
+        $this->updateIndex();
+    }
+
+    public function testDeleteRecord()
+    {
+        $record = self::$records['record_24'];
+
+        $results = self::$searchEngine->query('recordid=' . $record->get_record_id(), 0, 1);
+        $floor = $results->total();
+
+        self::$searchEngine->addRecord($record);
+        $this->updateIndex();
+
+        $results = self::$searchEngine->query('recordid=' . $record->get_record_id(), 0, 1);
+        $this->assertEquals($floor + 1, $results->total());
+
+        self::$searchEngine->removeRecord($record);
+        $this->updateIndex();
+
+        $results = self::$searchEngine->query('recordid=' . $record->get_record_id(), 0, 1);
+        $this->assertEquals($floor, $results->total());
+    }
+
+    public function testAvailableTypes()
+    {
+        $this->assertTrue(is_array(self::$searchEngine->availableTypes()));
+        foreach (self::$searchEngine->availableTypes() as $type) {
+            $this->assertTrue(in_array($type, array(SearchEngineInterface::GEM_TYPE_ENTRY, SearchEngineInterface::GEM_TYPE_RECORD, SearchEngineInterface::GEM_TYPE_STORY)));
+        }
+    }
+
+    public function testConfigurationPanel()
+    {
+        $this->assertInstanceOf('\\Alchemy\\Phrasea\\SearchEngine\\ConfigurationPanelInterface', self::$searchEngine->configurationPanel());
+    }
+
+    public function testStatus()
+    {
+        foreach (self::$searchEngine->status() as $StatusKeyValue) {
+            $this->assertTrue(is_array($StatusKeyValue));
+            $this->assertTrue(is_scalar($StatusKeyValue[0]));
+            $this->assertTrue(is_scalar($StatusKeyValue[1]));
+        }
+    }
+
+    public function testAddStory()
+    {
+        $story = self::$records['record_story_1'];
+
+        $options = new SearchEngineOptions();
+        $options->onCollections(self::$user->ACL()->get_granted_base());
+        $options->setSearchType(SearchEngineOptions::RECORD_GROUPING);
+        self::$searchEngine->setOptions($options);
+
+        $results = self::$searchEngine->query('storyid=' . $story->get_record_id(), 0, 1);
+
+        $this->assertEquals(0, $results->total());
+
+        self::$searchEngine->addStory($story);
+        $this->updateIndex();
+
+        $results = self::$searchEngine->query('storyid=' . $story->get_record_id(), 0, 1);
+
+        $this->assertEquals(1, $results->total());
+
+        self::$searchEngine->removeStory($story);
+        $this->updateIndex();
+    }
+
+    public function testUpdateStory()
+    {
+        $story = self::$records['record_story_1'];
+
+        $options = new SearchEngineOptions();
+        $options->onCollections(self::$user->ACL()->get_granted_base());
+        $options->setSearchType(SearchEngineOptions::RECORD_GROUPING);
+        self::$searchEngine->setOptions($options);
+
+        self::$searchEngine->addStory($story);
+        $this->updateIndex();
+
+        $query_string = 'boomboklot_' . $story->get_record_id() . '_';
+
+        $results = self::$searchEngine->query($query_string, 0, 1);
+        $this->assertEquals(0, $results->total());
+
+        $toupdate = array();
+
+        foreach ($story->get_databox()->get_meta_structure()->get_elements() as $field) {
+            try {
+                $values = $story->get_caption()->get_field($field->get_name())->get_values();
+                $value = array_pop($values);
+                $meta_id = $value->getId();
+            } catch (\Exception $e) {
+                $meta_id = null;
+            }
+
+            $toupdate[$field->get_id()] = array(
+                'meta_id'        => $meta_id
+                , 'meta_struct_id' => $field->get_id()
+                , 'value'          => $query_string
+            );
+            break;
+        }
+
+        $story->set_metadatas($toupdate);
+
+        self::$searchEngine->updateStory($story);
+        $this->updateIndex();
+
+        $results = self::$searchEngine->query($query_string, 0, 1);
+        $this->assertEquals(1, $results->total());
+
+        self::$searchEngine->removeStory($story);
+        $this->updateIndex();
+    }
+
+    abstract function testAutocomplete();
 
 }
--- tests/Alchemy/Phrasea/SearchEngine/SphinxSearchEngineTest.php
+++ tests/Alchemy/Phrasea/SearchEngine/SphinxSearchEngineTest.php
@@ -3,48 +3,107 @@
 namespace Alchemy\Phrasea\SearchEngine;
 
 use Alchemy\Phrasea\SearchEngine\SphinxSearch\SphinxSearchEngine;
+use Symfony\Component\Process\ExecutableFinder;
+use Symfony\Component\Process\Process;
 
 require_once __DIR__ . '/SearchEngineAbstractTest.php';
 
 class SphinxSearchEngineTest extends SearchEngineAbstractTest
 {
+    protected static $config;
     protected static $searchd;
+
     public static function setUpBeforeClass()
     {
         parent::setUpBeforeClass();
-        $appbox = \appbox::get_instance(self::$core);
+        $appbox = \appbox::get_instance(\bootstrap::getCore());
         self::$searchEngine = new SphinxSearchEngine('127.0.0.1', 9306, '127.0.0.1', 9308);
 
-        $config = tempnam(sys_get_temp_dir(), 'tmp_sphinx.conf');
+        self::$config = tempnam(sys_get_temp_dir(), 'tmp_sphinx.conf');
         $configFile = self::$searchEngine->configurationPanel()->generateSphinxConf($appbox->get_databoxes(), self::$searchEngine->configurationPanel()->getConfiguration());
-        $configFile = str_replace('sql_pass              =', 'sql_pass              = toor', $configFile);
 
-        file_put_contents($config, $configFile);
+        file_put_contents(self::$config, $configFile);
 
-        $binaryFinder = new \Symfony\Component\Process\ExecutableFinder();
+        $binaryFinder = new ExecutableFinder();
         $indexer = $binaryFinder->find('indexer');
 
         $searchd = $binaryFinder->find('searchd');
 
-        $process = new \Symfony\Component\Process\Process($indexer.' --all -c '.$config);
+        $process = new Process($indexer . ' --all -c ' . self::$config);
         $process->run();
-        self::$searchd = new \Symfony\Component\Process\Process($searchd.' --nodetach -c '.$config);
-        self::$searchd->start();
 
-        sleep(2);
+        self::$searchd = new Process($searchd . ' -c ' . self::$config);
+        self::$searchd->run();
+
         self::$searchEngine = new SphinxSearchEngine('127.0.0.1', 9306, '127.0.0.1', 9308);
     }
 
+
     public static function tearDownAfterClass()
     {
-        self::$searchd->stop();
+        $binaryFinder = new ExecutableFinder();
+        $searchd = $binaryFinder->find('searchd');
+
+        self::$searchd = new Process($searchd . ' --stop -c ' . self::$config);
+        self::$searchd->run();
+
+        unlink(self::$config);
+
         parent::tearDownAfterClass();
     }
-
 
     public function setUp()
     {
         parent::setUp();
     }
+
+    public function testAutocomplete()
+    {
+        $record = self::$records['record_24'];
+
+        $toupdate = array();
+
+        foreach ($record->get_databox()->get_meta_structure()->get_elements() as $field) {
+            try {
+                $values = $record->get_caption()->get_field($field->get_name())->get_values();
+                $value = array_pop($values);
+                $meta_id = $value->getId();
+            } catch (\Exception $e) {
+                $meta_id = null;
+            }
+
+            $toupdate[$field->get_id()] = array(
+                'meta_id'        => $meta_id
+                , 'meta_struct_id' => $field->get_id()
+                , 'value'          => 'jeanne, jeannine, jeannette, jean-pierre et jean claude'
+            );
+            break;
+        }
+
+        $record->set_metadatas($toupdate);
+
+        self::$searchEngine->addRecord($record);
+        $this->updateIndex();
+
+        $binaryFinder = new ExecutableFinder();
+        $indexer = $binaryFinder->find('indexer');
+
+        $process = new Process($indexer . ' --all --rotate -c ' . self::$config);
+        $process->run();
+
+        $appbox = \appbox::get_instance(self::$core);
+        self::$searchEngine->buildSuggestions($appbox->get_databoxes(), self::$config, 0);
+
+        $suggestions = self::$searchEngine->autoComplete('jean');
+        $this->assertInstanceOf('\\Doctrine\\Common\\Collections\\ArrayCollection',$suggestions);
+
+        $this->assertGreaterThan(2, count($suggestions));
+
+        foreach($suggestions as $suggestion) {
+            $this->assertInstanceof('\\Alchemy\\Phrasea\\SearchEngine\\SearchEngineSuggestion', $suggestion);
+        }
+    }
+
+
 }
 
