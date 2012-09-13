52788c28291c01adf1d054894b8092a6834cd7e2
Romain Neutron <imprec@gmail.com>
2012-09-13 11:17:49 +0200

    Add unit tests

---
+++ tests/caption/caption_fieldTest.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once __DIR__ . '/../../lib/classes/caption/field.class.php';
+
+class caption_fieldTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers caption_field::get_multi_values
+     * @dataProvider getMultiValues
+     */
+    public function testGet_multi_values($separator, $serialized, $values)
+    {
+        $this->assertEquals($values, caption_field::get_multi_values($serialized, $separator));
+    }
+
+    public function getMultiValues()
+    {
+        return array(
+            array(';', 'arbre;fleur-chien maison', array('arbre', 'fleur-chien maison')),
+            array('-', 'arbre;fleur-chien maison', array('arbre;fleur', 'chien maison')),
+            array(';-', 'arbre;fleur-chien maison', array('arbre','fleur', 'chien maison')),
+            array(';- ', 'arbre;fleur-chien maison', array('arbre','fleur', 'chien', 'maison')),
+            array('/', 'arbre/fleur/chien maison', array('arbre','fleur', 'chien maison')),
+            array('\\', 'arbre\fleur\chien maison', array('arbre','fleur', 'chien maison')),
+            array('|', 'arbre|fleur|chien maison', array('arbre','fleur', 'chien maison')),
+            array(' ', 'arbre|fleur|chien maison', array('arbre|fleur|chien','maison')),
+            array(' ', 'arbre\fleur|chien maison', array('arbre\fleur|chien','maison')),
+        );
+    }
+
+}
