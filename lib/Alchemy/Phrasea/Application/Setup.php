7d56a617135b1fb16709cb5c6cfd3b8453bda629
Romain Neutron <imprec@gmail.com>
2012-10-02 14:08:54 +0200

    Update setup application

--- lib/Alchemy/Phrasea/Application/Setup.php
+++ lib/Alchemy/Phrasea/Application/Setup.php
@@ -12,17 +12,16 @@
 namespace Alchemy\Phrasea\Application;
 
 use Alchemy\Phrasea\Application as PhraseaApplication;
-use Alchemy\Phrasea\Setup;
 use Alchemy\Phrasea\Controller\Setup\Installer;
 use Alchemy\Phrasea\Controller\Utils\ConnectionTest;
 use Alchemy\Phrasea\Controller\Utils\PathFileTest;
 
 return call_user_func(function() {
 
-    $app = new Setup();
+    $app = new Application();
 
     $app->get('/', function(PhraseaApplication $app) {
-        if($app['phraseanet.configuration']->isInstalled()) {
+        if (!$app['phraseanet.configuration-tester']->isBlank()) {
             return $app->redirect('/login/');
         }
 
@@ -34,12 +33,12 @@ return call_user_func(function() {
     $app->mount('/connection_test', new ConnectionTest());
 
     $app->error(function($e) use ($app) {
-        if ($e instanceof \Exception_Setup_PhraseaAlreadyInstalled) {
-            return $app->redirect('/login/');
-        }
+            if ($e instanceof \Exception_Setup_PhraseaAlreadyInstalled) {
+                return $app->redirect('/login/');
+            }
 
-        return new Response('Internal Server Error', 500, array('X-Status-Code' => 500));
-    });
+            return new Response('Internal Server Error', 500, array('X-Status-Code' => 500));
+        });
 
     return $app;
 });
