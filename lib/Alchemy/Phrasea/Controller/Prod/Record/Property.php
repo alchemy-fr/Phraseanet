d4b4a59839c8d60288ecd9bc79195500a5194fc1
Nicolas Le Goff <legoff.n@gmail.com>
2012-10-03 17:18:42 +0200

    Refactor record property (status, type)
    update property route
    
    Fixed status

--- lib/Alchemy/Phrasea/Controller/Prod/Record/Property.php
+++ lib/Alchemy/Phrasea/Controller/Prod/Record/Property.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Alchemy\Phrasea\Controller\Prod;
+namespace Alchemy\Phrasea\Controller\Prod\Record;
 
 use Alchemy\Phrasea\Controller\RecordsRequest;
 use Silex\Application;
@@ -40,7 +40,10 @@ class Property implements ControllerProviderInterface
             });
 
         $controllers->get('/', $this->call('displayProperty'))
-            ->bind('display-property');
+            ->bind('display_property');
+
+        $controllers->post('/status/', $this->call('changeStatus'))
+            ->bind('change_status');
 
         return $controllers;
     }
@@ -54,44 +57,63 @@ class Property implements ControllerProviderInterface
      */
     public function displayProperty(Application $app, Request $request)
     {
-        $records = RecordsRequest::fromRequest($app, $request);
+        if (!$request->isXmlHttpRequest()) {
+            $app->abort(400);
+        }
+
+        $records = RecordsRequest::fromRequest($app, $request, false, array('chgstatus'));
         $databoxStatus = \databox_status::getDisplayStatus($app);
+        $statusBit = $recordsType = $nRec = $toRemove = array();
 
-        $records->filter(function($record) use ($app, $databoxStatus) {
-                $success = false;
+        foreach ($records as $key => $record) {
+            if (!$app['phraseanet.user']->ACL()->has_hd_grant($record) ||
+                !$app['phraseanet.user']->ACL()->has_preview_grant($record)) {
+                try {
+                    $conn = $record->get_databox()->get_connection();
 
-                if (!$app['phraseanet.user']->ACL()->has_preview_grant($record) ||
-                    !$app['phraseanet.user']->ACL()->has_preview_grant($record)) {
-                    try {
-                        $conn = $record->get_databox()->get_connection();
+                    $sql = sprintf('SELECT record_id FROM record WHERE ((status ^ %s) & %s) = 0 AND record_id = :record_id', $app['phraseanet.user']->ACL()->get_mask_xor($record->get_base_id()), $app['phraseanet.user']->ACL()->get_mask_and($record->get_base_id()));
 
-                        $sql = '
-                        SELECT record_id FROM record WHERE ((status ^ ' . $app['phraseanet.user']->ACL()->get_mask_xor($record->get_base_id()) . ')
-                        & ' . $app['phraseanet.user']->ACL()->get_mask_and($record->get_base_id()) . ')=0' .
-                            ' AND record_id = :record_id';
+                    $stmt = $conn->prepare($sql);
+                    $stmt->execute(array(':record_id' => $record->get_record_id()));
 
-                        $stmt = $conn->prepare($sql);
-                        $stmt->execute(array(':record_id' => $record->get_id()));
-
-                        if (0 < $stmt->rowCount()) {
-                            $success = true;
-                        }
-
-                        $stmt->closeCursor();
-                        unset($stmt);
-                    } catch (Exception $e) {
-
+                    if (0 === $stmt->rowCount()) {
+                        $toRemove[] = $key;
                     }
-                } else {
-                    $success = true;
+
+                    $stmt->closeCursor();
+                    unset($stmt);
+                } catch (Exception $e) {
+                    $toRemove[] = $key;
                 }
+            }
+        }
 
-                return $success;
-            });
+        foreach ($toRemove as $key) {
+            $records->remove($key);
+        }
 
-        $statusBit = array();
         foreach ($records as $record) {
             $sbasId = $record->get_databox()->get_sbas_id();
+
+            if (!isset($nRec[$sbasId])) {
+                $nRec[$sbasId] = array('stories' => 0, 'records' => 0);
+            }
+
+            $nRec[$sbasId]['records']++;
+
+            if ($record->is_grouping()) {
+                $nRec[$sbasId]['stories']++;
+            }
+
+            if (!isset($recordsType[$sbasId])) {
+                $recordsType[$sbasId] = array();
+            }
+
+            if (!isset($recordsType[$sbasId][$record->get_type()])) {
+                $recordsType[$sbasId][$record->get_type()] = array();
+            }
+
+            $recordsType[$sbasId][$record->get_type()] = $record;
 
             if (!isset($statusBit[$sbasId])) {
 
@@ -103,13 +125,71 @@ class Property implements ControllerProviderInterface
             }
 
             $status = strrev($record->get_status());
-            foreach (array_key($statusBit[$sbasId]) as $bit) {
+
+            foreach (array_keys($statusBit[$sbasId]) as $bit) {
                 $statusBit[$sbasId][$bit]["nset"] += substr($status, $bit, 1) !== "0" ? 1 : 0;
             }
         }
 
-        $nbStories = $records->stories()->count();
-        $nbRecords = $records->count();
+        foreach ($records->databoxes() as $databox) {
+            $sbasId = $databox->get_sbas_id();
+            foreach ($statusBit[$sbasId] as $bit => $values) {
+                $statusBit[$sbasId][$bit]["status"] = $values["nset"] == 0 ? 0 : ($values["nset"] == $nRec[$sbasId]['records'] ? 1 : 2);
+            }
+        }
+
+        return $app['twig']->render('prod/actions/Property/index.html.twig', array(
+                'records'     => $records,
+                'statusBit'   => $statusBit,
+                'recordsType' => $recordsType,
+                'nRec'        => $nRec
+            ));
+    }
+
+    public function changeStatus(Application $app, Request $request)
+    {
+        $applyStatusToChildren = $request->request->get('apply_to_children', array());
+        $records = RecordsRequest::fromRequest($app, $request, false, array('chgstatus'));
+        $updated = array();
+        $postStatus = $request->request->get('status');
+
+        foreach ($records as $record) {
+            $sbasId = $record->get_databox()->get_sbas_id();
+
+            //update record
+            $updated[$record->get_serialize_key()] = $this->updateRecordStatus($record, $postStatus);
+
+            //update children if current record is a story
+            if (isset($applyStatusToChildren[$sbasId]) && $record->is_grouping()) {
+                foreach ($record->get_children() as $child) {
+                   $updated[$record->get_serialize_key()] = $this->updateRecordStatus($child, $postStatus);
+                }
+            }
+        }
+
+        return $app->json(array('success' => true, 'updated' => $updated), 201);
+    }
+
+    private function updateRecordStatus(\record_adapter $record, Array $postStatus)
+    {
+        $sbasId = $record->get_databox()->get_sbas_id();
+
+        if (isset($postStatus[$sbasId]) && is_array($postStatus[$sbasId])) {
+            $postStatus = $postStatus[$sbasId];
+            $currentStatus = strrev($record->get_status());
+
+            $newStatus = '';
+            foreach (range(0, 63) as $i) {
+                $newStatus .= isset($postStatus[$i]) ? ($postStatus[$i] ? '1' : '0') : $currentStatus[$i];
+            }
+
+            $record->set_binary_status(strrev($newStatus));
+
+            return array(
+                'current_status' => $currentStatus,
+                'new_status'     => $newStatus
+            );
+        }
     }
 
     /**
