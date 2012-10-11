0d091c3f376e9e32a51b589871392454c87320f3
Nicolas Le Goff <legoff.n@gmail.com>
2012-10-11 14:45:54 +0200

    Fix catch wrong exception

--- lib/Alchemy/Phrasea/Command/BuildMissingSubdefs.php
+++ lib/Alchemy/Phrasea/Command/BuildMissingSubdefs.php
@@ -15,6 +15,7 @@ use Monolog\Handler\StreamHandler;
 use Alchemy\Phrasea\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileNotFoundException;
 
 /**
  * Rebuild only missing subdefs
@@ -64,7 +65,7 @@ class BuildMissingSubdefs extends Command
 
                 try {
                     $record->get_hd_file();
-                } catch (\Exception_Media_SubdefNotFound $e) {
+                } catch (FileNotFoundException $e) {
                     continue;
                 }
 
