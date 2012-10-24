618f81d207a052ad758cac4444b984daea4c456e
Nicolas Le Goff <legoff.n@gmail.com>
2012-10-24 11:56:49 +0200

    Add session test

---
+++ tests/Alchemy/Phrasea/Controller/Root/SessionTest.php
@@ -0,0 +1,70 @@
+<?php
+
+require_once __DIR__ . '/../../../../PhraseanetWebTestCaseAuthenticatedAbstract.class.inc';
+
+class SessionTest extends \PhraseanetWebTestCaseAuthenticatedAbstract
+{
+
+    /**
+     * @covers \Alchemy\Phrasea\Controller\Root\Session::updateSession
+     */
+    public function testUpdSessionLogout()
+    {
+        $this->logout(self::$DI['app']);
+        $this->XMLHTTPRequest('POST', '/session/update/');
+        $this->assertTrue(self::$DI['client']->getResponse()->isOk());
+        $datas = json_decode(self::$DI['client']->getResponse()->getContent());
+        $this->checkSessionReturn($datas);
+        $this->assertEquals('disconnected', $datas->status);
+    }
+
+    /**
+     * @covers \Alchemy\Phrasea\Controller\Root\Session::updateSession
+     */
+    public function testUpdSessionChangeUser()
+    {
+        $this->XMLHTTPRequest('POST', '/session/update/', array(
+            'usr' => self::$DI['user_alt1']->get_id()
+        ));
+        $this->assertTrue(self::$DI['client']->getResponse()->isOk());
+        $datas = json_decode(self::$DI['client']->getResponse()->getContent());
+        $this->checkSessionReturn($datas);
+        $this->assertEquals('disconnected', $datas->status);
+    }
+
+    /**
+     * @covers \Alchemy\Phrasea\Controller\Root\Session::updateSession
+     */
+    public function testUpdSession()
+    {
+        $auth = new \Session_Authentication_None(self::$DI['user']);
+        self::$DI['app']->openAccount($auth);
+
+        $this->XMLHTTPRequest('POST', '/session/update/', array(
+            'usr' => self::$DI['user']->get_id()
+        ));
+        $this->assertTrue(self::$DI['client']->getResponse()->isOk());
+        $datas = json_decode(self::$DI['client']->getResponse()->getContent());
+        $this->checkSessionReturn($datas);
+        $this->assertEquals('ok', $datas->status);
+    }
+
+     /**
+     * @covers \Alchemy\Phrasea\Controller\Root\Session::updateSession
+     * @expectedException Symfony\Component\HttpKernel\Exception\HttpException
+     */
+    public function testUpdSessionBadRequest()
+    {
+        self::$DI['client']->request('POST', '/session/update/');
+    }
+
+    private function checkSessionReturn(\stdClass $data)
+    {
+        var_dump($data);
+        $this->assertObjectHasAttribute('status', $data);
+        $this->assertObjectHasAttribute('message', $data);
+        $this->assertObjectHasAttribute('notifications', $data);
+        $this->assertObjectHasAttribute('changed', $data);
+    }
+
+}
