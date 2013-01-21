842ad40985f12381e896e981c229304443e6b7cc
Romain Neutron <imprec@gmail.com>
2013-01-21 23:28:58 +0100

    Bump copyright year

--- lib/classes/task/period/ftpPull.php
+++ lib/classes/task/period/ftpPull.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of Phraseanet
  *
- * (c) 2005-2012 Alchemy
+ * (c) 2005-2013 Alchemy
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
