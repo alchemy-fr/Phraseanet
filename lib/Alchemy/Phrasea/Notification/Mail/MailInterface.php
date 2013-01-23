ed80a230b464a6c9e0038ce3c9ee27a5464d7813
Romain Neutron <imprec@gmail.com>
2013-01-23 11:08:21 +0100

    Add Interfaces doc blocks

--- lib/Alchemy/Phrasea/Notification/Mail/MailInterface.php
+++ lib/Alchemy/Phrasea/Notification/Mail/MailInterface.php
@@ -1,23 +1,98 @@
 <?php
 
+/*
+ * This file is part of Phraseanet
+ *
+ * (c) 2005-2013 Alchemy
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Alchemy\Phrasea\Notification\Mail;
 
-use Alchemy\Phrasea\Notification\Emitter;
-use Alchemy\Phrasea\Notification\Receiver;
+use Alchemy\Phrasea\Notification\EmitterInterface;
+use Alchemy\Phrasea\Notification\ReceiverInterface;
 
 interface MailInterface
 {
     /**
-     * @return Emitter
+     * @return EmitterInterface
      */
-    public function emitter();
+    public function getEmitter();
+
     /**
-     * @return Receiver
+     * @return ReceiverInterface
      */
-    public function receiver();
-    public function subject();
+    public function getReceiver();
 
-    public function message();
+    /**
+     * Gets the message for the link button
+     *
+     * @return string
+     */
+    public function getSubject();
 
+    /**
+     * Returns a text version of the e-mail
+     *
+     * @return string
+     */
+    public function getMessage();
+
+    /**
+     * Returns an HTML version of the e-mail
+     *
+     * @return string
+     */
     public function renderHTML();
+
+    /**
+     * Returns the title of the Phraseanet Install
+     *
+     * @return string
+     */
+    public function getPhraseanetTitle();
+
+    /**
+     * Returns the absolute URL to Phraseanet Install
+     *
+     * @return string
+     */
+    public function getPhraseanetURL();
+
+    /**
+     * Returns an URL for a logo
+     *
+     * @return string
+     */
+    public function getLogoUrl();
+
+    /**
+     * Returns a alternate text for the logo
+     *
+     * @return string
+     */
+    public function getLogoText();
+
+    /**
+     * Gets an expiration date for the meaning of the message
+     *
+     * @return \DateTime
+     */
+    public function getExpirationMessage();
+
+    /**
+     * Gets the message for the link button
+     *
+     * @return string
+     */
+    public function getButtonText();
+
+    /**
+     * Sets a URL for the link button
+     *
+     * @return MailInterface
+     */
+    public function getButtonURL();
 }
