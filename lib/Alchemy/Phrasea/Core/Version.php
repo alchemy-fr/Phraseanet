c9181ccff3af3b8c08a4557719a27774f10e06d2
Romain Neutron <imprec@gmail.com>
2013-02-01 15:18:22 +0100

    Bump to version 3.7.6

--- lib/Alchemy/Phrasea/Core/Version.php
+++ lib/Alchemy/Phrasea/Core/Version.php
@@ -18,7 +18,7 @@ namespace Alchemy\Phrasea\Core;
  */
 class Version
 {
-    protected static $number = '3.7.5';
+    protected static $number = '3.7.6';
     protected static $name = 'Carnosaurus';
 
     public static function getNumber()
