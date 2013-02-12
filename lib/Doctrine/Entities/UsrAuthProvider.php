832a5d2f465de0a53923ab770a099c6bcc347d77
Romain Neutron <imprec@gmail.com>
2013-02-12 14:16:58 +0100

    Add new AUsrAuthProvider entity

---
+++ lib/Doctrine/Entities/UsrAuthProvider.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * UsrAuthProvider
+ */
+class UsrAuthProvider
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var integer
+     */
+    private $usr_id;
+
+    /**
+     * @var string
+     */
+    private $provider;
+
+    /**
+     * @var \DateTime
+     */
+    private $updated;
+
+    /**
+     * @var \DateTime
+     */
+    private $created;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set usr_id
+     *
+     * @param integer $usrId
+     * @return UsrAuthProvider
+     */
+    public function setUsrId($usrId)
+    {
+        $this->usr_id = $usrId;
+    
+        return $this;
+    }
+
+    /**
+     * Get usr_id
+     *
+     * @return integer 
+     */
+    public function getUsrId()
+    {
+        return $this->usr_id;
+    }
+
+    /**
+     * Set provider
+     *
+     * @param string $provider
+     * @return UsrAuthProvider
+     */
+    public function setProvider($provider)
+    {
+        $this->provider = $provider;
+    
+        return $this;
+    }
+
+    /**
+     * Get provider
+     *
+     * @return string 
+     */
+    public function getProvider()
+    {
+        return $this->provider;
+    }
+
+    /**
+     * Set updated
+     *
+     * @param \DateTime $updated
+     * @return UsrAuthProvider
+     */
+    public function setUpdated($updated)
+    {
+        $this->updated = $updated;
+    
+        return $this;
+    }
+
+    /**
+     * Get updated
+     *
+     * @return \DateTime 
+     */
+    public function getUpdated()
+    {
+        return $this->updated;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     * @return UsrAuthProvider
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+    
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime 
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+}
