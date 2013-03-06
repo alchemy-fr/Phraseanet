f406771b757f9c81435b2cd9258982715f8b074a
Nicolas Le Goff <legoff.n@gmail.com>
2013-03-06 17:55:22 +0100

    Add Phrasea Renew Password Form

---
+++ lib/Alchemy/Phrasea/Form/Login/PhraseaRenewPasswordForm.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of Phraseanet
+ *
+ * (c) 2005-2013 Alchemy
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Alchemy\Phrasea\Form\Login;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class PhraseaRenewPasswordForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('password', 'password', array(
+            'label' => _('Password'),
+            'required' => true,
+            'constraints' => array(
+                new Assert\NotBlank()
+            )
+        ));
+
+        $builder->add('passwordConfirm', 'password', array(
+            'label' => _('Password'),
+            'required' => true,
+            'constraints' => array(
+                new Assert\NotBlank()
+            )
+        ));
+    }
+
+    public function getName()
+    {
+        return null;
+    }
+}
