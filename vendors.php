43595489d71c6c0dd44559c8ce00c224de3b41c1
Romain Neutron <imprec@gmail.com>
2013-03-06 10:33:51 +0100

    Update vendors script

--- vendors.php
+++ vendors.php
@@ -59,15 +59,5 @@ if (isset($argv[1]) && $argv[1] == '--no-dev') {
     system($composer . ' install --dev --optimize-autoloader');
 }
 
-system('git submodule init');
-system('git submodule update');
+system('bin/setup less:compile');
 
-$iterator = new RecursiveDirectoryIterator(__DIR__ . '/lib/vendor/');
-
-foreach ($iterator as $file) {
-    /* @var $file SplFileInfo */
-    if ($file->isDir()) {
-        $cmd = sprintf('cd %s && git submodule init && git submodule update', escapeshellarg($file->getPathname()));
-        system($cmd);
-    }
-}
