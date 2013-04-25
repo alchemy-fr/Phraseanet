be66d73774aff73d50cb499af3b55a743b63a6a1
Romain Neutron <imprec@gmail.com>
2013-04-25 12:54:08 +0200

    Prepare version 3.7.11

--- lib/Alchemy/Phrasea/Core/Version.php
+++ lib/Alchemy/Phrasea/Core/Version.php
@@ -18,7 +18,7 @@ namespace Alchemy\Phrasea\Core;
  */
 class Version
 {
-    protected static $number = '3.7.10';
+    protected static $number = '3.7.11';
     protected static $name = 'Carnosaurus';
 
     public static function getNumber()
