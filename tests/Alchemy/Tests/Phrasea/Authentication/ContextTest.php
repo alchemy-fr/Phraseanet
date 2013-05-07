a3634bd09606ae31fddfe48cf171661fca7c5bd9
Romain Neutron <imprec@gmail.com>
2013-05-07 17:39:36 +0200

    Add authentication Context

---
+++ tests/Alchemy/Tests/Phrasea/Authentication/ContextTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Alchemy\Tests\Phrasea\Authentication;
+
+use Alchemy\Phrasea\Authentication\Context;
+
+class ContextTest extends \PHPUnit_Framework_TestCase
+{
+    public function testWithValidCOntext()
+    {
+        $contextValue = Context::CONTEXT_GUEST;
+        $context = new Context($contextValue);
+
+        $this->assertEquals($contextValue, $context->getContext());
+        $context->setContext(Context::CONTEXT_OAUTH2_NATIVE);
+        $this->assertEquals(Context::CONTEXT_OAUTH2_NATIVE, $context->getContext());
+    }
+
+    /**
+     * @expectedException Alchemy\Phrasea\Exception\InvalidArgumentException
+     */
+    public function testWithInvalidCOntext()
+    {
+        new Context('No context like this');
+    }
+}
