765b043fa8df3d407f64ce57cae3287fa14b5fb1
Romain Neutron <imprec@gmail.com>
2013-05-30 13:09:11 +0200

    Update autoloader path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 
-$loader = require __DIR__ . '/../vendor/autoload.php';
+$loader = require __DIR__ . '/../lib/autoload.php';
 $loader->add('Alchemy\\Tests', __DIR__);
 $loader->add('', __DIR__ . "/classes");
