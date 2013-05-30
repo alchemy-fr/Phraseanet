c2f7a5685e806b83803c5ebf6d9565728cedef4c
Andrey <andrey.kalinovsky@gmail.com>
2013-05-30 18:55:53 +0200

    Add token entities and repositories

---
+++ lib/Doctrine/Entities/AggregateToken.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * AggregateToken
+ */
+class AggregateToken
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var integer
+     */
+    private $usr_id;
+
+    /**
+     * @var string
+     */
+    private $value;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set usr_id
+     *
+     * @param integer $usrId
+     * @return AggregateToken
+     */
+    public function setUsrId($usrId)
+    {
+        $this->usr_id = $usrId;
+    
+        return $this;
+    }
+
+    /**
+     * Get usr_id
+     *
+     * @return integer 
+     */
+    public function getUsrId()
+    {
+        return $this->usr_id;
+    }
+
+    /**
+     * Set value
+     *
+     * @param string $value
+     * @return AggregateToken
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+    
+        return $this;
+    }
+
+    /**
+     * Get value
+     *
+     * @return string 
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
