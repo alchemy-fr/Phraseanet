b0ede4bb118395658ddce1f5f54b7cce5a702191
Romain Neutron <imprec@gmail.com>
2013-06-11 18:39:22 +0200

    Add command to console

--- lib/Alchemy/Phrasea/Command/Compile/Configuration.php
+++ lib/Alchemy/Phrasea/Command/Compile/Configuration.php
@@ -20,12 +20,14 @@ class Configuration extends Command
     public function __construct()
     {
         parent::__construct('compile:configuration');
+        $this->setDescription('Compile configuration');
     }
 
     protected function doExecute(InputInterface $input, OutputInterface $output)
     {
         $this->container['phraseanet.configuration']->compileAndWrite();
-
+        $output->writeln("Confguration compiled.");
+        
         return 0;
     }
 }
