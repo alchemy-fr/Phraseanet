09c6361475220ea1874a885ca05a2811c98431b5
Romain Neutron <imprec@gmail.com>
2013-06-13 20:09:57 +0200

    Add trusted proxies configuration

--- tests/Alchemy/Tests/Phrasea/Core/Provider/ConfigurationServiceProviderTest.php
+++ tests/Alchemy/Tests/Phrasea/Core/Provider/ConfigurationServiceProviderTest.php
@@ -2,6 +2,10 @@
 
 namespace Alchemy\Tests\Phrasea\Core\Provider;
 
+use Alchemy\Phrasea\Core\Provider\ConfigurationServiceProvider;
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+
 /**
  * @covers Alchemy\Phrasea\Core\Provider\ConfigurationServiceProvider
  */
@@ -16,5 +20,19 @@ class ConfigurationServiceProvidertest extends ServiceProviderTestCase
                 'Alchemy\\Phrasea\\Core\\Configuration\\Configuration'
             ),
         );
+    }
+
+    public function testRequestTrustedProxiesAreSetOnBoot()
+    {
+        $app = new Application();
+        $app['root.path'] = __DIR__ . '/../../../../../..';
+        $app->register(new ConfigurationServiceProvider());
+        $app['phraseanet.configuration.config-path'] = __DIR__ . '/fixtures/config-proxies.yml';
+        $app['phraseanet.configuration.config-compiled-path'] = __DIR__ . '/fixtures/config-proxies.php';
+        $this->assertSame(array(), Request::getTrustedProxies());
+        $app->boot();
+        $this->assertSame(array('127.0.0.1', '66.6.66.6'), Request::getTrustedProxies());
+
+        unlink($app['phraseanet.configuration.config-compiled-path']);
     }
 }
