4ed16a8f75a4db0c11651afef3e32c03308f7b8c
Nicolas Le Goff <legoff.n@gmail.com>
2013-06-14 20:12:43 +0200

    Use html5 to validate setup form, use html5 fallback if client does not support this feature
    Add modernizr to admin group

--- lib/conf.d/minifyGroupsConfig.php
+++ lib/conf.d/minifyGroupsConfig.php
@@ -35,7 +35,8 @@ $groups = array(
         , '//include/jquery.image_enhancer.js'
         , '//include/jslibs/jquery.contextmenu_scroll.js'),
     'admin' => array(
-        '//include/jslibs/jquery.validate.js'
+        '//assets/modernizr/modernizr.js'
+        ,'//include/jslibs/jquery.validate.js'
         , '//include/jslibs/jquery.validate.password.js'
         , '//include/jslibs/jquery-ui-1.8.24.js'
         , '//include/jslibs/jquery.cookie.js'
