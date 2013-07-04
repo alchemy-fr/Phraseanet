d78a239d869ec4b573208297b5d2ff385218d0db
Romain Neutron <imprec@gmail.com>
2013-07-04 14:10:33 +0200

    Bump to alpha 15

---
+++ lib/classes/patch/3815.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of Phraseanet
+ *
+ * (c) 2005-2012 Alchemy
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Alchemy\Phrasea\Application;
+
+class patch_3815 implements patchInterface
+{
+    /** @var string */
+    private $release = '3.8.0.a15';
+
+    /** @var array */
+    private $concern = array(base::APPLICATION_BOX);
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get_release()
+    {
+        return $this->release;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function require_all_upgrades()
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function concern()
+    {
+        return $this->concern;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply(base $appbox, Application $app)
+    {
+        $binaries = $app['phraseanet.configuration']['binaries'];
+        unset($binaries['composite_binary'], $binaries['convert_binary']);
+        $app['phraseanet.configuration']['binaries'] = $binaries;
+
+        return true;
+    }
+}
