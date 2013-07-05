870c832fb55d2581c8ed7365746aa3ce6b89434c
Nicolas Le Goff <legoff.n@gmail.com>
2013-07-05 18:32:28 +0200

    Remove register_enable function from login class

--- lib/classes/login.php
+++ lib/classes/login.php
@@ -9,21 +9,4 @@ class login
     {
         return databox_cgu::getHome($app);
     }
-
-    public function register_enabled(Application $app)
-    {
-        require_once __DIR__ . '/deprecated/inscript.api.php';
-
-        $bases = giveMeBases($app);
-
-        if ($bases) {
-            foreach ($bases as $base) {
-                if ($base['inscript']) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
 }
