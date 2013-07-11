f64d662a9d60e0ccc227c50efc3371f739b584b5
Romain Neutron <imprec@gmail.com>
2013-07-11 11:59:36 +0200

    Add console APIs tests

---
+++ tests/Alchemy/Tests/Phrasea/Application/ConsoleAPITest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Alchemy\Tests\Phrasea\Application;
+
+use Symfony\Component\Process\Process;
+
+class ConsoleAPITest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider provideConsoleNames
+     */
+    public function testThatCommandsExitWithZero($console)
+    {
+        $process = new Process(__DIR__ . '/../../../../../bin/'.$console);
+        $process->run();
+
+        $this->assertSame(0, $process->getExitCode());
+    }
+
+    public function provideConsoleNames()
+    {
+        return array(
+            array('console'),
+            array('setup'),
+        );
+    }
+}
