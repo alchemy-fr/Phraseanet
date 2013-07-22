77791fd39a51cd1669c04eedc12a9b8a6a35c436
Romain Neutron <imprec@gmail.com>
2013-07-22 18:51:35 +0200

    Exit script in case of failure

--- build-env.sh
+++ build-env.sh
@@ -1,11 +1,10 @@
-rm -f config/configuration.yml config/services.yml config/connexions.yml config/config.yml config/config.inc config/connexion.inc config/_GV.php config/_GV.php.old
-cp -f hudson/connexion.inc config/
-cp -f hudson/_GV.php config/
-./bin/developer dependencies:all
-sudo mysql -e 'drop database ab_test;drop database db_test; drop database ab_unitTests; drop database db_unitTests;'
-sudo mysql -e 'create database ab_test;create database db_test; create database ab_unitTests; create database db_unitTests;'
-sudo mysql -e "GRANT ALL PRIVILEGES ON ab_unitTests.* TO 'phraseaUnitTests'@'localhost' IDENTIFIED BY 'iWvGxPE8' WITH GRANT OPTION"
-sudo mysql -e "GRANT ALL PRIVILEGES ON db_unitTests.* TO 'phraseaUnitTests'@'localhost' IDENTIFIED BY 'iWvGxPE8' WITH GRANT OPTION"
-
-sudo mysql -e "source `pwd`/hudson/fixtures.sql"
-sudo mysql -e 'SET @@global.sql_mode= "";'
+rm -f config/configuration.yml config/services.yml config/connexions.yml config/config.yml config/config.inc config/connexion.inc config/_GV.php config/_GV.php.old || exit 1
+cp -f hudson/connexion.inc config/ || exit 1
+cp -f hudson/_GV.php config/ || exit 1
+./bin/developer dependencies:all || exit 1
+sudo mysql -e 'drop database ab_test;drop database db_test; drop database ab_unitTests; drop database db_unitTests;' || exit 1
+sudo mysql -e 'create database ab_test;create database db_test; create database ab_unitTests; create database db_unitTests;' || exit 1
+sudo mysql -e "GRANT ALL PRIVILEGES ON ab_unitTests.* TO 'phraseaUnitTests'@'localhost' IDENTIFIED BY 'iWvGxPE8' WITH GRANT OPTION" || exit 1
+sudo mysql -e "GRANT ALL PRIVILEGES ON db_unitTests.* TO 'phraseaUnitTests'@'localhost' IDENTIFIED BY 'iWvGxPE8' WITH GRANT OPTION" || exit 1
+sudo mysql -e "source `pwd`/hudson/fixtures.sql" || exit 1
+sudo mysql -e 'SET @@global.sql_mode= "";' || exit 1
