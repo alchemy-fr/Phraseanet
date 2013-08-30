7f20905b8e533f179ed4485f287d363a3cc5388f
Romain Neutron <imprec@gmail.com>
2013-08-30 17:39:24 +0200

    Cleanup Job on failure

--- lib/Alchemy/Phrasea/TaskManager/Job/AbstractJob.php
+++ lib/Alchemy/Phrasea/TaskManager/Job/AbstractJob.php
@@ -54,11 +54,20 @@ abstract class AbstractJob extends AbstractTMJob implements JobInterface
     final protected function doRun(JobDataInterface $data = null)
     {
         if (!$data instanceof JobData) {
+            // removes lock files, unregister tick handlers
+            $this->finish();
             throw new InvalidArgumentException('JobData must be passed to a JobInterface::Run command.');
         }
 
         $this->setPauseDuration($data->getTask()->getPeriod());
-        $this->doJob($data);
+
+        try {
+            $this->doJob($data);
+        } catch (\Exception $e) {
+            $this->finish();
+            $this->log('error', sprintf('Error while running %s : %s', $this->getName(), $e->getMessage()));
+            throw $e;
+        }
     }
 
     /**
