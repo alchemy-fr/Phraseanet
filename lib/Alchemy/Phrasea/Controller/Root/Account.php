41bbd13f7d5fb3da003ae2db22537a38e9b470f6
Nicolas Le Goff <legoff.n@gmail.com>
2013-09-03 18:03:43 +0200

    Fix #1432 User settings for notifications are not saved in

--- lib/Alchemy/Phrasea/Controller/Root/Account.php
+++ lib/Alchemy/Phrasea/Controller/Root/Account.php
@@ -425,7 +425,7 @@ class Account implements ControllerProviderInterface
         foreach ($app['events-manager']->list_notifications_available($app['authentication']->getUser()->get_id()) as $notifications) {
             foreach ($notifications as $notification) {
                 $notifId = $notification['id'];
-                $notifName = sprintf('notification_%d', $notifId);
+                $notifName = sprintf('notification_%s', $notifId);
 
                 if (isset($requestedNotifications[$notifId])) {
                     $app['authentication']->getUser()->setPrefs($notifName, '1');
