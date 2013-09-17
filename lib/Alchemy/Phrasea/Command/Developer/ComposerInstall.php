91dd9ce321dc9392f518eaaf8569b2d3ab68c81b
Romain Neutron <imprec@gmail.com>
2013-09-17 11:27:23 +0200

    Disable composer interactivity

--- lib/Alchemy/Phrasea/Command/Developer/ComposerInstall.php
+++ lib/Alchemy/Phrasea/Command/Developer/ComposerInstall.php
@@ -38,7 +38,7 @@ class ComposerInstall extends Command
         $composer->command('self-update');
         $output->writeln("<info>OK</info>");
 
-        $commands = array('install', '--optimize-autoloader');
+        $commands = array('install', '--optimize-autoloader', '--quiet', '--no-interaction');
         if ($input->getOption('prefer-source')) {
             $commands[] = '--prefer-source';
         }
