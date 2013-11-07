f8411a604e2c971db8e9542540f62c4d8ea9cae7
Romain Neutron <imprec@gmail.com>
2013-11-07 15:37:58 +0100

    Fix patch naming mess

---
+++ lib/classes/patch/380alpha18a.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * This file is part of Phraseanet
+ *
+ * (c) 2005-2012 Alchemy
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Alchemy\Phrasea\Application;
+use Symfony\Component\Process\ExecutableFinder;
+
+class patch_380alpha18a implements patchInterface
+{
+    /** @var string */
+    private $release = '3.8.0-alpha.18';
+
+    /** @var array */
+    private $concern = array(base::APPLICATION_BOX);
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get_release()
+    {
+        return $this->release;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function require_all_upgrades()
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function concern()
+    {
+        return $this->concern;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply(base $appbox, Application $app)
+    {
+        $finder = new ExecutableFinder();
+
+        $binaries = $app['phraseanet.configuration']['binaries'];
+        $binaries['recess_binary'] = $finder->find('recess');
+        $app['phraseanet.configuration']['binaries'] = $binaries;
+
+        return true;
+    }
+}
---
+++ lib/classes/patch/380alpha2a.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * This file is part of Phraseanet
+ *
+ * (c) 2005-2013 Alchemy
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Alchemy\Phrasea\Application;
+
+/**
+ *
+ * @license     http://opensource.org/licenses/gpl-3.0 GPLv3
+ * @link        www.phraseanet.com
+ */
+class patch_380alpha2a implements patchInterface
+{
+    /**
+     *
+     * @var string
+     */
+    private $release = '3.8.0-alpha.2';
+
+    /**
+     *
+     * @var Array
+     */
+    private $concern = array(base::APPLICATION_BOX);
+
+    /**
+     *
+     * @return string
+     */
+    public function get_release()
+    {
+        return $this->release;
+    }
+
+    public function require_all_upgrades()
+    {
+        return false;
+    }
+
+    /**
+     *
+     * @return Array
+     */
+    public function concern()
+    {
+        return $this->concern;
+    }
+
+    /**
+     * @param base        $databox
+     * @param Application $app
+     */
+    public function apply(base $databox, Application $app)
+    {
+        $config = $app['phraseanet.configuration']->getConfig();
+
+        if (isset($config['main']['database-test'])) {
+            $config['main']['database-test']['path'] = '/tmp/db.sqlite';
+        }
+
+        $app['phraseanet.configuration']->setConfig($config);
+    }
+}
