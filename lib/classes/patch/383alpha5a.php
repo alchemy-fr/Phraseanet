1456d92dbd1944cec687acec222c66dfb12a232c
Nicolas Le Goff <legoff.n@gmail.com>
2014-02-04 13:44:38 +0100

    Fix latest merge

--- lib/classes/patch/383alpha5a.php
+++ lib/classes/patch/383alpha5a.php
@@ -30,6 +30,14 @@ class patch_383alpha5a implements patchInterface
     /**
      * {@inheritdoc}
      */
+    public function getDoctrineMigrations()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function require_all_upgrades()
     {
         return false;
