965a685236233e60ea6e890e48a2b868fd51e569
Romain Neutron <imprec@gmail.com>
2014-02-26 16:31:52 +0100

    Fix namespace

--- lib/Alchemy/Phrasea/SearchEngine/SphinxSearch/SphinxSearchEngineSubscriber.php
+++ lib/Alchemy/Phrasea/SearchEngine/SphinxSearch/SphinxSearchEngineSubscriber.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Alchemy\Phrasea\SearchEngine\Phrasea;
+namespace Alchemy\Phrasea\SearchEngine\SphinxSearch;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
