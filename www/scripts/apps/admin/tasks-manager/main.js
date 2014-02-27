5216e8fc9cf8f9da15da86bd9efc3299a4301753
Nicolas Le Goff <legoff.n@gmail.com>
2014-02-27 14:24:13 +0100

    Remove unnecessary dependencies

--- www/scripts/apps/admin/tasks-manager/main.js
+++ www/scripts/apps/admin/tasks-manager/main.js
@@ -12,17 +12,8 @@ require.config({
     baseUrl: "/scripts",
     paths: {
         jquery: "../assets/jquery/jquery",
-        jqueryui: "../assets/jquery.ui/jquery-ui",
         underscore: "../assets/underscore-amd/underscore",
-        backbone: "../assets/backbone-amd/backbone",
-        i18n: "../assets/i18next/i18next.amd-1.6.3",
-        bootstrap: "../assets/bootstrap/js/bootstrap.min"
-    },
-    shim: {
-        bootstrap: ["jquery"],
-        jqueryui: {
-            deps: [ "jquery" ]
-        }
+        backbone: "../assets/backbone-amd/backbone"
     }
 });
 
