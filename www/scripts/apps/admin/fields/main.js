029925009761e7d1f485900ae5d7e9a0c2ecc44a
Nicolas Le Goff <legoff.n@gmail.com>
2014-02-28 11:06:28 +0100

    Refactor require.js config

--- www/scripts/apps/admin/fields/main.js
+++ www/scripts/apps/admin/fields/main.js
@@ -7,25 +7,6 @@
  * file that was distributed with this source code.
  */
 
-// configure AMD loading
-require.config({
-    baseUrl: "/scripts",
-    paths: {
-        jquery: "../assets/jquery/jquery",
-        jqueryui: "../assets/jquery.ui/jquery-ui",
-        underscore: "../assets/underscore-amd/underscore",
-        backbone: "../assets/backbone-amd/backbone",
-        i18n: "../assets/i18next/i18next.amd-1.6.3",
-        bootstrap: "../assets/bootstrap/js/bootstrap.min"
-    },
-    shim: {
-        bootstrap: ["jquery"],
-        jqueryui: {
-            deps: [ "jquery" ]
-        }
-    }
-});
-
 // launch application
 require(["apps/admin/fields/app"], function (App) {
     App.initialize();
--- www/scripts/apps/admin/main/main.js
+++ www/scripts/apps/admin/main/main.js
@@ -7,47 +7,6 @@
  * file that was distributed with this source code.
  */
 
-// configure AMD loading
-require.config({
-    baseUrl: "/scripts",
-    paths: {
-        jquery: "../assets/jquery/jquery",
-        "jquery.ui": "../assets/jquery.ui/jquery-ui",
-        underscore: "../assets/underscore-amd/underscore",
-        backbone: "../assets/backbone-amd/backbone",
-        "jquery.ui.widget": "../assets/jquery-file-upload/jquery.ui.widget",
-        "jquery.cookie": "../assets/jquery.cookie/jquery.cookie",
-        "jquery.treeview": "../assets/jquery.treeview/jquery.treeview",
-        "jquery.tooltip": "../assets/jquery.tooltip/jquery.tooltip",
-        "blueimp.loadimage" : "../assets/blueimp-load-image/load-image",
-        "jfu.iframe-transport": "../assets/jquery-file-upload/jquery.iframe-transport",
-        "jfu.fileupload": "../assets/jquery-file-upload/jquery.fileupload"
-    },
-    shim: {
-        "jquery.treeview": {
-            deps: ['jquery', 'jquery.cookie'],
-            exports: '$.fn.treeview'
-        },
-        "jquery.cookie": {
-            deps: ["jquery"],
-            exports: '$.fn.cookie'
-        },
-        "jquery.tooltip": {
-            deps: ["jquery"],
-            exports: '$.fn.tooltip'
-        },
-        "jquery.ui": {
-            deps: ["jquery"]
-        },
-        "jquery.ui.widget": {
-            deps: ["jquery"]
-        },
-        "jfu.fileupload": {
-            deps: ["jquery.ui.widget"]
-        }
-    }
-});
-
 // launch application
 require(["jquery", "apps/admin/main/app"], function ($, App) {
     App.initialize({
