536e54fed766a2e5e0efc3092a9a995f903d4c33
Nicolas Le Goff <legoff.n@gmail.com>
2014-03-05 15:25:21 +0100

    Add ApiOauthCode Manipulator

---
+++ lib/Alchemy/Phrasea/Model/Manipulator/ApiOauthCodeManipulator.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * This file is part of Phraseanet
+ *
+ * (c) 2005-2014 Alchemy
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Alchemy\Phrasea\Model\Manipulator;
+
+use Alchemy\Phrasea\Application;
+use Alchemy\Phrasea\Authentication\ACLProvider;
+use Alchemy\Phrasea\Model\Entities\ApiAccount;
+use Alchemy\Phrasea\Model\Entities\ApiOauthCode;
+use Alchemy\Phrasea\Model\Entities\User;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityRepository;
+use RandomLib\Generator;
+
+class ApiOauthCodeManipulator implements ManipulatorInterface
+{
+    private $om;
+    private $repository;
+    private $randomGenerator;
+
+    public function __construct(ObjectManager $om, EntityRepository $repo, Generator $random)
+    {
+        $this->om = $om;
+        $this->repository = $repo;
+        $this->randomGenerator = $random;
+    }
+
+    public function create(ApiAccount $account, $redirectUri, \DateTime $expire = null, $scope = null)
+    {
+        $code = new ApiOauthCode();
+
+        $code->setCode($this->getNewCode());
+        $code->setRedirectUri($redirectUri);
+        $code->setAccount($account);
+        $code->setExpires($expire);
+        $code->setScope($scope);
+
+        $this->update($code);
+
+        return $code;
+    }
+
+    public function delete(ApiOauthCode $code)
+    {
+        $this->om->remove($code);
+        $this->om->flush();
+    }
+
+    public function update(ApiOauthCode $code)
+    {
+        $this->om->persist($code);
+        $this->om->flush();
+    }
+
+    private function getNewCode()
+    {
+        do {
+            $code = $this->randomGenerator->generateString(16, \random::LETTERS_AND_NUMBERS);
+        } while (null !== $this->repository->find($code));
+
+        return $code;
+    }
+}
