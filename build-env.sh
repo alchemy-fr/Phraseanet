d95c78a0696edf324040bb09d2c5bf0b72191a67
Nicolas Le Goff <legoff.n@gmail.com>
2014-03-17 00:40:25 +0100

    Use sudo to install npm package globally

--- build-env.sh
+++ build-env.sh
@@ -2,7 +2,7 @@ composer install --dev
 ./bin/developer system:uninstall || exit 1
 cp -f hudson/connexion.inc config/ || exit 1
 cp -f hudson/_GV.php config/ || exit 1
-npm install -g uglify-js recess grunt-cli jake
+sudo npm install -g uglify-js recess grunt-cli jake
 npm install
 if [ "$1" != "--no-dependencies" ]
 then
@@ -10,8 +10,16 @@ then
 else
 echo "Dependencies retrieval discarded"
 fi
-sudo mysql -e 'drop database update39_test;drop database ab_test;drop database db_test; drop database ab_unitTests; drop database db_unitTests;' || exit 1
-sudo mysql -e 'create database update39_test;create database ab_test;create database db_test; create database ab_unitTests; create database db_unitTests;' || exit 1
+sudo mysql -e 'drop database update39_test;'
+sudo mysql -e 'drop database ab_test;'
+sudo mysql -e 'drop database db_test;'
+sudo mysql -e 'drop database ab_unitTests;'
+sudo mysql -e 'drop database db_unitTests;'
+sudo mysql -e 'create database update39_test;'
+sudo mysql -e 'create database ab_test;'
+sudo mysql -e 'create database db_test;'
+sudo mysql -e 'create database ab_unitTests;'
+sudo mysql -e 'create database db_unitTests;'
 sudo mysql -e "GRANT ALL PRIVILEGES ON ab_unitTests.* TO 'phraseaUnitTests'@'localhost' IDENTIFIED BY 'iWvGxPE8' WITH GRANT OPTION" || exit 1
 sudo mysql -e "GRANT ALL PRIVILEGES ON db_unitTests.* TO 'phraseaUnitTests'@'localhost' IDENTIFIED BY 'iWvGxPE8' WITH GRANT OPTION" || exit 1
 sudo mysql -e "source `pwd`/hudson/fixtures.sql" || exit 1
