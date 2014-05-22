ab5f7d449263a77faf3252da47e8399bad948d8c
Nicolas Le Goff <legoff.n@gmail.com>
2014-05-22 14:48:58 +0200

    Fix PHRAS-123 Fix tooltip content display

--- lib/Alchemy/Phrasea/Controller/Prod/Tooltip.php
+++ lib/Alchemy/Phrasea/Controller/Prod/Tooltip.php
@@ -103,12 +103,10 @@ class Tooltip implements ControllerProviderInterface
 
     public function displayPreview(Application $app, $sbas_id, $record_id)
     {
-        $record = new \record_adapter($app, $sbas_id, $record_id);
-
-        return $app['twig']->render(
-                'prod/Tooltip/Preview.html.twig'
-                , array('record'      => $record, 'not_wrapped' => true)
-        );
+        return $app['twig']->render('prod/Tooltip/Preview.html.twig', array(
+            'record' => new \record_adapter($app, $sbas_id, $record_id),
+            'not_wrapped' => true
+        ));
     }
 
     public function displayCaption(Application $app, $sbas_id, $record_id, $context)
