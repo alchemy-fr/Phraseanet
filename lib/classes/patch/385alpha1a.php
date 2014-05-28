4dcd0b85fb86fa11c8b2f36bc45d2f012fddcce0
Nicolas Le Goff <legoff.n@gmail.com>
2014-05-28 12:25:03 +0200

    Add patch

---
+++ lib/classes/patch/385alpha1a.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ * This file is part of Phraseanet
+ *
+ * (c) 2005-2014 Alchemy
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Alchemy\Phrasea\Application;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Filesystem\Filesystem;
+
+class patch_385alpha1a implements patchInterface
+{
+    /** @var string */
+    private $release = '3.8.5-alpha.1';
+
+    /** @var array */
+    private $concern = array(base::APPLICATION_BOX);
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get_release()
+    {
+        return $this->release;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function require_all_upgrades()
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function concern()
+    {
+        return $this->concern;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply(base $appbox, Application $app)
+    {
+        $config = $app['phraseanet.configuration']->getConfig();
+
+        $config['static-file'] = array(
+            'enabled' => false,
+            'type' => '',
+        );
+
+        $app['phraseanet.configuration']->setConfig($config);
+
+        return true;
+    }
+}
