01516a5dc0d15335319c6f8e20eac5bd33615910
Nicolas Le Goff <legoff.n@gmail.com>
2014-10-14 19:53:57 +0200

    Add new RecordInterface

---
+++ lib/Alchemy/Phrasea/Model/Entities/RecordInterface.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace Alchemy\Phrasea\Model\Entities;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+interface RecordInterface
+{
+    public function getId();
+
+    /**
+     * @return mixed
+     */
+    public function getBaseId();
+
+    /**
+     * @param mixed $baseId
+     */
+    public function setBaseId($baseId);
+
+    /**
+     * @return mixed
+     */
+    public function getCollectionId();
+
+    /**
+     * @param mixed $collectionId
+     */
+    public function setCollectionId($collectionId);
+
+    /**
+     * @return mixed
+     */
+    public function getCreated();
+
+    /**
+     * @param mixed $created
+     */
+    public function setCreated($created);
+
+    /**
+     * @return mixed
+     */
+    public function getDataboxId();
+
+    /**
+     * @param mixed $databoxId
+     */
+    public function setDataboxId($databoxId);
+
+    /**
+     * @return mixed
+     */
+    public function isStory();
+
+    /**
+     * @param mixed $isStory
+     */
+    public function setIsStory($isStory);
+
+    /**
+     * @return mixed
+     */
+    public function getMimeType();
+
+    /**
+     * @param mixed $mimeType
+     */
+    public function setMimeType($mimeType);
+
+    /**
+     * @return mixed
+     */
+    public function getOriginalName();
+
+    /**
+     * @param mixed $originalName
+     */
+    public function setOriginalName($originalName);
+
+    /**
+     * @return mixed
+     */
+    public function getRecordId();
+
+    /**
+     * @param mixed $recordId
+     */
+    public function setRecordId($recordId);
+
+    /**
+     * @return mixed
+     */
+    public function getSha256();
+
+    /**
+     * @param mixed $sha256
+     */
+    public function setSha256($sha256);
+
+    /**
+     * @return mixed
+     */
+    public function getTitle();
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title);
+
+    /**
+     * @return mixed
+     */
+    public function getType();
+
+    /**
+     * @param mixed $type
+     */
+    public function setType($type);
+
+    /**
+     * @return mixed
+     */
+    public function getUpdated();
+
+    /**
+     * @param mixed $updated
+     */
+    public function setUpdated($updated);
+
+    /**
+     * @return mixed
+     */
+    public function getUuid();
+
+    /**
+     * @param mixed $uuid
+     */
+    public function setUuid($uuid);
+
+
+    /**
+     * @return mixed
+     */
+    public function getExif();
+
+    /**
+     * @param mixed $exif
+     */
+    public function setExif(ArrayCollection $exif);
+
+    /**
+     * @return mixed
+     */
+    public function getCaption();
+
+    /**
+     * @param mixed $caption
+     */
+    public function setCaption(ArrayCollection $caption);
+
+
+    /**
+     * @return mixed
+     */
+    public function getSubdefs();
+
+    /**
+     * @param mixed $subdefs
+     */
+    public function setSubdefs(ArrayCollection $subdefs);
+}
