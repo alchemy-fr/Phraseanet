1b120e6569abe36177a98cec046c0df1a2c4b9aa
Mathieu Darse <mdarse@jolicode.com>
2014-10-28 11:08:21 +0100

    WIP query grammar

--- grammar/query.pp
+++ grammar/query.pp
@@ -1,21 +1,25 @@
 %skip   space           \s
 
-%token  and             AND
-%token  or              OR
+%token  quote_          "        -> string
+%token  string:string   [^"]+
+%token  string:_quote   "        -> default
 %token  in              IN
 %token  word            \S+
 
-query:
-    expression() *
+#query:
+    expression()+
 
 expression:
-    in_expression() | text()
+    in_expression() | text() | unrestricted_text()
 
-in_expression:
-    text() ::in:: field()
+#in_expression:
+    text() ::in:: ( <word> | string() )
 
-field:
-    <word>
+#text:
+    ( <word> | string() )+
 
-text:
-    <word> +
+#unrestricted_text:
+    ( <word> | string() | <in> )+
+
+string:
+    ::quote_:: <string> ::_quote::
