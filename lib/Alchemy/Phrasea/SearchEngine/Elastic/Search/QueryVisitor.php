810a49652170997653c7946eee1f008b17411632
Mathieu Darse <mdarse@jolicode.com>
2014-11-26 11:42:39 +0100

    Refactor query visitor

--- lib/Alchemy/Phrasea/SearchEngine/Elastic/Search/QueryVisitor.php
+++ lib/Alchemy/Phrasea/SearchEngine/Elastic/Search/QueryVisitor.php
@@ -23,79 +23,119 @@ class QueryVisitor implements Visit
     public function visit(Element $element, &$handle = null, $eldnah = null)
     {
         if (null !== $value = $element->getValue()) {
-            switch ($value['token']) {
-                case self::NODE_TOKEN_WORD:
-                    return new AST\TextNode($value['value']);
-
-                case self::NODE_TOKEN_STRING:
-                    return new AST\QuotedTextNode($value['value']);
-
-                default:
-                    // Generic handling off other tokens for unresctricted text
-                    return new AST\TextNode($value['value']);
-            }
+            return $this->visitToken($value['token'], $value['value']);
+        } else {
+            return $this->visitNode($element);
         }
+    }
 
+    private function visitToken($token, $value)
+    {
+        switch ($token) {
+            case self::NODE_TOKEN_WORD:
+                return new AST\TextNode($value);
+
+            case self::NODE_TOKEN_STRING:
+                return new AST\QuotedTextNode($value);
+
+            default:
+                // Generic handling off other tokens for unresctricted text
+                return new AST\TextNode($value);
+        }
+    }
+
+    private function visitNode(Element $element)
+    {
         switch ($element->getId()) {
             case self::NODE_TYPE_QUERY:
-                $root = null;
-                foreach ($element->getChildren() as $child) {
-                    $node = $child->accept($this, $handle, $eldnah);
-                    if ($root) {
-                        $root = new AST\AndExpression($root, $node);
-                    } else {
-                        $root = $node;
-                    }
-                }
-                return new Query($root);
+                return $this->visitQuery($element);
 
             case self::NODE_TYPE_IN_EXPR:
-                if ($element->getChildrenNumber() !== 2) {
-                    throw new \Exception('IN expression can only have 2 childs.');
-                }
-                $expression = $element->getChild(0)->accept($this);
-                $field = new AST\FieldNode($element->getChild(1)->getValue()['value']);
-                return new AST\InExpression($field, $expression);
+                return $this->visitInNode($element);
 
             case self::NODE_TYPE_AND_EXPR:
-                if ($element->getChildrenNumber() !== 2) {
-                    throw new \Exception('AND expression can only have 2 childs.');
-                }
-                $left  = $element->getChild(0)->accept($this);
-                $right = $element->getChild(1)->accept($this);
-                return new AST\AndExpression($left, $right);
+                return $this->visitAndNode($element);
 
             case self::NODE_TYPE_OR_EXPR:
-                if ($element->getChildrenNumber() !== 2) {
-                    throw new \Exception('OR expression can only have 2 childs.');
-                }
-                $left  = $element->getChild(0)->accept($this);
-                $right = $element->getChild(1)->accept($this);
-                return new AST\OrExpression($left, $right);
+                return $this->visitOrNode($element);
 
             case self::NODE_TYPE_TEXT:
             case self::NODE_TYPE_UNRESTRICTED_TEXT:
-                $root = null;
-                foreach ($element->getChildren() as $child) {
-                    $node = $child->accept($this, $handle, $eldnah);
-                    if ($root) {
-                        // $root = new AST\AndExpression($root, $node);
-                        // Merge text nodes together, but not with quoted ones
-                        if ($root instanceof AST\TextNode &&
-                            !$root instanceof AST\QuotedTextNode &&
-                            !$node instanceof AST\QuotedTextNode) {
-                            $root = new AST\TextNode(sprintf('%s %s', $root->getText(), $node->getText()));
-                        } else {
-                            $root = new AST\AndExpression($root, $node);
-                        }
-                    } else {
-                        $root = $node;
-                    }
-                }
-                return $root;
+                return $this->visitText($element);
 
             default:
                 throw new \Exception(sprintf('Unknown node type "%s".', $element->getId()));
         }
+    }
+
+    private function visitQuery(Element $element)
+    {
+        $root = null;
+        foreach ($element->getChildren() as $child) {
+            $node = $child->accept($this);
+            if ($root) {
+                $root = new AST\AndExpression($root, $node);
+            } else {
+                $root = $node;
+            }
+        }
+        return new Query($root);
+    }
+
+    private function visitInNode(Element $element)
+    {
+        if ($element->getChildrenNumber() !== 2) {
+            throw new \Exception('IN expression can only have 2 childs.');
+        }
+        $expression = $element->getChild(0)->accept($this);
+        $field = new AST\FieldNode($element->getChild(1)->getValue()['value']);
+        return new AST\InExpression($field, $expression);
+    }
+
+    private function visitAndNode(Element $element)
+    {
+        return $this->handleBinaryNode($element, function($left, $right) {
+            return new AST\AndExpression($left, $right);
+        });
+    }
+
+    private function visitOrNode(Element $element)
+    {
+        return $this->handleBinaryNode($element, function($left, $right) {
+            return new AST\OrExpression($left, $right);
+        });
+    }
+
+    private function handleBinaryNode(Element $element, \Closure $factory)
+    {
+        if ($element->getChildrenNumber() !== 2) {
+            throw new \Exception('Binary expression can only have 2 childs.');
+        }
+        $left  = $element->getChild(0)->accept($this);
+        $right = $element->getChild(1)->accept($this);
+
+        return $factory($left, $right);
+    }
+
+    private function visitText(Element $element)
+    {
+        $root = null;
+        foreach ($element->getChildren() as $child) {
+            $node = $child->accept($this);
+            if ($root) {
+                // Merge text nodes together, but not with quoted ones
+                if ($root instanceof AST\TextNode &&
+                    !$root instanceof AST\QuotedTextNode &&
+                    !$node instanceof AST\QuotedTextNode) {
+                    $root = new AST\TextNode(sprintf('%s %s', $root->getText(), $node->getText()));
+                } else {
+                    $root = new AST\AndExpression($root, $node);
+                }
+            } else {
+                $root = $node;
+            }
+        }
+
+        return $root;
     }
 }
