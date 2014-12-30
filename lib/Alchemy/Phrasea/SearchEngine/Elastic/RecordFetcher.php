cdecfcd0ad3924db47bc67b33becf2651d005ad4
Nicolas Le Goff <legoff.n@gmail.com>
2014-12-30 17:53:33 +0100

    Add original name as default title

--- lib/Alchemy/Phrasea/SearchEngine/Elastic/RecordFetcher.php
+++ lib/Alchemy/Phrasea/SearchEngine/Elastic/RecordFetcher.php
@@ -228,12 +228,18 @@ SQL;
             SELECT
                 m.`record_id`,
                 CASE ms.`thumbtitle`
-                WHEN "1" THEN "default" ELSE ms.`thumbtitle`
+                  WHEN "1" THEN "default"
+                  WHEN "0" THEN "default"
+                  ELSE ms.`thumbtitle`
                 END AS locale,
-                GROUP_CONCAT(m.value ORDER BY ms.`thumbtitle`, ms.`sorter` SEPARATOR " - ") AS title
+                CASE ms.`thumbtitle`
+                  WHEN "0" THEN r.`originalname`
+                  ELSE GROUP_CONCAT(m.`value` ORDER BY ms.`thumbtitle`, ms.`sorter` SEPARATOR " - ")
+                END AS title
             FROM metadatas AS m FORCE INDEX(`record_id`)
-            STRAIGHT_JOIN metadatas_structure AS ms ON (ms.`id` = m.`meta_struct_id` AND ms.`thumbtitle` != "0")
-            WHERE record_id IN (?)
+            STRAIGHT_JOIN metadatas_structure AS ms ON (ms.`id` = m.`meta_struct_id`)
+            STRAIGHT_JOIN record AS r ON (r.`record_id` = m.`record_id`)
+            WHERE m.`record_id` IN (?)
             GROUP BY m.`record_id`, ms.`thumbtitle`
 SQL;
 
