b3d50619c5ee1cb33f64ee0152e6126691b10015
Nicolas Le Goff <legoff.n@gmail.com>
2015-01-12 15:10:46 +0100

    PHRAS-350 Deprecate client

--- tests/Alchemy/Tests/Phrasea/Controller/Client/RootTest.php
+++ tests/Alchemy/Tests/Phrasea/Controller/Client/RootTest.php
@@ -2,8 +2,6 @@
 
 namespace Alchemy\Tests\Phrasea\Controller\Client;
 
-use Alchemy\Phrasea\SearchEngine\SearchEngineOptions;
-
 class RootTest extends \PhraseanetWebTestCaseAuthenticatedAbstract
 {
     protected $client;
@@ -25,57 +23,6 @@ class RootTest extends \PhraseanetWebTestCaseAuthenticatedAbstract
     {
         $this->authenticate(self::$DI['app']);
         self::$DI['client']->request("GET", "/client/");
-        $this->assertTrue(self::$DI['client']->getResponse()->isOk());
-    }
-
-    /**
-     * @covers Alchemy\Phrasea\Controller\Client\Root::getClientLanguage
-     */
-    public function testGetLanguage()
-    {
-        self::$DI['client']->request("GET", "/client/language/");
-        $this->assertTrue(self::$DI['client']->getResponse()->isOk());
-    }
-
-    /**
-     * @covers Alchemy\Phrasea\Controller\Client\Root::getClientPublications
-     */
-    public function testGetPublications()
-    {
-        self::$DI['client']->request("GET", "/client/publications/");
-        $this->assertTrue(self::$DI['client']->getResponse()->isOk());
-    }
-
-    /**
-     * @covers Alchemy\Phrasea\Controller\Client\Root::getClientHelp
-     */
-    public function testGetClientHelp()
-    {
-        self::$DI['client']->request("GET", "/client/help/");
-        $this->assertTrue(self::$DI['client']->getResponse()->isOk());
-    }
-
-    /**
-     * @covers Alchemy\Phrasea\Controller\Client\Root::query
-     * @covers Alchemy\Phrasea\Controller\Client\Root::buildQueryFromRequest
-     */
-    public function testExecuteQuery()
-    {
-        $queryParameters = array();
-        $queryParameters["mod"] = self::$DI['user']->getPrefs('client_view') ? : '3X6';
-        $queryParameters["bas"] = array_keys(self::$DI['user']->ACL()->get_granted_base());
-        $queryParameters["qry"] = self::$DI['user']->getPrefs('start_page_query') ? : 'all';
-        $queryParameters["pag"] = 0;
-        $queryParameters["search_type"] = SearchEngineOptions::RECORD_RECORD;
-        $queryParameters["qryAdv"] = '';
-        $queryParameters["opAdv"] = array();
-        $queryParameters["status"] = array();
-        $queryParameters["recordtype"] = SearchEngineOptions::TYPE_ALL;
-        $queryParameters["sort"] = self::$DI['app']['phraseanet.registry']->get('GV_phrasea_sort', '');
-        $queryParameters["infield"] = array();
-        $queryParameters["ord"] = SearchEngineOptions::SORT_MODE_DESC;
-
-        self::$DI['client']->request("POST", "/client/query/", $queryParameters);
-        $this->assertTrue(self::$DI['client']->getResponse()->isOk());
+        $this->assertTrue(self::$DI['client']->getResponse()->isRedirect());
     }
 }
