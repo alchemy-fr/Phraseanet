18bbddde528702d3f5d6c77fa21124641b494af7
Benoît Burnichon <bburnichon@alchemy.fr>
2015-03-14 00:22:42 +0100

    Improve tests/bootstrap.sh script

--- tests/bootstrap.sh
+++ tests/bootstrap.sh
@@ -2,18 +2,39 @@
 
 set -e
 
+USAGE="First argument should be install or update, followed by optional verbosity for commands"
+
+if test $# -lt 1; then echo "$USAGE" && exit -1; fi
+case "$1" in
+    install)
+        INSTALL_MODE="install";
+        ;;
+    update)
+        INSTALL_MODE="update";
+        ;;
+    *)
+        echo "Wrong mode."
+	echo "$USAGE"
+        exit -1
+esac
+shift
+VERBOSITY=$@
+
+set -x
 mysql -uroot -ptoor -e 'SET @@global.sql_mode=STRICT_ALL_TABLES;'
 if ! ./bin/developer system:uninstall > /dev/null 2>&1
 then
     rm -f config/configuration.yml config/configuration-compiled.php
 fi
-./bin/setup system:install --email=test@phraseanet.com --password=test --db-user=root --db-template=fr --db-password=toor --databox=db_test --appbox=ab_test --server-name=http://127.0.0.1 -y -vvv
-case "$1" in
+./bin/setup system:install --email=test@phraseanet.com --password=test --db-user=root --db-template=fr --db-password=toor --databox=db_test --appbox=ab_test --server-name=http://127.0.0.1 -y $VERBOSITY
+case "$INSTALL_MODE" in
     update)
-        ./bin/developer ini:reset --email=test@phraseanet.com --password=test --run-patches --no-setup-dbs -vvv
-        php resources/hudson/cleanupSubdefs.php
+        ./bin/developer ini:reset --email=test@phraseanet.com --password=test --run-patches --no-setup-dbs $VERBOSITY
+        php resources/hudson/cleanupSubdefs.php $VERBOSITY
+        ;;
+    install)
         ;;
 esac
-./bin/developer ini:setup-tests-dbs -vvv
-./bin/console searchengine:index:create
-./bin/developer phraseanet:regenerate-sqlite -vvv
+./bin/developer ini:setup-tests-dbs $VERBOSITY
+./bin/console searchengine:index:create $VERBOSITY
+./bin/developer phraseanet:regenerate-sqlite $VERBOSITY
