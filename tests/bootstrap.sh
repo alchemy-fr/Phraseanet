8788850acc48d4c164ff1187e593eeb35cc1da62
Benoît Burnichon <bburnichon@alchemy.fr>
2015-03-13 11:12:34 +0100

    allow test bootstrap to install in update or install mode

--- tests/bootstrap.sh
+++ tests/bootstrap.sh
@@ -4,11 +4,15 @@ set -e
 
 if ! ./bin/developer system:uninstall > /dev/null 2>&1
 then
-  rm -f config/configuration.yml config/configuration-compiled.php
+    rm -f config/configuration.yml config/configuration-compiled.php
 fi
 ./bin/setup system:install --email=test@phraseanet.com --password=test --db-user=root --db-template=fr --db-password=toor --databox=db_test --appbox=ab_test --server-name=http://127.0.0.1 -y -vvv
-./bin/developer ini:reset --email=test@phraseanet.com --password=test --run-patches --no-setup-dbs -vvv
-php resources/hudson/cleanupSubdefs.php
+case "$1" in
+    update)
+        ./bin/developer ini:reset --email=test@phraseanet.com --password=test --run-patches --no-setup-dbs -vvv
+        php resources/hudson/cleanupSubdefs.php
+        ;;
+esac
 ./bin/developer ini:setup-tests-dbs -vvv
 ./bin/console searchengine:index:create
 ./bin/developer phraseanet:regenerate-sqlite -vvv
