10395f410c04c4ae332b06147fa40c092dafc942
Mathieu Darse <mdarse@jolicode.com>
2015-04-01 19:33:48 +0200

    Add tests for query compiler

---
+++ tests/Alchemy/Tests/Phrasea/SearchEngine/QueryCompilerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Alchemy\Tests\Phrasea\SearchEngine;
+
+use Alchemy\Phrasea\SearchEngine\Elastic\Search\QueryCompiler;
+use Alchemy\Phrasea\SearchEngine\Elastic\Thesaurus;
+use Alchemy\Tests\Tools\CsvFileIterator;
+use Hoa\Compiler;
+use Hoa\File;
+
+
+class QueryCompilerTest extends \PHPUnit_Framework_TestCase
+{
+    private $compiler;
+
+    protected function setUp()
+    {
+        $grammar_path = 'grammar/query.pp';
+        $project_root = '../../../../..';
+        $grammar_path = realpath(implode('/', [__DIR__, $project_root, $grammar_path]));
+        $parser = Compiler\Llk\Llk::load(new File\Read($grammar_path));
+
+        $thesaurus = $this->getMockBuilder(Thesaurus::class)
+                          ->disableOriginalConstructor()
+                          ->getMock();
+
+        $this->compiler = new QueryCompiler($parser, $thesaurus);
+    }
+
+    /**
+     * @dataProvider queryProvider
+     */
+    public function testQueryParsing($query, $expected)
+    {
+        $this->assertEquals($expected, $this->compiler->parse($query)->dump());
+    }
+
+    public function queryProvider()
+    {
+        return new CsvFileIterator(sprintf('%s/resources/queries.csv', __DIR__), '|', '\'');
+    }
+}
