4f1c654ede0ee9665ec011b21732eaa332ac272d
Benoît Burnichon <bburnichon@alchemy.fr>
2015-06-01 10:59:05 +0200

    Refactor Prod ShareController

--- lib/Alchemy/Phrasea/Controller/Prod/ShareController.php
+++ lib/Alchemy/Phrasea/Controller/Prod/ShareController.php
@@ -9,9 +9,7 @@
  */
 namespace Alchemy\Phrasea\Controller\Prod;
 
-use Alchemy\Phrasea\Application;
 use Alchemy\Phrasea\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class ShareController extends Controller
@@ -19,22 +17,20 @@ class ShareController extends Controller
     /**
      *  Share a record
      *
-     * @param  Application $app
-     * @param  Request     $request
      * @param  integer     $base_id
      * @param  integer     $record_id
      * @return Response
      */
-    public function shareRecord(Application $app, Request $request, $base_id, $record_id)
+    public function shareRecord($base_id, $record_id)
     {
-        $record = new \record_adapter($app, \phrasea::sbasFromBas($app, $base_id), $record_id);
+        $record = new \record_adapter($this->app, \phrasea::sbasFromBas($this->app, $base_id), $record_id);
 
-        if (!$app['acl']->get($app['authentication']->getUser())->has_access_to_subdef($record, 'preview')) {
-            $app->abort(403);
+        if (!$this->getAclForUser()->has_access_to_subdef($record, 'preview')) {
+            $this->app->abort(403);
         }
 
-        return new Response($app['twig']->render('prod/Share/record.html.twig', [
+        return $this->renderResponse('prod/Share/record.html.twig', [
             'record' => $record,
-        ]));
+        ]);
     }
 }
