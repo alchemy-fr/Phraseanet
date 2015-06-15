alchemy-fr/Phraseanet

commit 40e2df6c3f8653ff482f3cd8165cfbea9461f7e5

Search in private fields using new structure features

lib/Alchemy/Phrasea/SearchEngine/Elastic/ElasticSearchEngine.php
@@ -260,7 +260,11 @@ class ElasticSearchEngine implements SearchEngineInterface
     {
         $options = $options ?: new SearchEngineOptions();
 
-        $queryContext = new QueryContext($this->locales, $this->app['locale']);
+        $queryContext = new QueryContext(
+            $this->structure,
+            $this->locales,
+            $this->app['locale']
+        );
         $recordQuery = $this->app['query_compiler']->compile($string, $queryContext);
 
         $params = $this->createRecordQueryParams($recordQuery, $options, null);
@@ -377,11 +381,11 @@ class ElasticSearchEngine implements SearchEngineInterface
         $aggs['Collection'] = $collection_facet_agg;
 
         foreach ($this->structure->getFacetFields() as $name => $field) {
-            $prefix = $field->isPrivate() ? 'private_caption' : 'caption';
             // 2015-05-26 (mdarse) Removed databox filtering.
             // It was already done by the ACL filter in the query scope, so no
             // document that shouldn't be displayed can go this far.
-            $aggs[$name]['terms']['field'] = sprintf('%s.%s.raw', $prefix, $name);
+            $field_name = RecordHelper::getIndexFieldName($field);
+            $aggs[$name]['terms']['field'] = sprintf('%s.raw', $field_name);
         }
 
         return $aggs;

lib/Alchemy/Phrasea/SearchEngine/Elastic/RecordHelper.php
@@ -13,6 +13,7 @@ namespace Alchemy\Phrasea\SearchEngine\Elastic;
 
 use Alchemy\Phrasea\SearchEngine\Elastic\Exception\MergeException;
 use Alchemy\Phrasea\SearchEngine\Elastic\Mapping;
+use Alchemy\Phrasea\SearchEngine\Elastic\Structure\Field;
 use appbox;
 use igorw;
 
@@ -83,5 +84,14 @@ class RecordHelper
         } catch (\Exception $e) {
             return null;
         }
+    }
+
+    public static function getIndexFieldName(Field $field)
+    {
+        return sprintf(
+            '%scaption.%s',
+            $field->isPrivate() ? 'private_' : '',
+            $field->getName()
+        );
     }
 }

lib/Alchemy/Phrasea/SearchEngine/Elastic/Search/QueryContext.php
@@ -3,15 +3,22 @@
 namespace Alchemy\Phrasea\SearchEngine\Elastic\Search;
 
 use Alchemy\Phrasea\SearchEngine\Elastic\Exception\QueryException;
+use Alchemy\Phrasea\SearchEngine\Elastic\RecordHelper;
+use Alchemy\Phrasea\SearchEngine\Elastic\Structure\Structure;
 
+/**
+ * @todo Check for private fields and only search on them if allowed
+ */
 class QueryContext
 {
+    private $structure;
     private $locales;
     private $queryLocale;
     private $fields;
 
-    public function __construct(array $locales, $queryLocale, array $fields = null)
+    public function __construct(Structure $structure, array $locales, $queryLocale, array $fields = null)
     {
+        $this->structure = $structure;
         $this->locales = $locales;
         $this->queryLocale = $queryLocale;
         $this->fields = $fields;
@@ -32,14 +39,18 @@ class QueryContext
 
     public function getRawFields()
     {
-        // TODO Private fields handling
         if ($this->fields === null) {
-            return array('caption_all.raw');
+            return array(
+                'caption_all.raw',
+                'private_caption_all.raw'
+            );
         }
 
         $fields = array();
-        foreach ($this->fields as $field) {
-            $fields[] = sprintf('caption.%s.raw', $field);
+        foreach ($this->fields as $name) {
+            if ($field = $this->normalizeField($name)) {
+                $fields[] = sprintf('%s.raw', $field);
+            }
         }
 
         return $fields;
@@ -47,14 +58,17 @@ class QueryContext
 
     public function getLocalizedFields()
     {
-        // TODO Private fields handling
         if ($this->fields === null) {
-            return $this->localizeField('caption_all');
+            return array_merge(
+                $this->localizeField('caption_all'),
+                $this->localizeField('private_caption_all')
+            );
         }
 
         $fields = array();
         foreach ($this->fields as $field) {
-            foreach ($this->localizeField(sprintf('caption.%s', $field)) as $fields[]);
+            $normalized = $this->normalizeField($field);
+            foreach ($this->localizeField($normalized) as $fields[]);
         }
 
         return $fields;
@@ -73,11 +87,14 @@ class QueryContext
         return $fields;
     }
 
-    public function normalizeField($field)
+    public function normalizeField($name)
     {
-        // TODO Private fields handling
+        $field = $this->structure->get($name);
+        if (!$field) {
+            return;
+        }
         // TODO Field label dereferencing (we only want names)
-        return sprintf('caption.%s', $field);
+        return RecordIndexer::getIndexFieldName($field);
     }
 
     public function getFields()

commit ea8eb4dc074beb8eaceae30dfa6037cdf61589ca

Fix another IN search regression from 40e2df6c3f8653ff482f3cd8165cfbea9461f7e5
Move getIndexFieldName() from `RecordHelper` to `Field`.
Also remove an unused injected dependency

lib/Alchemy/Phrasea/Core/Provider/SearchEngineServiceProvider.php
@@ -59,7 +59,6 @@ class SearchEngineServiceProvider implements ServiceProviderInterface
                 $app['elasticsearch.client'],
                 $app['elasticsearch.options']['index'],
                 $app['locales.available'],
-                $app['elasticsearch.record_helper'],
                 $app['elasticsearch.facets_response.factory']
             );
         });

lib/Alchemy/Phrasea/SearchEngine/Elastic/ElasticSearchEngine.php
@@ -41,15 +41,13 @@ class ElasticSearchEngine implements SearchEngineInterface
     private $indexName;
     private $configurationPanel;
     private $locales;
-    private $recordHelper;
 
-    public function __construct(Application $app, Structure $structure, Client $client, $indexName, array $locales, RecordHelper $recordHelper, Closure $facetsResponseFactory)
+    public function __construct(Application $app, Structure $structure, Client $client, $indexName, array $locales, Closure $facetsResponseFactory)
     {
         $this->app = $app;
         $this->structure = $structure;
         $this->client = $client;
         $this->locales = array_keys($locales);
-        $this->recordHelper = $recordHelper;
         $this->facetsResponseFactory = $facetsResponseFactory;
 
         if ('' === trim($indexName)) {
@@ -384,7 +382,7 @@ class ElasticSearchEngine implements SearchEngineInterface
             // 2015-05-26 (mdarse) Removed databox filtering.
             // It was already done by the ACL filter in the query scope, so no
             // document that shouldn't be displayed can go this far.
-            $field_name = RecordHelper::getIndexFieldName($field);
+            $field_name = $field->getIndexFieldName();
             $aggs[$name]['terms']['field'] = sprintf('%s.raw', $field_name);
         }
 

lib/Alchemy/Phrasea/SearchEngine/Elastic/RecordHelper.php
@@ -85,13 +85,4 @@ class RecordHelper
             return null;
         }
     }
-
-    public static function getIndexFieldName(Field $field)
-    {
-        return sprintf(
-            '%scaption.%s',
-            $field->isPrivate() ? 'private_' : '',
-            $field->getName()
-        );
-    }
 }

lib/Alchemy/Phrasea/SearchEngine/Elastic/Search/QueryContext.php
@@ -94,7 +94,7 @@ class QueryContext
             return;
         }
         // TODO Field label dereferencing (we only want names)
-        return RecordIndexer::getIndexFieldName($field);
+        return $field->getIndexFieldName();
     }
 
     public function getFields()

lib/Alchemy/Phrasea/SearchEngine/Elastic/Structure/Field.php
@@ -73,6 +73,15 @@ class Field
         return $this->name;
     }
 
+    public function getIndexFieldName()
+    {
+        return sprintf(
+            '%scaption.%s',
+            $this->is_private ? 'private_' : '',
+            $this->name
+        );
+    }
+
     public function getType()
     {
         return $this->type;

tests/Alchemy/Tests/Phrasea/SearchEngine/Search/QueryContextTest.php
@@ -2,7 +2,9 @@
 
 namespace Alchemy\Tests\Phrasea\SearchEngine\Search;
 
+use Alchemy\Phrasea\SearchEngine\Elastic\Mapping;
 use Alchemy\Phrasea\SearchEngine\Elastic\Search\QueryContext;
+use Alchemy\Phrasea\SearchEngine\Elastic\Structure\Field;
 use Alchemy\Phrasea\SearchEngine\Elastic\Structure\Structure;
 
 class QueryContextTest extends \PHPUnit_Framework_TestCase
@@ -14,5 +16,18 @@ class QueryContextTest extends \PHPUnit_Framework_TestCase
         $context = new QueryContext($structure, $available_locales, 'fr');
         $narrowed = $context->narrowToFields(['some_field']);
         $this->assertEquals(['some_field'], $narrowed->getFields());
+    }
+
+    public function testFieldNormalization()
+    {
+        $public_field = new Field('foo', Mapping::TYPE_STRING, true, false);
+        $restricted_field = new Field('bar', Mapping::TYPE_STRING, true, true);
+        $structure = $this->prophesize(Structure::class);
+        $structure->get('foo')->willReturn($public_field);
+        $structure->get('bar')->willReturn($restricted_field);
+
+        $context = new QueryContext($structure->reveal(), [], 'fr');
+        $this->assertEquals('caption.foo', $context->normalizeField('foo'));
+        $this->assertEquals('private_caption.bar', $context->normalizeField('bar'));
     }
 }