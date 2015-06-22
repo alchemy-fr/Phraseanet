f3cf58404e66302ce48f9a077f222f6e5e575ba3
Benoît Burnichon <bburnichon@alchemy.fr>
2015-06-22 15:27:32 +0200

    Refactor RootController

--- lib/Alchemy/Phrasea/Controller/Root/RootController.php
+++ lib/Alchemy/Phrasea/Controller/Root/RootController.php
@@ -9,17 +9,15 @@
  */
 namespace Alchemy\Phrasea\Controller\Root;
 
-use Alchemy\Phrasea\Application;
 use Alchemy\Phrasea\Controller\Controller;
 use Symfony\Component\HttpFoundation\Cookie;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class RootController extends Controller
 {
-    public function getRobots(Application $app, Request $request)
+    public function getRobots()
     {
-        if ($app['conf']->get(['registry', 'general', 'allow-indexation']) === true) {
+        if ($this->getConf()->get(['registry', 'general', 'allow-indexation']) === true) {
             $buffer = "User-Agent: *\n" . "Allow: /\n";
         } else {
             $buffer = "User-Agent: *\n" . "Disallow: /\n";
@@ -28,21 +26,21 @@ class RootController extends Controller
         return new Response($buffer, 200, ['Content-Type' => 'text/plain']);
     }
 
-    public function getRoot(Application $app, Request $request)
+    public function getRoot()
     {
-        return $app->redirectPath('homepage');
+        return $this->app->redirectPath('homepage');
     }
 
-    public function setLocale(Application $app, Request $request, $locale)
+    public function setLocale($locale)
     {
-        $response = $app->redirectPath('root');
+        $response = $this->app->redirectPath('root');
         $response->headers->setCookie(new Cookie('locale', $locale));
 
         return $response;
     }
 
-    public function getAvailableLanguages(Application $app, Request $request)
+    public function getAvailableLanguages()
     {
-        return $app->json($app['locales.available']);
+        return $this->app->json($this->app['locales.available']);
     }
 }
