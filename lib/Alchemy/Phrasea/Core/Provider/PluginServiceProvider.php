a01849bfa73c3a4735c5d9308b113f3efd42729a
Benoît Burnichon <bburnichon@alchemy.fr>
2015-08-10 13:39:08 +0200

    Add plugin.actionbar pimple container to PluginServiceProvider

--- lib/Alchemy/Phrasea/Core/Provider/PluginServiceProvider.php
+++ lib/Alchemy/Phrasea/Core/Provider/PluginServiceProvider.php
@@ -14,9 +14,13 @@ namespace Alchemy\Phrasea\Core\Provider;
 use Alchemy\Phrasea\Plugin\PluginManager;
 use Alchemy\Phrasea\Plugin\Schema\ManifestValidator;
 use Alchemy\Phrasea\Plugin\Schema\PluginValidator;
+use ArrayObject;
 use JsonSchema\Validator as JsonValidator;
+use Pimple;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
+use Twig_Environment;
+use Twig_SimpleFunction;
 
 class PluginServiceProvider implements ServiceProviderInterface
 {
@@ -39,21 +43,24 @@ class PluginServiceProvider implements ServiceProviderInterface
         $app['plugins.manager'] = $app->share(function (Application $app) {
             return new PluginManager($app['plugin.path'], $app['plugins.plugins-validator'], $app['conf']);
         });
+        $app['plugin.actionbar'] = $app->share(function () {
+            return new Pimple();
+        });
         $app['plugin.workzone'] = $app->share(function () {
-            return new \Pimple();
+            return new Pimple();
         });
 
-        $app['plugin.locale.textdomains'] = new \ArrayObject();
+        $app['plugin.locale.textdomains'] = new ArrayObject();
 
         // Routes will be bound after all others
         // Add a new controller provider can be added as follows
         // $app['plugin.controller_providers'][] = array('/prefix', 'controller_provider_service_key');
-        $app['plugin.controller_providers.root'] = new \ArrayObject();
+        $app['plugin.controller_providers.root'] = new ArrayObject();
 
         // Routes will be bound after all others
         // Add a new controller provider can be added as follows
         // $app['plugin.controller_providers'][] = array('/prefix', 'controller_provider_service_key');
-        $app['plugin.controller_providers.api'] = new \ArrayObject();
+        $app['plugin.controller_providers.api'] = new ArrayObject();
     }
 
     public function boot(Application $app)
@@ -64,8 +71,8 @@ class PluginServiceProvider implements ServiceProviderInterface
         }
 
         $app['twig'] = $app->share(
-            $app->extend('twig', function (\Twig_Environment $twig) {
-                $function = new \Twig_SimpleFunction('plugin_asset', ['Alchemy\Phrasea\Plugin\Management\AssetsManager', 'twigPluginAsset']);
+            $app->extend('twig', function (Twig_Environment $twig) {
+                $function = new Twig_SimpleFunction('plugin_asset', array('Alchemy\Phrasea\Plugin\Management\AssetsManager', 'twigPluginAsset'));
                 $twig->addFunction($function);
 
                 return $twig;
