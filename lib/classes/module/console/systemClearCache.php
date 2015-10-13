80322ed9575a90f25b5a286193fa3fe1b92ea3ac
Benoît Burnichon <bburnichon@alchemy.fr>
2015-10-13 10:32:52 +0200

    Proper use of ArrayObject::getArrayCopy()
    ArrayObject::toArray() does not exists

--- lib/classes/module/console/systemClearCache.php
+++ lib/classes/module/console/systemClearCache.php
@@ -31,7 +31,7 @@ class module_console_systemClearCache extends Command
     {
         $finder = new Finder();
 
-        $in = $this->container['cache.paths']->toArray();
+        $in = $this->container['cache.paths']->getArrayCopy();
         $finder
             ->exclude('.git')
             ->exclude('.svn')
