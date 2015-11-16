479fad6c4bcb714008580034c13d71548d60d187
Benoît Burnichon <bburnichon@alchemy.fr>
2015-11-16 15:49:58 +0100

    Remove unused use statement

--- lib/Alchemy/Phrasea/TaskManager/Editor/SubdefsEditor.php
+++ lib/Alchemy/Phrasea/TaskManager/Editor/SubdefsEditor.php
@@ -11,7 +11,6 @@
 
 namespace Alchemy\Phrasea\TaskManager\Editor;
 
-use Alchemy\Phrasea\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
