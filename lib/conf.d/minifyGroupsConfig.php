82a35ce457aed287a74fa76b7f486310f9409c73
Florian BLOUET <florian.blouet@gmail.com>
2015-12-02 14:47:36 +0100

    remove useless minifyGroups

--- lib/conf.d/minifyGroupsConfig.php
+++ lib/conf.d/minifyGroupsConfig.php
@@ -8,15 +8,6 @@
  * You may wish to use the Minify URI Builder app to suggest
  * changes. http://yourdomain/min/builder/
  * */
-$groups = [
-    'authentication' => [
-        '//assets/modernizr/modernizr.js',
-        '//assets/requirejs/require.js',
-        '//scripts/apps/login/home/config.js'
-    ],
-    'modalBox' => [
-         '//assets/jquery.ui/i18n/jquery-ui-i18n.js'
-    ]
-];
+$groups = [];
 
 return $groups;
