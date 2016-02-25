0d6c44fac3bafba641ec335f698e0b5fbb443831
Benoît Burnichon <bburnichon@alchemy.fr>
2016-02-25 19:32:12 +0100

    Add some missing copyrights

--- lib/Alchemy/Phrasea/Collection/Repository/CachedCollectionRepository.php
+++ lib/Alchemy/Phrasea/Collection/Repository/CachedCollectionRepository.php
@@ -1,8 +1,8 @@
 <?php
-/*
+/**
  * This file is part of Phraseanet
  *
- * (c) 2005-2015 Alchemy
+ * (c) 2005-2016 Alchemy
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
