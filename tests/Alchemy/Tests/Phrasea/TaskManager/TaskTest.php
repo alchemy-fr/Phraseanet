8ac4b3671bd1044dc55963def22dd75a58cb6ff8
Benoît Burnichon <bburnichon@alchemy.fr>
2016-03-01 12:42:37 +0100

    Fix invalid process comparison

--- tests/Alchemy/Tests/Phrasea/TaskManager/TaskTest.php
+++ tests/Alchemy/Tests/Phrasea/TaskManager/TaskTest.php
@@ -7,33 +7,32 @@ use Alchemy\Phrasea\TaskManager\Task;
 
 class TaskTest extends TaskTestCase
 {
+    private $process;
+
     public function testThatCreatedProcessAreDifferents()
     {
-        $process = $this->getMockBuilder('Symfony\Component\Process\Process')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $taskEntity = $this->getMock('Alchemy\Phrasea\Model\Entities\Task');
-        $task = new Task($taskEntity, 'task number', 42, $process);
+        $task = $this->getTask();
 
         $created1 = $task->createProcess();
 
-        $this->assertEquals($process, $created1);
-        $this->assertNotSame($process, $created1);
+        $this->assertEquals($this->process, $created1);
+        $this->assertNotSame($this->process, $created1);
 
         $created2 = $task->createProcess();
 
         $this->assertEquals($created1, $created2);
-        $this->assertNotSame($process, $created2);
+        $this->assertNotSame($this->process, $created2);
         $this->assertNotSame($created1, $created2);
     }
 
     protected function getTask()
     {
-        $process = $this->getMockBuilder('Symfony\Component\Process\Process')
+        $this->process = $this->getMockBuilder('Symfony\Component\Process\Process')
             ->disableOriginalConstructor()
+            ->disableOriginalClone()
             ->getMock();
         $taskEntity = $this->getMock('Alchemy\Phrasea\Model\Entities\Task');
 
-        return new Task($taskEntity, 'task number', 42, $process);
+        return new Task($taskEntity, 'task number', 42, $this->process);
     }
 }
