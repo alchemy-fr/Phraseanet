0030e51561ba8fbfcedca7ee4675be0bc36adecf
Benoît Burnichon <bburnichon@alchemy.fr>
2016-04-19 11:04:55 +0200

    WIP RecordTransformer

--- lib/Alchemy/Phrasea/Search/CaptionView.php
+++ lib/Alchemy/Phrasea/Search/CaptionView.php
@@ -10,12 +10,19 @@
 
 namespace Alchemy\Phrasea\Search;
 
+use Assert\Assertion;
+
 class CaptionView
 {
     /**
      * @var \caption_record
      */
     private $caption;
+
+    /**
+     * @var \caption_field[]
+     */
+    private $fields = [];
 
     public function __construct(\caption_record $caption)
     {
@@ -28,5 +35,27 @@ class CaptionView
     public function getCaption()
     {
         return $this->caption;
+    }
+
+    /**
+     * @param \caption_field[] $fields
+     */
+    public function setFields($fields)
+    {
+        Assertion::allIsInstanceOf($fields, \caption_field::class);
+
+        $this->fields = [];
+
+        foreach ($fields as $field) {
+            $this->fields[$field->get_name()] = $field;
+        }
+    }
+
+    /**
+     * @return \caption_field[]
+     */
+    public function getFields()
+    {
+        return $this->fields;
     }
 }
