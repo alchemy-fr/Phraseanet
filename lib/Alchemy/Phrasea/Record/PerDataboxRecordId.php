f6c3672eeb33798ff31a1f697ca0c9aece87a977
Benoît Burnichon <bburnichon@alchemy.fr>
2016-04-22 12:22:27 +0200

    Add RecordCollection class to avoid multiple record_adapter fetching

---
+++ lib/Alchemy/Phrasea/Record/PerDataboxRecordId.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * This file is part of Phraseanet
+ *
+ * (c) 2005-2016 Alchemy
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Alchemy\Phrasea\Record;
+
+interface PerDataboxRecordId
+{
+    /**
+     * @param int $databoxId
+     * @return int[]
+     */
+    public function getDataboxRecordIds($databoxId);
+}
