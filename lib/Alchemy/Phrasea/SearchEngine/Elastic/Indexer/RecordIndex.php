68ab86d9d446cd632b771f17dc19e25ec16faff7
Jean-Yves Gaulier <gaulier@alchemy.fr>
2016-11-02 17:29:20 +0100

    PHRAS-1260_INDEX-WIDTH-HEIGHT  - new : added with & height of document to es

--- lib/Alchemy/Phrasea/SearchEngine/Elastic/Indexer/RecordIndex.php
+++ lib/Alchemy/Phrasea/SearchEngine/Elastic/Indexer/RecordIndex.php
@@ -66,6 +66,9 @@ class RecordIndex implements MappingProvider
         $mapping->addStringField('type')->disableAnalysis();
         $mapping->addStringField('record_type')->disableAnalysis();
 
+        $mapping->addIntegerField('width')->disableIndexing();
+        $mapping->addIntegerField('height')->disableIndexing();
+
         $mapping->addDateField('created_on', FieldMapping::DATE_FORMAT_MYSQL_OR_CAPTION);
         $mapping->addDateField('updated_on', FieldMapping::DATE_FORMAT_MYSQL_OR_CAPTION);
 
