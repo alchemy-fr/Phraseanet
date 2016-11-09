3a0517774c0be44993f7590613e91e9552282eb5
Jean-Yves Gaulier <gaulier@alchemy.fr>
2016-11-09 15:22:19 +0100

    PHRAS-1155_UPDATE-3.8-4.0-FAILS fix : the new column "Orders.notification_method" creation (2016...) is moved to an older patch (2013...) since this patch runs on 2016 orm object "Orders"

--- lib/Alchemy/Phrasea/Setup/DoctrineMigrations/OrderMigration.php
+++ lib/Alchemy/Phrasea/Setup/DoctrineMigrations/OrderMigration.php
@@ -11,6 +11,7 @@
 
 namespace Alchemy\Phrasea\Setup\DoctrineMigrations;
 
+use Alchemy\Phrasea\Model\Entities\Order;
 use Doctrine\DBAL\Schema\Schema;
 
 class OrderMigration extends AbstractMigration
@@ -23,6 +24,8 @@ class OrderMigration extends AbstractMigration
     public function doUpSql(Schema $schema)
     {
         $this->addSql("CREATE TABLE Orders (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, basket_id INT DEFAULT NULL, order_usage VARCHAR(2048) NOT NULL, todo INT DEFAULT NULL, deadline DATETIME NOT NULL, created_on DATETIME NOT NULL, INDEX IDX_E283F8D8A76ED395 (user_id), UNIQUE INDEX UNIQ_E283F8D81BE1FB52 (basket_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB");
+        $this->addSql(sprintf("ALTER TABLE Orders ADD COLUMN notification_method VARCHAR(32) NOT NULL DEFAULT '%s'", Order::NOTIFY_MAIL));
+        $this->addSql("ALTER TABLE Orders ALTER COLUMN notification_method DROP DEFAULT");
         $this->addSql("CREATE TABLE OrderElements (id INT AUTO_INCREMENT NOT NULL, order_master INT DEFAULT NULL, order_id INT DEFAULT NULL, base_id INT NOT NULL, record_id INT NOT NULL, deny TINYINT(1) DEFAULT NULL, INDEX IDX_8C7066C8EE86B303 (order_master), INDEX IDX_8C7066C88D9F6D38 (order_id), UNIQUE INDEX unique_ordercle (base_id, record_id, order_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB");
         $this->addSql("ALTER TABLE Orders ADD CONSTRAINT FK_E283F8D8A76ED395 FOREIGN KEY (user_id) REFERENCES Users (id)");
         $this->addSql("ALTER TABLE Orders ADD CONSTRAINT FK_E283F8D81BE1FB52 FOREIGN KEY (basket_id) REFERENCES Baskets (id)");
