9b2ffed73725f4210b4e557d2f3263560f0eb4fb
Xavier Rousset <xrousset78800@gmail.com>
2017-04-21 18:08:29 +0200

    Update Vagrant provisionning. fix mariaDB vars names, fix windows.sh paths

--- resources/ansible/windows.sh
+++ resources/ansible/windows.sh
@@ -25,7 +25,7 @@ sudo apt-get update
 sudo apt-get install -y ansible
 
 # Setup Ansible for Local Use and Run
-cp /vagrant/ansible/inventories/dev /etc/ansible/hosts -f
+cp /vagrant/resources/ansible/inventories/dev /etc/ansible/hosts -f
 chmod 666 /etc/ansible/hosts
-cat /vagrant/ansible/files/authorized_keys >> /home/vagrant/.ssh/authorized_keys
-sudo ansible-playbook /vagrant/ansible/playbook.yml -e hostname=$1 --connection=local
+cat /vagrant/resources/ansible/files/authorized_keys >> /home/vagrant/.ssh/authorized_keys
+sudo ansible-playbook /vagrant/resources/ansible/playbook.yml -e hostname=$1 --connection=local
