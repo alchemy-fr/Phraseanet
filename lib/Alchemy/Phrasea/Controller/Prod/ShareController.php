88f5d1db43988dc692a594b1495b9af2deab7803
Mike Ng <mike.esokia@gmail.com>
2017-09-12 17:07:31 +0400

    wip dynamic selection of permalink

--- lib/Alchemy/Phrasea/Controller/Prod/ShareController.php
+++ lib/Alchemy/Phrasea/Controller/Prod/ShareController.php
@@ -23,43 +23,100 @@ class ShareController extends Controller
      */
     public function shareRecord($base_id, $record_id)
     {
+//        $outputVars = [
+//            'isAvailable' => false,
+//            'subdefList' => [],
+//            'defaultKey' => 'preview',
+//            'preview' => [
+//              'permalinkUrl' => '',
+//              'permaviewUrl' => '',
+//              'embedUrl' => '',
+//              'width' => '',
+//              'height' => ''
+//            ]
+//        ];
+
         $outputVars = [
             'isAvailable' => false,
-            'preview' => [
-              'permalinkUrl' => '',
-              'permaviewUrl' => '',
-              'embedUrl' => '',
-              'width' => '',
-              'height' => ''
-            ]
+            'subdefList' => [],
+            'defaultKey' => 'preview',
         ];
+
         $record = new \record_adapter($this->app, \phrasea::sbasFromBas($this->app, $base_id), $record_id);
 
-        if (!$this->getAclForUser()->has_access_to_subdef($record, 'preview')) {
-            $this->app->abort(403);
-        }
+        $databox = $this->app->findDataboxById($record->getDataboxId());
+        $subdefGroup = $databox->get_subdef_structure()->getSubdefGroup($record->getType());
 
-        $preview = $record->get_preview();
+        $subdefList = array();
+        if ($subdefGroup) {
+            foreach ($subdefGroup as $subdefObj) {
+                $value = $subdefObj->get_class();
+                if (!in_array($value, $subdefList))
+                {
+                    if (!$this->getAclForUser()->has_access_to_subdef($record, $value)) {
+                        return;
+                    }
+                    $preview = $record->get_subdef($value);
+                    if (null !== $previewLink = $preview->get_permalink()) {
+                        $permalinkUrl = $previewLink->get_url()->__toString();
+                        $permaviewUrl = $previewLink->get_page();
+                        $previewWidth = $preview->get_width();
+                        $previewHeight = $preview->get_height();
 
-        if (null !== $previewLink = $preview->get_permalink()) {
-            $permalinkUrl = $previewLink->get_url();
-            $permaviewUrl = $previewLink->get_page();
-            $previewWidth = $preview->get_width();
-            $previewHeight = $preview->get_height();
+                        $embedUrl = $this->app->url('alchemy_embed_view', ['url' => (string)$permalinkUrl]);
+                        $previewData = [
+                            'permalinkUrl' => $permalinkUrl,
+                            'permaviewUrl' => $permaviewUrl,
+                            'embedUrl'     => $embedUrl,
+                            'width'        => $previewWidth,
+                            'height'       => $previewHeight
+                        ];
+                        $subdefList[$value] = $previewData;
+                    }
+                }
+            }
 
-            $embedUrl = $this->app->url('alchemy_embed_view', ['url' => (string)$permalinkUrl]);
+            //set default key
+            if (array_key_exists("preview",$subdefList)) {
+                $defaultKey = 'preview';
+            }else if(array_key_exists("thumbnail",$subdefList)) {
+                $defaultKey = 'thumbnail';
+            }
 
             $outputVars = [
                 'isAvailable' => true,
-                'preview' => [
-                    'permalinkUrl' => $permalinkUrl,
-                    'permaviewUrl' => $permaviewUrl,
-                    'embedUrl' => $embedUrl,
-                    'width' => $previewWidth,
-                    'height' => $previewHeight
-                ]
+                'subdefList' => $subdefList,
+                'defaultKey' => $defaultKey
             ];
         }
+
+
+//        if (!$this->getAclForUser()->has_access_to_subdef($record, 'thumbnail')) {
+//            $this->app->abort(403);
+//        }
+
+//        $preview = $record->get_preview();
+//
+//        if (null !== $previewLink = $preview->get_permalink()) {
+//            $permalinkUrl = $previewLink->get_url();
+//            $permaviewUrl = $previewLink->get_page();
+//            $previewWidth = $preview->get_width();
+//            $previewHeight = $preview->get_height();
+//
+//            $embedUrl = $this->app->url('alchemy_embed_view', ['url' => (string)$permalinkUrl]);
+//
+//            $outputVars = [
+//                'isAvailable' => true,
+//                'subdefList' => $subdefList,
+//                'preview' => [
+//                    'permalinkUrl' => $permalinkUrl,
+//                    'permaviewUrl' => $permaviewUrl,
+//                    'embedUrl' => $embedUrl,
+//                    'width' => $previewWidth,
+//                    'height' => $previewHeight
+//                ]
+//            ];
+//        }
 
         return $this->renderResponse('prod/Share/record.html.twig', $outputVars);
     }
