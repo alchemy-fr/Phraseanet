ddc12d54d575ec1aefdfcee29c133ccaf0a1e613
Mike Ng <mike.esokia@gmail.com>
2017-10-12 10:29:45 +0400

    add hidden field in symfony form to keep track of selected tab

--- lib/Alchemy/Phrasea/SearchEngine/Elastic/ElasticsearchOptions.php
+++ lib/Alchemy/Phrasea/SearchEngine/Elastic/ElasticsearchOptions.php
@@ -11,6 +11,10 @@ namespace Alchemy\Phrasea\SearchEngine\Elastic;
 
 class ElasticsearchOptions
 {
+    const POPULATE_ORDER_RID = "RECORD_ID";
+    const POPULATE_ORDER_MODDATE = "MODIFICATION_DATE";
+    const POPULATE_DIRECTION_ASC = "ASC";
+    const POPULATE_DIRECTION_DESC = "DESC";
     /** @var string */
     private $host;
     /** @var int */
@@ -31,14 +35,9 @@ class ElasticsearchOptions
     private $populateOrder;
     /** @var string */
     private $populateDirection;
-
     /** @var  int[] */
     private $_customValues;
-
-    const POPULATE_ORDER_RID  = "RECORD_ID";
-    const POPULATE_ORDER_MODDATE = "MODIFICATION_DATE";
-    const POPULATE_DIRECTION_ASC  = "ASC";
-    const POPULATE_DIRECTION_DESC = "DESC";
+    private $activeTab;
 
     /**
      * Factory method to hydrate an instance from serialized options
@@ -49,16 +48,17 @@ class ElasticsearchOptions
     public static function fromArray(array $options)
     {
         $defaultOptions = [
-            'host' => '127.0.0.1',
-            'port' => 9200,
-            'index' => '',
-            'shards' => 3,
-            'replicas' => 0,
-            'minScore' => 4,
-            'highlight' => true,
-            'max_result_window' => 500000,
-            'populate_order' => self::POPULATE_ORDER_RID,
+            'host'               => '127.0.0.1',
+            'port'               => 9200,
+            'index'              => '',
+            'shards'             => 3,
+            'replicas'           => 0,
+            'minScore'           => 4,
+            'highlight'          => true,
+            'max_result_window'  => 500000,
+            'populate_order'     => self::POPULATE_ORDER_RID,
             'populate_direction' => self::POPULATE_DIRECTION_DESC,
+            'activeTab'          => null,
         ];
         foreach(self::getAggregableTechnicalFields() as $k => $f) {
             $defaultOptions[$k.'_limit'] = 0;
@@ -77,241 +77,12 @@ class ElasticsearchOptions
         $self->setMaxResultWindow($options['max_result_window']);
         $self->setPopulateOrder($options['populate_order']);
         $self->setPopulateDirection($options['populate_direction']);
+        $self->setActiveTab($options['activeTab']);
         foreach(self::getAggregableTechnicalFields() as $k => $f) {
             $self->setAggregableFieldLimit($k, $options[$k.'_limit']);
         }
 
         return $self;
-    }
-
-    /**
-     * @return array
-     */
-    public function toArray()
-    {
-        $ret = [
-            'host' => $this->host,
-            'port' => $this->port,
-            'index' => $this->indexName,
-            'shards' => $this->shards,
-            'replicas' => $this->replicas,
-            'minScore' => $this->minScore,
-            'highlight' => $this->highlight,
-            'maxResultWindow' => $this->maxResultWindow,
-            'populate_order' => $this->populateOrder,
-            'populate_direction' => $this->populateDirection,
-        ];
-        foreach(self::getAggregableTechnicalFields() as $k => $f) {
-            $ret[$k.'_limit'] = $this->getAggregableFieldLimit($k);
-        }
-
-        return $ret;
-    }
-
-    /**
-     * @param string $order
-     * @return bool returns false if order is invalid
-     */
-    public function setPopulateOrder($order)
-    {
-        $order = strtoupper($order);
-        if(in_array($order, [self::POPULATE_ORDER_RID, self::POPULATE_ORDER_MODDATE])) {
-            $this->populateOrder = $order;
-
-            return true;
-        }
-
-        return false;
-    }
-
-    /**
-     * @param string $direction
-     * @return bool returns false if direction is invalid
-     */
-    public function setPopulateDirection($direction)
-    {
-        $direction = strtoupper($direction);
-        if(in_array($direction, [self::POPULATE_DIRECTION_DESC, self::POPULATE_DIRECTION_ASC])) {
-            $this->populateDirection = $direction;
-
-            return true;
-        }
-
-        return false;
-    }
-
-    /**
-     * @return string
-     */
-    public function getPopulateOrderAsSQL()
-    {
-        static $orderAsColumn = [
-            self::POPULATE_ORDER_RID     => "`record_id`",
-            self::POPULATE_ORDER_MODDATE => "`moddate`",
-        ];
-        // populateOrder IS one of the keys (ensured by setPopulateOrder)
-        return $orderAsColumn[$this->populateOrder];
-    }
-
-    /**
-     * @return string
-     */
-    public function getPopulateDirectionAsSQL()
-    {
-        // already a SQL word
-        return $this->populateDirection;
-    }
-
-    /**
-     * @param string $host
-     */
-    public function setHost($host)
-    {
-        $this->host = $host;
-    }
-
-    /**
-     * @return string
-     */
-    public function getHost()
-    {
-        return $this->host;
-    }
-
-    /**
-     * @param int $port
-     */
-    public function setPort($port)
-    {
-        $this->port = (int)$port;
-    }
-
-    /**
-     * @return int
-     */
-    public function getPort()
-    {
-        return $this->port;
-    }
-
-    /**
-     * @param int $minScore
-     */
-    public function setMinScore($minScore)
-    {
-        $this->minScore = (int)$minScore;
-    }
-
-    /**
-     * @return int
-     */
-    public function getMinScore()
-    {
-        return $this->minScore;
-    }
-
-    /**
-     * @param string $indexName
-     */
-    public function setIndexName($indexName)
-    {
-        $this->indexName = $indexName;
-    }
-
-    /**
-     * @return string
-     */
-    public function getIndexName()
-    {
-        return $this->indexName;
-    }
-
-    /**
-     * @param int $shards
-     */
-    public function setShards($shards)
-    {
-        $this->shards = (int)$shards;
-    }
-
-    /**
-     * @return int
-     */
-    public function getShards()
-    {
-        return $this->shards;
-    }
-
-    /**
-     * @param int $replicas
-     */
-    public function setReplicas($replicas)
-    {
-        $this->replicas = (int)$replicas;
-    }
-
-    /**
-     * @return int
-     */
-    public function getReplicas()
-    {
-        return $this->replicas;
-    }
-
-    /**
-     * @return bool
-     */
-    public function getHighlight()
-    {
-        return $this->highlight;
-    }
-
-    /**
-     * @param bool $highlight
-     */
-    public function setHighlight($highlight)
-    {
-        $this->highlight = $highlight;
-    }
-
-    /**
-     * @param int $maxResultWindow
-     */
-    public function setMaxResultWindow($maxResultWindow)
-    {
-        $this->maxResultWindow = (int)$maxResultWindow;
-    }
-
-    /**
-     * @return int
-     */
-    public function getMaxResultWindow()
-    {
-        return $this->maxResultWindow;
-    }
-
-
-    public function setAggregableFieldLimit($key, $value)
-    {
-        $this->_customValues[$key.'_limit'] = $value;
-    }
-
-    public function getAggregableFieldLimit($key)
-    {
-        return $this->_customValues[$key.'_limit'];
-    }
-
-    public function __get($key)
-    {
-        if(!array_key_exists($key, $this->_customValues)) {
-            $this->_customValues[$key] = 0;
-        }
-        return $this->_customValues[$key];
-    }
-
-    public function __set($key, $value)
-    {
-        $this->_customValues[$key] = $value;
     }
 
     public static function getAggregableTechnicalFields()
@@ -396,5 +167,247 @@ class ElasticsearchOptions
                 'query' => 'meta.MimeType:%s',
             ],
         ];
+    }
+
+    /**
+     * @param string $order
+     * @return bool returns false if order is invalid
+     */
+    public function setPopulateOrder($order)
+    {
+        $order = strtoupper($order);
+        if (in_array($order, [self::POPULATE_ORDER_RID, self::POPULATE_ORDER_MODDATE])) {
+            $this->populateOrder = $order;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param string $direction
+     * @return bool returns false if direction is invalid
+     */
+    public function setPopulateDirection($direction)
+    {
+        $direction = strtoupper($direction);
+        if (in_array($direction, [self::POPULATE_DIRECTION_DESC, self::POPULATE_DIRECTION_ASC])) {
+            $this->populateDirection = $direction;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public function setAggregableFieldLimit($key, $value)
+    {
+        $this->_customValues[$key . '_limit'] = $value;
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        $ret = [
+            'host'               => $this->host,
+            'port'               => $this->port,
+            'index'              => $this->indexName,
+            'shards'             => $this->shards,
+            'replicas'           => $this->replicas,
+            'minScore'           => $this->minScore,
+            'highlight'          => $this->highlight,
+            'maxResultWindow'    => $this->maxResultWindow,
+            'populate_order'     => $this->populateOrder,
+            'populate_direction' => $this->populateDirection,
+            'activeTab'          => $this->activeTab
+        ];
+        foreach (self::getAggregableTechnicalFields() as $k => $f) {
+            $ret[$k . '_limit'] = $this->getAggregableFieldLimit($k);
+        }
+
+        return $ret;
+    }
+
+    public function getAggregableFieldLimit($key)
+    {
+        return $this->_customValues[$key . '_limit'];
+    }
+
+    /**
+     * @return string
+     */
+    public function getPopulateOrderAsSQL()
+    {
+        static $orderAsColumn = [
+            self::POPULATE_ORDER_RID     => "`record_id`",
+            self::POPULATE_ORDER_MODDATE => "`moddate`",
+        ];
+
+        // populateOrder IS one of the keys (ensured by setPopulateOrder)
+        return $orderAsColumn[$this->populateOrder];
+    }
+
+    /**
+     * @return string
+     */
+    public function getPopulateDirectionAsSQL()
+    {
+        // already a SQL word
+        return $this->populateDirection;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHost()
+    {
+        return $this->host;
+    }
+
+    /**
+     * @param string $host
+     */
+    public function setHost($host)
+    {
+        $this->host = $host;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPort()
+    {
+        return $this->port;
+    }
+
+    /**
+     * @param int $port
+     */
+    public function setPort($port)
+    {
+        $this->port = (int)$port;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMinScore()
+    {
+        return $this->minScore;
+    }
+
+    /**
+     * @param int $minScore
+     */
+    public function setMinScore($minScore)
+    {
+        $this->minScore = (int)$minScore;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIndexName()
+    {
+        return $this->indexName;
+    }
+
+    /**
+     * @param string $indexName
+     */
+    public function setIndexName($indexName)
+    {
+        $this->indexName = $indexName;
+    }
+
+    /**
+     * @return int
+     */
+    public function getShards()
+    {
+        return $this->shards;
+    }
+
+    /**
+     * @param int $shards
+     */
+    public function setShards($shards)
+    {
+        $this->shards = (int)$shards;
+    }
+
+    /**
+     * @return int
+     */
+    public function getReplicas()
+    {
+        return $this->replicas;
+    }
+
+    /**
+     * @param int $replicas
+     */
+    public function setReplicas($replicas)
+    {
+        $this->replicas = (int)$replicas;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getHighlight()
+    {
+        return $this->highlight;
+    }
+
+    /**
+     * @param bool $highlight
+     */
+    public function setHighlight($highlight)
+    {
+        $this->highlight = $highlight;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMaxResultWindow()
+    {
+        return $this->maxResultWindow;
+    }
+
+    /**
+     * @param int $maxResultWindow
+     */
+    public function setMaxResultWindow($maxResultWindow)
+    {
+        $this->maxResultWindow = (int)$maxResultWindow;
+    }
+
+    public function getActiveTab()
+    {
+        return $this->activeTab;
+    }
+
+    public function setActiveTab($activeTab)
+    {
+        $this->activeTab = $activeTab;
+    }
+
+    public function __get($key)
+    {
+        if (!array_key_exists($key, $this->_customValues)) {
+            $this->_customValues[$key] = 0;
+        }
+
+        return $this->_customValues[$key];
+    }
+
+    public function __set($key, $value)
+    {
+        $this->_customValues[$key] = $value;
     }
 }
