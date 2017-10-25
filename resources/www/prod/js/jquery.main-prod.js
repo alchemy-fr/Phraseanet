f992015f5ad929d86cf069db20b96294121d1341
Mike Ng <mike.esokia@gmail.com>
2017-10-25 12:56:19 +0400

    wip - add multiple filters on facets

--- resources/www/prod/js/jquery.main-prod.js
+++ resources/www/prod/js/jquery.main-prod.js
@@ -691,57 +691,72 @@ function getFacetsTree() {
                 var query = data.node.data.query;
                 if (query) {
                     var facet = data.node.parent;
-                    selectedFacetValues[facet.title] = {
+                    var facetData = {
                         value: data.node.data,
                         mode: event.altKey ? "EXCEPT" : "AND"
                     };
+
+                    if (selectedFacetValues[facet.title] == null) {
+                        selectedFacetValues[facet.title] = [];
+                    }
+                    selectedFacetValues[facet.title].push(facetData);
                     facetCombinedSearch();
                 }
             },
             renderNode: function(event, data){
                 var facetFilter = "";
                 if(data.node.folder && !_.isUndefined(selectedFacetValues[data.node.title])) {
-                    facetFilter = selectedFacetValues[data.node.title].value.label;
-
-                    var s_label = document.createElement("SPAN");
-                    s_label.setAttribute("class", "facetFilter-label");
-                    s_label.setAttribute("title", facetFilter);
-
-                    var length = 15;
-                    var facetFilterString = facetFilter;
-                    if( facetFilterString.length > length) {
-                        facetFilterString = facetFilterString.substring(0,length) + '…';
-                    }
-                    s_label.appendChild(document.createTextNode(facetFilterString));
-
-                    var s_closer = document.createElement("A");
-                    s_closer.setAttribute("class", "facetFilter-closer");
-
-                    var s_gradient = document.createElement("SPAN");
-                    s_gradient.setAttribute("class", "facetFilter-gradient");
-                    s_gradient.appendChild(document.createTextNode("\u00A0"));
-
-                    s_label.appendChild(s_gradient);
-
-                    var s_facet = document.createElement("SPAN");
-                    s_facet.setAttribute("class", "facetFilter" + '_' + selectedFacetValues[data.node.title].mode);
-                    s_facet.appendChild(s_label);
-                    s_closer = $(s_facet.appendChild(s_closer));
-                    s_closer.data("facetTitle", data.node.title);
-
-                    s_closer.click(
-                        function(event) {
-                            event.stopPropagation();
-                            var facetTitle = $(this).data("facetTitle");
-                            delete selectedFacetValues[facetTitle];
-                            facetCombinedSearch();
-                            return false;
-                        }
-                    );
-
+                    var dataNode = document.createElement('div');
+                    dataNode.setAttribute("class", "dataNode");
                     $(".fancytree-folder", data.node.li).append(
-                        $(s_facet)
+                        dataNode
                     );
+                    _.each(selectedFacetValues[data.node.title], function (facetValue) {
+                        facetFilter = facetValue.value.label;
+
+                        var s_label = document.createElement("SPAN");
+                        s_label.setAttribute("class", "facetFilter-label");
+                        s_label.setAttribute("title", facetFilter);
+
+                        var length = 15;
+                        var facetFilterString = facetFilter;
+                        if (facetFilterString.length > length) {
+                            facetFilterString = facetFilterString.substring(0, length) + '…';
+                        }
+                        s_label.appendChild(document.createTextNode(facetFilterString));
+
+                        var s_closer = document.createElement("A");
+                        s_closer.setAttribute("class", "facetFilter-closer");
+
+                        var s_gradient = document.createElement("SPAN");
+                        s_gradient.setAttribute("class", "facetFilter-gradient");
+                        s_gradient.appendChild(document.createTextNode("\u00A0"));
+
+                        s_label.appendChild(s_gradient);
+
+                        var s_facet = document.createElement("SPAN");
+                        s_facet.setAttribute("class", "facetFilter" + '_' + facetValue.mode);
+                        s_facet.appendChild(s_label);
+                        s_closer = $(s_facet.appendChild(s_closer));
+                        s_closer.data("facetTitle", data.node.title);
+
+                        s_closer.click(
+                            function (event) {
+                                event.stopPropagation();
+                                var facetTitle = $(this).data("facetTitle");
+                                delete selectedFacetValues[facetTitle];
+                                facetCombinedSearch();
+                                return false;
+                            }
+                        );
+
+                        var newNode = document.createElement('div');
+                        newNode.setAttribute("class", "newNode");
+                        newNode.appendChild(s_facet);
+                        $(".fancytree-folder .dataNode", data.node.li).append(
+                            newNode
+                        );
+                    })
                 }
             }
         });
@@ -754,22 +769,24 @@ function facetCombinedSearch() {
     var q = $("#EDIT_query").val();
     var q_facet_and = "";
     var q_facet_except = "";
-    _.each(_.values(selectedFacetValues), function(facetValue) {
-        switch(facetValue.mode) {
-            case "AND":
-                q_facet_and += (q_facet_and ? " AND " : "") + '(' + facetValue.value.query + ')';
-                break;
-            case "EXCEPT":
-                q_facet_except += (q_facet_except ? " OR " : "") + '(' + facetValue.value.query + ')';
-                break;
-        }
+    _.each(_.values(selectedFacetValues), function (facet) {
+        _.each(facet, function (facetValue) {
+            switch (facetValue.mode) {
+                case "AND":
+                    q_facet_and += (q_facet_and ? " AND " : "") + '(' + facetValue.value.query + ')';
+                    break;
+                case "EXCEPT":
+                    q_facet_except += (q_facet_except ? " OR " : "") + '(' + facetValue.value.query + ')';
+                    break;
+            }
+        });
     });
     if(!q && !q_facet_and && q_facet_except) {
         // too bad : an except with no query.
         q = "created_on>1900/01/01";    // fake "all"
     }
     if(q_facet_and != "") {
-        if(q) {
+        if (q) {
             q = '(' + q + ') AND '
         }
         q += q_facet_and;
