f8fb8837cb6282537faecaa5d09eb730fbfe3ba2
mike-esokia <mike.esokia@gmail.com>
2018-05-23 17:00:15 +0400

    wip restore hightlights and multivalue seperator

--- lib/Alchemy/Phrasea/Core/Provider/TwigServiceProvider.php
+++ lib/Alchemy/Phrasea/Core/Provider/TwigServiceProvider.php
@@ -130,17 +130,18 @@ class TwigServiceProvider implements ServiceProviderInterface
         $twig->addFilter(new \Twig_SimpleFilter('parseColor', function (\Twig_Environment $twig, $string) use ($app) {
             $re = '/^(.*)\[#([0-9a-fA-F]{6})]$/m';
             $stringArr = explode(';', $string);
+
             foreach ($stringArr as $key => $value) {
                 preg_match_all($re, trim($value), $matches);
                 if ($matches && $matches[1] != null && $matches[2] != null) {
                     $colorCode = '#' . $matches[2][0];
                     $colorName = $matches[1][0];
 
-                    $stringArr[$key] = '<span class="color-dot" style="margin-right: 4px; background-color: ' . $colorCode . '"></span>' . $colorName . '<br />';
+                    $stringArr[$key] = '<span style="white-space: nowrap;"><span class="color-dot" style="margin-right: 4px; background-color: ' . $colorCode . '"></span>' . $colorName . '</span>';
                 }
             }
 
-            return implode('', $stringArr);
+            return implode('; ', $stringArr);
         }, ['needs_environment' => true, 'is_safe' => ['html']]));
 
         $twig->addFilter(new \Twig_SimpleFilter('bounce',
