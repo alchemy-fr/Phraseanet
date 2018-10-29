2aa58ed734de12deed9c82ac2d57424f991a4425
Filip Vilic <filip.t.vilic@gmail.com>
2018-10-29 15:41:03 +0100

    PHRAS-2033_build_json_query

--- resources/www/prod/js/jquery.main-prod.js
+++ resources/www/prod/js/jquery.main-prod.js
@@ -520,12 +520,17 @@ function initAnswerForm() {
         var $this = $(this),
             method = $this.attr('method') ? $this.attr('method') : 'POST';
 
+        loadFacets(lastFilterResults);
+
         var data = $this.serializeArray();
+        var jsonData = serializeJSON(data);
+
+        console.log(jsonData);
 
         answAjax = $.ajax({
             type: method,
             url: $this.attr('action'),
-            data: data,
+            data: jsonData,
             dataType: 'json',
             beforeSend: function (formData) {
                 if (answAjaxrunning && answAjax.abort)
@@ -558,9 +563,7 @@ function initAnswerForm() {
                 });
 
                 //load last result collected or [] if length == 0
-                if (datas.facets.length == 0) {
-                    loadFacets(lastFilterResults);
-                } else {
+                if (datas.facets.length > 0) {
                     lastFilterResults = datas.facets;
                     loadFacets(datas.facets);
                 }
@@ -962,8 +965,147 @@ function facetCombinedSearch() {
     newSearch(q);
 }
 
+function serializeJSON(data) {
+    var json = {},
+        obj = {},
+        bases = [],
+        statuses = [],
+        fields = [],
+        aggregates = [],
+        databoxName = function(id, col) {            
+            var str = $.trim($('#ADVSRCH_SB_ZONE_' + id + ' td:nth-child('+ (col + 1) +') .custom_checkbox_label').text());
+            return str.replace(/ /g,"_");
+        };
 
+    $.each(data, function(i, el) {
+        obj[el.name] = el.value;
 
+        var col = parseInt(el.value);
+
+        if(el.name === 'bases[]') {
+            bases.push(col);
+        }
+
+        if(el.name.startsWith('status')) {
+            var databoxId = el.name.match(/\d+/g)[0],
+                databoxRow = el.name.match(/\d+/g)[1],
+                statusMatch = false;
+            
+            $.each(statuses, function(i, status) {
+                
+                if (status.databox === databoxId) {                    
+                    for (let j = 0; j < status.status.length; j++) {
+                        var st = status.status[j].name;
+                        var st_id = st.substr(0, st.indexOf(':'));
+                        
+                        if (st_id === databoxRow) {
+                            statusMatch = true;                            
+                        }
+                    }
+                    statuses.splice((databoxId -1), 1);
+                }
+            });
+            if (!statusMatch) {
+                statuses.push({
+                    'databox': databoxId,
+                    'status': [
+                        {
+                            'name': databoxRow + ':' + databoxName(databoxId, col),
+                            'value': !!(parseInt(el.value))
+                        }
+                    ]
+                });
+            }
+        }
+    });
+
+    $('.term_select_field').each(function(i, el) {
+        if ($(el).val()) {
+            fields.push({
+                'type': 'TEXT-FIELD',
+                'field': 'field.' + $(el).val(),
+                'operator': $(el).next().val() === 'contains' ? ":" : "=",
+                'value': $(el).next().next().val(),
+                "enabled": true
+            });
+        }
+    }); 
+    
+    $('.fancytree-node.fancytree-folder').each(function(i, el) {
+        
+        var facetFilterTitle = $(el).find('.fancytree-title').text();
+        var facetFilterTitleS = facetFilterTitle.replace(/\s+/, '-');
+        var negated = false,
+            enabled = true,
+            nodeEl = $(el).find('[class^="facetFilter_"]');
+
+        if (nodeEl.is('[class$="_EXCEPT"]')) {
+            negated = true;
+        }
+
+        _.each(selectedFacetValues[facetFilterTitle], function(facet) {
+            var query = facet.value.query;
+            var value = query.substr(0, query.indexOf(':'));
+            
+            aggregates.push({
+                'type': facetFilterTitleS.toUpperCase() + '-AGGREGATE',
+                'field': value.startsWith('meta') ? value.replace('meta', 'metadata_tags') : value,
+                'value': facet.value.label,
+                'negated': negated,
+                'enabled': enabled
+            });
+        });
+    });
+    
+    json['sort'] = {
+        'field': obj.sort,
+        'order': obj.ord
+    };
+    json['perpage'] = parseInt($('#nperpage_value').val());
+    json['page'] = obj.pag === "" ? 1 : parseInt(obj.pag);
+    json['use_truncation'] = obj.truncation === "on" ? true : false;
+    json['phrasea_recordtype'] = obj.search_type === 0 ? 'RECORD' : 'STORY';
+    json['phrasea_mediatype'] = obj.record_type.toUpperCase();
+    json['bases'] = bases;
+    json['statuses'] = statuses;
+    json['query'] = {
+        '_ux_zone': $('.menu-bar .selectd').text().toUpperCase(),
+        'type': 'CLAUSES',
+        'must_match': 'ALL',
+        'enabled': true,
+        'clauses': [
+            {
+                '_ux_zone': 'FULLTEXT',
+                'type': 'FULLTEXT',
+                'value': obj.fake_qry,
+                'enabled': true
+            },
+            {
+                '_ux_zone': 'FIELDS',
+                'type': 'CLAUSES',
+                'must_match': obj.must_match,
+                'enabled': true,
+                'clauses': fields
+            },
+            {
+                "type": "DATE-FIELD",
+                "field": "DATE",
+                "from": $('#ADVSRCH_DATE_ZONE input[name=date_min]', 'form.phrasea_query .adv_options').val(),
+                "to": $('#ADVSRCH_DATE_ZONE input[name=date_max]', 'form.phrasea_query .adv_options').val(),
+                "enabled": true
+            },
+            {
+                "_ux_zone": "AGGREGATES",
+                "type": "CLAUSES",
+                "must_match": "ALL",
+                "enabled": true,
+                "clauses": aggregates
+            }
+        ]
+    }
+
+    return JSON.stringify(json);
+}
 
 
 $(document).ready(function() {
