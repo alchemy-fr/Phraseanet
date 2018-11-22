c58ed453334a0dd5ae4bd5d0a23a85e65ca36dc2
aina-esokia <asr@esokia-webagency.com>
2018-11-22 14:32:42 +0400

    fix prod escaping

--- lib/classes/record/adapter.php
+++ lib/classes/record/adapter.php
@@ -939,7 +939,7 @@ class record_adapter implements RecordInterface, cache_cacheableInterface
             $this->set_data_to_cache(self::CACHE_TITLE, $title);
         }
 
-        return $title;
+        return htmlspecialchars($title);
     }
 
     /**
