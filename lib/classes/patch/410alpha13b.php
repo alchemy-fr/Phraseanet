46ec167c1a06ab9509d2e25df35fb9e291d0df78
aina-esokia <asr@esokia-webagency.com>
2018-12-21 15:12:28 +0400

    update

--- lib/classes/patch/410alpha13b.php
+++ lib/classes/patch/410alpha13b.php
@@ -8,6 +8,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 use Alchemy\Phrasea\Application;
 
 class patch_410alpha13b implements patchInterface
