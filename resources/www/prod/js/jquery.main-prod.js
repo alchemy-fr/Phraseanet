8c9950ad2647942773f270011365eebaad2d4b88
Harrys Ravalomanana <hra@esokia.com>
2019-04-16 11:06:26 +0400

    PHRAS-2510-PROD__Facets-facets_values_are_trucated_at_15_characters

--- resources/www/prod/js/jquery.main-prod.js
+++ resources/www/prod/js/jquery.main-prod.js
@@ -730,7 +730,7 @@ function loadFacets(facets) {
         var values = _.map(_.sortBy(facet.values, sortIteration), function (value) {
             var type = facet.type;     // todo : define a new phraseanet "color" type for fields. for now we push a "type" for every value, copied from field type
             // patch "color" type values
-            var textLimit = 15;     // cut long values (set to 0 to not cut)
+            var textLimit = 200;     // cut long values (set to 0 to not cut)
             var text = (value.value).toString();
             var label = title = tooltip = text;
             var match = text.match(/^(.*)\[#([0-9a-fA-F]{6})].*$/);
@@ -1314,7 +1314,7 @@ function serializeJSON(data, selectedFacets, facets) {
     json['perpage'] = parseInt($('#nperpage_value').val());
     json['page'] = obj.pag === "" ? 1 : parseInt(obj.pag);
     json['use_truncation'] = (obj.truncation === "on");
-    json['phrasea_recordtype'] = obj.search_type == 0 ? 'RECORD' : 'STORY';
+    json['phrasea_recordtype'] = obj.search_type == 1 ? 'STORY' : 'RECORD';
     json['phrasea_mediatype'] = obj.record_type.toUpperCase();
     json['bases'] = bases;
     json['statuses'] = statuses;
