ed5d23579b378fdac8393e568804e3ba8341c1c2
MilosEsokia <mim@esokia-webagency.com>
2019-04-25 09:10:16 +0200

    Removing install from DownloadPlugin command

--- lib/Alchemy/Phrasea/Command/Plugin/DownloadPlugin.php
+++ lib/Alchemy/Phrasea/Command/Plugin/DownloadPlugin.php
@@ -28,9 +28,9 @@ class DownloadPlugin extends AbstractPluginCommand
         parent::__construct('plugins:download');
 
         $this
-            ->setDescription('Downloads and installs a plugin to Phraseanet')
+            ->setDescription('Downloads a plugin to Phraseanet')
             ->addArgument('source', InputArgument::REQUIRED, 'The source is a remote url (.zip or .git)')
-            ->addArgument('destination', InputArgument::OPTIONAL, 'Temporary download destination');
+            ->addArgument('destination', InputArgument::OPTIONAL, 'Download destination');
     }
 
 
@@ -48,7 +48,7 @@ class DownloadPlugin extends AbstractPluginCommand
             $destination = ltrim($destination, '/');
             $destination = rtrim($destination, '/');
 
-            $local_download_path = $destination . $destination_subdir;
+            $local_download_path = $destination;
 
         } else {
 
@@ -59,13 +59,13 @@ class DownloadPlugin extends AbstractPluginCommand
             mkdir($local_download_path, 0755, true);
         }
 
-        $local_unpack_path = 'tmp/plugin-unpack'. $destination_subdir;
+        $local_unpack_path = 'tmp/plugin-zip'. $destination_subdir;
 
         if (!is_dir($local_unpack_path)) {
             mkdir($local_unpack_path, 0755, true);
         }
 
-        $local_archive_file = $local_download_path . '/plugin-downloaded.zip';
+        $local_archive_file = $local_unpack_path . '/plugin-downloaded.zip';
 
         $extension = $this->validateSource($source);
 
@@ -93,7 +93,7 @@ class DownloadPlugin extends AbstractPluginCommand
 
                     if ($zip->open($local_archive_file)) {
                         for ($i = 0; $i < $zip->numFiles; $i++) {
-                            if (!($zip->extractTo($local_unpack_path, array($zip->getNameIndex($i))))) {
+                            if (!($zip->extractTo($local_download_path, array($zip->getNameIndex($i))))) {
                                 $error_unpack = true;
                             }
                         }
@@ -103,29 +103,18 @@ class DownloadPlugin extends AbstractPluginCommand
                     if ($error_unpack){
                         $output->writeln("Failed unzipping <info>$source</info>");
                     } else {
-
-                        // check if composer.json is present in root of extracted files or in subdirectory (git.zip default)
-                        $is_composer_in_root = false;
-                        $ffs = scandir($local_unpack_path);
-                        foreach ($ffs as $ff){
-                            if (is_dir($local_unpack_path.'/'.$ff)){
-                                $local_plugin_source = $local_unpack_path.'/'.$ff;
-                            }
-                            if ($ff == 'composer.json'){
-                                $is_composer_in_root = true;
-                            }
-                        }
-                        if ($is_composer_in_root) {
-                            $local_plugin_source = $local_unpack_path;
-                        }
+                        $output->writeln("Plugin downloaded to <info>$local_download_path</info>");
                     }
+
+                    // remove zip archive
+                    $this->delDirTree($local_unpack_path);
 
                     break;
 
                 case 'git':
                     $output->writeln("Downloading <info>$source</info>...");
                     $repo = GitRepository::cloneRepository($source, $local_download_path);
-                    $local_plugin_source = $local_download_path;
+                    $output->writeln("Plugin downloaded to <info>$local_download_path</info>");
                     break;
 
             }
@@ -133,21 +122,7 @@ class DownloadPlugin extends AbstractPluginCommand
         } else {
 
             $output->writeln("The source <info>$source</info> is not valid remote(.zip or .git)");
-
         }
-
-        $command = $this->getApplication()->find('plugins:add');
-        $arguments = [
-            'command' => 'plugins:add',
-            'source'  => $local_plugin_source
-        ];
-
-        $downloadInput = new ArrayInput($arguments);
-        $returnCode = $command->run($downloadInput, $output);
-
-
-        // remove unpacked archive, keep zip file
-        $this->delDirTree($local_unpack_path);
 
         return 0;
     }
