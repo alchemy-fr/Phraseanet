5a703f207ae9dab9f1b0d6d70b20d86744d5c020
aynsix <asr@esokia-webagency.com>
2019-12-12 12:13:56 +0400

    fix record order in story

--- lib/Alchemy/Phrasea/Databox/Record/LegacyRecordRepository.php
+++ lib/Alchemy/Phrasea/Databox/Record/LegacyRecordRepository.php
@@ -140,6 +140,7 @@ class LegacyRecordRepository implements RecordRepository
                 's.rid_parent IN (:storyIds)',
                 'r.parent_record_id = 0'
             )
+            ->orderBy('s.ord', 'ASC')
             ->setParameter('storyIds', $storyIds, Connection::PARAM_INT_ARRAY)
         ;
 
