72d2bc028d0ad328c01922f448f43b4819ac6599
nmaillat <maillat@alchemy.fr>
2020-07-06 20:13:27 +0200

    set gateway fpm timeout

--- docker/nginx/root/entrypoint.sh
+++ docker/nginx/root/entrypoint.sh
@@ -3,5 +3,6 @@
 set -xe
 
 cat /nginx.conf.sample | sed "s/\$MAX_BODY_SIZE/$MAX_BODY_SIZE/g" > /etc/nginx/conf.d/default.conf
+cat /nginx.conf.sample | sed "s/\$GATEWAY_SEND_TIMEOUT/$GATEWAY_SEND_TIMEOUT/g" > /etc/nginx/conf.d/default.conf
 
 exec "$@"
