41591356a1bd1c6baa9b315b9afe745f0c62d6f9
Moctar <diouf@alchemy.fr>
2020-07-16 16:09:36 +0200

    add cgi and proxy nginx timeout parameters

--- docker/nginx/root/entrypoint.sh
+++ docker/nginx/root/entrypoint.sh
@@ -2,6 +2,6 @@
 
 set -xe
 
-cat /nginx.conf.sample | sed "s/\$MAX_BODY_SIZE/$MAX_BODY_SIZE/g" | sed "s/\$GATEWAY_SEND_TIMEOUT/$GATEWAY_SEND_TIMEOUT/g" > /etc/nginx/conf.d/default.conf
-
+cat /nginx.conf.sample | sed "s/\$MAX_BODY_SIZE/$MAX_BODY_SIZE/g" | sed "s/\$GATEWAY_SEND_TIMEOUT/$GATEWAY_SEND_TIMEOUT/g"  | sed "s/\$GATEWAY_FASTCGI_TIMEOUT/$GATEWAY_FASTCGI_TIMEOUT/g" | sed "s/\$MAX_BODY_SIZE/$MAX_BODY_SIZE/g" | sed "s/\$GATEWAY_PROXY_TIMEOUT/$GATEWAY_PROXY_TIMEOUT/g"  > /etc/nginx/conf.d/default.conf
+cat /fastcgi_timeout.conf  | sed "s/\$GATEWAY_FASTCGI_TIMEOUT/$GATEWAY_FASTCGI_TIMEOUT/g" >> /etc/nginx/fastcgi_params
 exec "$@"
