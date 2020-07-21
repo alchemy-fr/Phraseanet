1314850358cf2b7c6010c91941263f48c52ebaea
Moctar <diouf@alchemy.fr>
2020-07-21 12:52:20 +0200

    fix fastcgi_params duplicating values

--- docker/nginx/root/entrypoint.sh
+++ docker/nginx/root/entrypoint.sh
@@ -3,5 +3,5 @@
 set -xe
 
 cat /nginx.conf.sample | sed "s/\$MAX_BODY_SIZE/$MAX_BODY_SIZE/g" | sed "s/\$GATEWAY_SEND_TIMEOUT/$GATEWAY_SEND_TIMEOUT/g"  | sed "s/\$GATEWAY_FASTCGI_TIMEOUT/$GATEWAY_FASTCGI_TIMEOUT/g" | sed "s/\$MAX_BODY_SIZE/$MAX_BODY_SIZE/g" | sed "s/\$GATEWAY_PROXY_TIMEOUT/$GATEWAY_PROXY_TIMEOUT/g"  > /etc/nginx/conf.d/default.conf
-cat /fastcgi_timeout.conf  | sed "s/\$GATEWAY_FASTCGI_TIMEOUT/$GATEWAY_FASTCGI_TIMEOUT/g" >> /etc/nginx/fastcgi_params
+cat /fastcgi_timeout.conf  | sed "s/\$GATEWAY_FASTCGI_TIMEOUT/$GATEWAY_FASTCGI_TIMEOUT/g" > /etc/nginx/fastcgi_extended_params
 exec "$@"
