c456320d97f023172eaecba20dfcaebbc2893545
jygaulier <gaulier@alchemy.fr>
2020-09-08 15:53:07 +0200

    - mark command as experimental - better formatting - add : --now and --days to allow overriding defaults and listing past/future

--- lib/Alchemy/Phrasea/Command/SendValidationRemindersCommand.php
+++ lib/Alchemy/Phrasea/Command/SendValidationRemindersCommand.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of Phraseanet
  *
- * (c) 2005-2016 Alchemy
+ * (c) 2005-2020 Alchemy
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -21,9 +21,11 @@ use Alchemy\Phrasea\Model\Repositories\ValidationParticipantRepository;
 use Alchemy\Phrasea\Notification\Emitter;
 use Alchemy\Phrasea\Notification\Mail\MailInfoValidationReminder;
 use Alchemy\Phrasea\Notification\Receiver;
+use DateInterval;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -31,6 +33,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 class SendValidationRemindersCommand extends Command
 {
     use NotifierAware;
+
+    const DATE_FMT = "Y-m-d H:i:s";
 
     /** @var InputInterface */
     private $input;
@@ -41,13 +45,19 @@ class SendValidationRemindersCommand extends Command
     /** @var bool */
     private $dry;
 
+    /** @var DateTime */
+    private $now;
+
+    private $days;
 
     public function __construct( /** @noinspection PhpUnusedParameterInspection */ $name = null)
     {
         parent::__construct('validation:remind');
 
-        $this->setDescription('Send validation reminders.');
+        $this->setDescription('Send validation reminders. <comment>(experimental)</comment>');
         $this->addOption('dry',null, InputOption::VALUE_NONE,'dry run, list but don\'t act');
+        $this->addOption('now', null,InputArgument::OPTIONAL, 'fake today');
+        $this->addOption('days', null,InputArgument::OPTIONAL, 'overwrite validation-reminder-days');
     }
 
 
@@ -58,9 +68,37 @@ class SendValidationRemindersCommand extends Command
      */
     protected function sanitizeArgs()
     {
+        $r = true;
+
+        // --dry
         $this->dry  = $this->input->getOption('dry') ? true : false;
 
-        return true;
+        // --now
+        if(($v = $this->input->getOption('now')) !== null) {
+            try {
+                $this->now = new DateTime($v);
+            }
+            catch (Exception $e) {
+                $this->output->writeln(sprintf('<error>bad --date "%s"</error>', $v));
+                $r = false;
+            }
+        }
+        else {
+            $this->now = new DateTime();
+        }
+
+        // --days
+        if(($v = $this->input->getOption('days')) !== null) {
+            if(($this->days = (int)$v) <= 0) {
+                $this->output->writeln(sprintf('<error>--days must be > 0 (bad value "%s")</error>', $v));
+                $r = false;
+            }
+        }
+        else {
+            $this->days = (int)$this->getConf()->get(['registry', 'actions', 'validation-reminder-days']);
+        }
+
+        return $r;
     }
 
     protected function doExecute(InputInterface $input, OutputInterface $output)
@@ -74,43 +112,103 @@ class SendValidationRemindersCommand extends Command
             return -1;
         }
 
+        $date_to = clone($this->now);
+        $interval = sprintf('P%dD', $this->days);
         try {
-            $date = new DateTime('+' . (int)$this->getConf()->get(['registry', 'actions', 'validation-reminder-days']) . ' days');
+            $date_to->add(new DateInterval($interval));
         }
-        catch (Exception $e) {
-            $output->writeln('<error>Bad or unset setting "registry.actions.validation-reminder-days"</error>');
-            return 1;
+        catch(Exception $e) {
+            $this->output->writeln(sprintf('<error>Bad interval "%s" ?</error>', $interval));
+            return -1;
         }
 
         if($this->dry) {
             $this->output->writeln('<info>dry mode : emails will NOT be sent</info>');
         }
 
+        $output->writeln(sprintf('from "%s" to "%s" (days=%d), ', $this->now->format(self::DATE_FMT), $date_to->format(self::DATE_FMT), $this->days));
 
-        foreach ($this->getValidationParticipantRepository()->findNotConfirmedAndNotRemindedParticipantsByExpireDate($date) as $participant) {
+        $fmt = '   participant: %-11s  user: %-10s %s  token: %-10s ';
+        //$output->writeln(sprintf($fmt, 'session', 'basket', 'participant', 'user', 'token', 'email'));
+
+        $last_session = null;
+        foreach ($this->getValidationParticipantRepository()->findNotConfirmedAndNotRemindedParticipantsByExpireDate($date_to, $this->now) as $participant) {
             $validationSession = $participant->getSession();
             $basket = $validationSession->getBasket();
-            $user = $participant->getUser();
+
+            // change session ? display header
+            if($validationSession->getId() !== $last_session) {
+                try {
+                    $basket_name = $basket->getName();
+                }
+                catch(Exception $e) {
+                    // basket not found ?
+                    $basket_name = '?';
+                }
+
+                try {
+                    $initiator_email = $validationSession->getInitiator()->getEmail();
+                }
+                catch(Exception $e) {
+                    // initiator user not found ?
+                    $initiator_email = '?';
+                }
+
+                $output->writeln('');
+                $output->writeln(sprintf('session_id: %s (created %s by "%s", expire %s), basket_id: %s ("%s")',
+                    $validationSession->getId(),
+                    $validationSession->getCreated()->format(self::DATE_FMT),
+                    $initiator_email,
+                    $validationSession->getExpires()->format(self::DATE_FMT),
+                    $basket->getId(),
+                    $basket_name
+                ));
+
+                $last_session = $validationSession->getId();
+            }
+
+            // now display participant
+            $can_send = true;
 
             // fu..ing doctrine : we can get user id if user does not exists ! we must try to hydrate to get an exception !
+            $user = $participant->getUser();        // always ok !
             try {
-                $user->getEmail();
+                $str_email = $user->getEmail();     // force to hydrate
             }
             catch (Exception $e) {
-                $output->writeln(sprintf('<error>user id %s for participant id %s not found</error>', $user->getId(), $participant->getId()));
-                continue;
+                $str_email = 'user not found';
+                $can_send = false;
             }
 
             $token = null;
             try {
                 $token = $this->getTokenRepository()->findValidationToken($basket, $user);
+                if($token) {
+                    $str_token = sprintf('%s (cre. %s, exp. %s)',
+                        $this->dotdot($token->getValue(), 10),
+                        $token->getCreated()->format(self::DATE_FMT),
+                        $token->getExpiration()->format(self::DATE_FMT)
+                    );
+                }
+                else {
+                    $str_token = '?';   // token not found
+                    $can_send = false;
+                }
             }
             catch (Exception $e) {
-                $output->writeln(sprintf('<error>error finding token for user "%s" (id=%s) on basket id %s</error>', $user->getEmail(), $user->getId(), $basket->getId()));
+                $str_token = '/!\\';    // crash while getting token
+                $can_send = false;
             }
 
-            if ($token === null) {
-                $output->writeln(sprintf('<error>token not found for user "%s" (id=%s) on basket id %s</error>', $user->getEmail(), $user->getId(), $basket->getId()));
+            $output->writeln(sprintf($fmt,
+                    $this->dotdot($participant->getId(), 10),
+                    $this->dotdot($user->getId(), 10),
+                    $this->dotdot($str_email, 30, 'left', '"', '"'),
+                    $str_token
+                )
+            );
+
+            if(!$can_send) {
                 continue;
             }
 
@@ -123,6 +221,30 @@ class SendValidationRemindersCommand extends Command
         $this->getEntityManager()->flush();
 
         return 0;
+    }
+
+    /**
+     * format a string to a specified length
+     *
+     * @param string $s
+     * @param int $l
+     * @param string $align  'left' or 'right'
+     * @param string $pfx       prefix to add, e.g '("'
+     * @param string $sfx       suffix to add, e.g '")'
+     * @return string
+     */
+    private function dotdot($s, $l, $align='left', $pfx='', $sfx='')
+    {
+        $l -= (strlen($pfx) + strlen($sfx));
+        if(strlen($s) > $l) {
+            $s = $pfx . substr($s, 0, $l-1) . "\xE2\x80\xA6" . $sfx;
+        }
+        else {
+            $spc = str_repeat(' ', $l-strlen($s));
+            $s = $align=='left' ? ($pfx . $s . $sfx . $spc) : ($spc . $pfx . $s . $sfx);
+        }
+
+        return $s;
     }
 
     private function doRemind(ValidationParticipant $participant, Basket $basket, $url)
@@ -157,7 +279,7 @@ class SendValidationRemindersCommand extends Command
             }
 
             if ($readyToSend) {
-                $this->output->writeln(sprintf('sending "%s" from "%s" to "%s"', $title, $receiver->getEmail(), $emitter->getEmail()));
+                $this->output->writeln(sprintf('    -> remind "<info>%s</info>" from "<info>%s</info>" to "<info>%s</info>"', $title, $receiver->getEmail(), $emitter->getEmail()));
                 if(!$this->dry) {
                     // for real
                     $mail = MailInfoValidationReminder::create($this->container, $receiver, $emitter);
