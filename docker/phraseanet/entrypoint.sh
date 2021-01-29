4d38dda9be9975398abc7cf0549fb79f7bae211c
gjacobjn <52928254+gjacobjn@users.noreply.github.com>
2021-01-29 14:08:23 +0100

    Update entrypoint.sh

--- docker/phraseanet/entrypoint.sh
+++ docker/phraseanet/entrypoint.sh
@@ -35,7 +35,7 @@ if [ -f "$FILE" ]; then
     bin/setup system:config set registry.api-clients.api-auth-token-header-only $PHRASEANET_API_AUTH_TOKEN_HEADER_ONLY
 
 
-    if [[ $PHRASEANET_SMTP_ENABLED && $PHRASEANET_SMTP_ENABLED = true && $PHRASEANET_ALLOW_EMAIL_SETUP == 1 ]]; then
+    if [[ $PHRASEANET_SMTP_ENABLED && $PHRASEANET_SMTP_ENABLED = true ]]; then
         bin/setup system:config set registry.email.smtp-enabled $PHRASEANET_SMTP_ENABLED
         bin/setup system:config set registry.email.smtp-auth-enabled $PHRASEANET_SMTP_AUTH_ENABLED
         bin/setup system:config set registry.email.smtp-secure-mode $PHRASEANET_SMTP_SECURE_MODE
