0a45e46d53d36bfbf71ceb7c3d6e57dc2b3b5f28
nmaillat <maillat@alchemy.fr>
2021-02-22 23:47:59 +0100

    PHRAS-3372 add debugger alloweb ip, fix issue case single entry for trusted proxies and language [SKIP CI]

--- docker/phraseanet/entrypoint.sh
+++ docker/phraseanet/entrypoint.sh
@@ -48,6 +48,7 @@ if [[ -f "$FILE" && $PHRASEANET_SETUP = 1 ]]; then
                 counter=$(( counter+1 ))
                 if [[ $counter -eq 1 ]] ; then
                     bin/setup system:config set languages.available $i
+                    bin/setup system:config add languages.available $i
                 else
                     bin/setup system:config add languages.available $i   
                 fi
@@ -64,8 +65,24 @@ if [[ -f "$FILE" && $PHRASEANET_SETUP = 1 ]]; then
                 counter=$(( counter+1 ))
                 if [[ $counter -eq 1 ]] ; then
                     bin/setup system:config set trusted-proxies $i
+                    bin/setup system:config add trusted-proxies $i
                 else
                     bin/setup system:config add trusted-proxies $i   
+                fi
+            done
+    fi
+
+    echo `date +"%Y-%m-%d %H:%M:%S"` " - Phraseanet setting for debugger allowed IP"
+    counter=0 
+    if [[ -n $PHRASEANET_DEBUG_ALLOWED_IP ]]; then
+        for i in $(echo $PHRASEANET_DEBUG_ALLOWED_IP | sed "s/,/ /g")
+            do
+                counter=$(( counter+1 ))
+                if [[ $counter -eq 1 ]] ; then
+                    bin/setup system:config set debugger.allowed-ips $i
+                    bin/setup system:config add debugger.allowed-ips $i
+                else
+                    bin/setup system:config add debugger.allowed-ips $i   
                 fi
             done
     fi
