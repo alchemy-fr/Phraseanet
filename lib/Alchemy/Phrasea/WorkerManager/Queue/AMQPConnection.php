0753cddc0f4a432e69a55e6bddd15329d8fbf1f0
aynsix <asr@esokia-webagency.com>
2021-03-19 14:02:52 +0300

    PHRAS-3146 add ttl in failed_q

--- lib/Alchemy/Phrasea/WorkerManager/Queue/AMQPConnection.php
+++ lib/Alchemy/Phrasea/WorkerManager/Queue/AMQPConnection.php
@@ -392,7 +392,9 @@ class AMQPConnection
                 ]);
                 break;
             case self::FAILED_QUEUE:
-                $this->queue_declare_and_bind($queueName, self::RETRY_ALCHEMY_EXCHANGE);
+                $this->queue_declare_and_bind($queueName, self::RETRY_ALCHEMY_EXCHANGE, [
+                    'x-message-ttl' =>  345600*1000  //  message in failed_q to be dead after 96 hours by default
+                    ]);
                 break;
             case self::BASE_QUEUE:
                 $this->queue_declare_and_bind($queueName, self::ALCHEMY_EXCHANGE);
