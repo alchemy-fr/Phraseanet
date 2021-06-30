2647580d5f1e707f89e4105533b3fe02a2ef9c57
jygaulier <gaulier@alchemy.fr>
2021-06-30 17:43:36 +0200

    PHRAS-3479_trace-crash-on-usec0 fix : "trace" do not report usec anymore

--- lib/Alchemy/Phrasea/WorkerManager/Subscriber/RecordSubscriber.php
+++ lib/Alchemy/Phrasea/WorkerManager/Subscriber/RecordSubscriber.php
@@ -210,7 +210,7 @@ class RecordSubscriber implements EventSubscriberInterface
     {
         $record = $event->getRecord();
 
-        file_put_contents(dirname(__FILE__).'/../../../../../logs/trace.txt', sprintf("%s [%s] : %s (%s); %s\n", (\DateTime::createFromFormat('U.u', microtime(TRUE)))->format('Y-m-d\TH:i:s.u'), getmypid(), __FILE__, __LINE__,
+        file_put_contents(dirname(__FILE__).'/../../../../../logs/trace.txt', sprintf("%s [%s] : %s (%s); %s\n", (date('Y-m-d\TH:i:s')), getmypid(), __FILE__, __LINE__,
             sprintf("Event WorkerEvents::SUBDEFINITION_WRITE_META catched  for %s.%s.%s", $record->getDataboxId(), $record->getRecordId(), $event->getSubdefName())
         ), FILE_APPEND | LOCK_EX);
 
@@ -288,19 +288,19 @@ class RecordSubscriber implements EventSubscriberInterface
                     ]
                 ];
 
-                file_put_contents(dirname(__FILE__).'/../../../../../logs/trace.txt', sprintf("%s [%s] : %s (%s); %s\n", (\DateTime::createFromFormat('U.u', microtime(TRUE)))->format('Y-m-d\TH:i:s.u'), getmypid(), __FILE__, __LINE__,
+                file_put_contents(dirname(__FILE__).'/../../../../../logs/trace.txt', sprintf("%s [%s] : %s (%s); %s\n", (date('Y-m-d\TH:i:s')), getmypid(), __FILE__, __LINE__,
                     sprintf("sending message MessagePublisher::WRITE_METADATAS_TYPE for %s.%s.%s ...", $databoxId, $recordId, $event->getSubdefName())
                 ), FILE_APPEND | LOCK_EX);
 
                 $this->messagePublisher->publishMessage($payload, MessagePublisher::WRITE_METADATAS_TYPE);
 
-                file_put_contents(dirname(__FILE__).'/../../../../../logs/trace.txt', sprintf("%s [%s] : %s (%s); %s\n", (\DateTime::createFromFormat('U.u', microtime(TRUE)))->format('Y-m-d\TH:i:s.u'), getmypid(), __FILE__, __LINE__,
+                file_put_contents(dirname(__FILE__).'/../../../../../logs/trace.txt', sprintf("%s [%s] : %s (%s); %s\n", (date('Y-m-d\TH:i:s')), getmypid(), __FILE__, __LINE__,
                     sprintf("   ... message MessagePublisher::WRITE_METADATAS_TYPE sent for %s.%s.%s", $databoxId, $recordId, $event->getSubdefName())
                 ), FILE_APPEND | LOCK_EX);
             }
             else {
 
-                file_put_contents(dirname(__FILE__).'/../../../../../logs/trace.txt', sprintf("%s [%s] : %s (%s); %s\n", (\DateTime::createFromFormat('U.u', microtime(TRUE)))->format('Y-m-d\TH:i:s.u'), getmypid(), __FILE__, __LINE__,
+                file_put_contents(dirname(__FILE__).'/../../../../../logs/trace.txt', sprintf("%s [%s] : %s (%s); %s\n", (date('Y-m-d\TH:i:s')), getmypid(), __FILE__, __LINE__,
                     sprintf("no MessagePublisher::WRITE_METADATAS_TYPE for %s.%s.%s because(isSubdefMetadataUpdateRequired=%d)", $databoxId, $recordId, $event->getSubdefName(), $this->isSubdefMetadataUpdateRequired($databox, $type, $subdef->get_name()))
                 ), FILE_APPEND | LOCK_EX);
 
