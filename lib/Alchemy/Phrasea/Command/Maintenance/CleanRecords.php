c7ede045af72d5a8bdd8fb0bb1a776851056954a
aynsix <asr@esokia-webagency.com>
2021-12-22 15:51:44 +0300

    PHRAS-3618 init bin/maintenance

---
+++ lib/Alchemy/Phrasea/Command/Maintenance/CleanRecords.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Alchemy\Phrasea\Command\Maintenance;
+
+use Alchemy\Phrasea\Command\Command;
+use Alchemy\Phrasea\Model\Entities\BasketElement;
+use Alchemy\Phrasea\Model\Repositories\BasketElementRepository;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class CleanRecords extends Command
+{
+    public function __construct()
+    {
+        parent::__construct('clean:records');
+
+        $this
+            ->setDescription('Remove orphans items')
+            ->addOption('from_baskets', null, InputOption::VALUE_NONE, 'remove orphans items from baskets')
+            ;
+    }
+
+    protected function doExecute(InputInterface $input, OutputInterface $output)
+    {
+        $fromBaskets = $input->getOption('from_baskets');
+
+        if ($fromBaskets === true) {
+            $output->writeln("<info> Remove orphans records from basket</info>");
+            $this->removeOrphansFromBasket();
+        }
+    }
+
+    private function removeOrphansFromBasket()
+    {
+        /** @var BasketElementRepository $basketElementRepository */
+        $basketElementRepository = $this->container['repo.basket-elements'];
+
+        /** @var BasketElement $basketElement */
+        foreach ($basketElementRepository->findAll() as $basketElement) {
+            try {
+                if ($this->container->findDataboxById($basketElement->getSbasId())->getRecordRepository()->find($basketElement->getRecordId()) == null) {
+                    $this->container['orm.em']->remove($basketElement);
+                }
+            } catch (NotFoundHttpException $e) {
+                // remove also if sbas_id not found
+                $this->container['orm.em']->remove($basketElement);
+            }
+        }
+        $this->container['orm.em']->flush();
+    }
+}
