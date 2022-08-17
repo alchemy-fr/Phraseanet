175c32e5ce59c07e9e8363a1dc9bdf06720dadc5
aynsix <asr@esokia-webagency.com>
2022-08-17 08:13:09 +0300

    test

--- lib/Alchemy/Phrasea/Controller/Admin/DataboxController.php
+++ lib/Alchemy/Phrasea/Controller/Admin/DataboxController.php
@@ -535,25 +535,25 @@ class DataboxController extends Controller
         try {
             $databox = $this->findDataboxById($databox_id);
 
-            // collection is now emptying by worker
-            // what about the databox ??
+            ∕∕ collection is now emptying by worker
+            ∕∕ what about the databox ??
 
-//            foreach ($databox->get_collections() as $collection) {
-//                if ($collection->get_record_amount() <= 500) {
-//                    $collection->empty_collection(500);
-//                } else {
-//                    /** @var TaskManipulator $taskManipulator */
-//                    $taskManipulator = $this->app['manipulator.task'];
-//                    $taskManipulator->createEmptyCollectionJob($collection);
-//                }
-//            }
-//
-//            $msg = $this->app->trans('Base empty successful');
-//            $success = true;
-//
-//            if ($taskCreated) {
-//                $msg = $this->app->trans('A task has been created, please run it to complete empty collection');
-//            }
+            foreach ($databox->get_collections() as $collection) {
+                if ($collection->get_record_amount() <= 500) {
+                    $collection->empty_collection(500);
+                } else {
+                    /** @var TaskManipulator $taskManipulator */
+                    $taskManipulator = $this->app['manipulator.task'];
+                    $taskManipulator->createEmptyCollectionJob($collection);
+                }
+            }
+
+            $msg = $this->app->trans('Base empty successful');
+            $success = true;
+
+            if ($taskCreated) {
+                $msg = $this->app->trans('A task has been created, please run it to complete empty collection');
+            }
         } catch (\Exception $e) {
 
         }
