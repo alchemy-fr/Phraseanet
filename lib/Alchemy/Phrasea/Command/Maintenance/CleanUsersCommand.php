1097f9e5df6c1dd9a1912f8e790cb8a4e3521f3f
aynsix <asr@esokia-webagency.com>
2022-09-26 16:34:16 +0300

    clean users

--- lib/Alchemy/Phrasea/Command/Maintenance/CleanUsersCommand.php
+++ lib/Alchemy/Phrasea/Command/Maintenance/CleanUsersCommand.php
@@ -11,10 +11,15 @@
 
 namespace Alchemy\Phrasea\Command\Maintenance;
 
+use Alchemy\Phrasea\Application\Helper\NotifierAware;
 use Alchemy\Phrasea\Command\Command;
+use Alchemy\Phrasea\Core\LazyLocator;
+use Alchemy\Phrasea\Model\Manipulator\BasketManipulator;
 use Alchemy\Phrasea\Model\Manipulator\UserManipulator;
 use Alchemy\Phrasea\Model\Repositories\BasketRepository;
 use Alchemy\Phrasea\Model\Repositories\UserRepository;
+use Alchemy\Phrasea\Notification\Mail\MailRequestInactifAccount;
+use Alchemy\Phrasea\Notification\Receiver;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -22,6 +27,7 @@ use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 class CleanUsersCommand extends Command
 {
+    use NotifierAware;
 
     const CLEAR_EMAIL = 8;
     const REVOKE = 4;
@@ -41,7 +47,11 @@ class CleanUsersCommand extends Command
         $this
             ->setDescription('Delete "sleepy" users (not connected since a long time)')
 //            ->addOption('action',     null, InputOption::VALUE_REQUIRED|InputOption::VALUE_IS_ARRAY, '"clear_email" ; "revoke" ; "delete_data" ; "delete_all"')
-            ->addOption('older_than', null, InputOption::VALUE_REQUIRED,                             'delete older than \<OLDER_THAN>')
+            ->addOption('mode', null, InputOption::VALUE_REQUIRED,                             'cleanup or purge deleted user')
+            ->addOption('older_than', null, InputOption::VALUE_REQUIRED,                             'cleanup older than \<OLDER_THAN>')
+            ->addOption('usertype',       null, InputOption::VALUE_REQUIRED,                             'specify type of user to clean')
+            ->addOption('send_mail', null, InputOption::VALUE_NONE,                                 'send information email to the user')
+            ->addOption('remove_basket', null, InputOption::VALUE_NONE,                                 'remove basket for user')
             ->addOption('dry',        null, InputOption::VALUE_NONE,                                 'dry run, count but don\'t delete')
             ->addOption('list',       null, InputOption::VALUE_REQUIRED,                             'list only, don\'t delete')
             ->addOption('show_sql',   null, InputOption::VALUE_NONE,                                 'show sql pre-selecting users')
@@ -55,12 +65,19 @@ class CleanUsersCommand extends Command
 //                . "<info>action=delete_all</info> will delete all db elements, including the dbox logs and the user itself\n"
 //                . "default = clear_email ; revoke ; delete_data"
 //                . "\n"
+                . "\<MODE>cleanup user or purge deleted user, e.g:\n"
+                . "- <info>cleanup</info>\n"
+                . "- <info>purge</info>\n"
                 . "\<OLDER_THAN> can be absolute or relative from now, e.g.:\n"
                 . "- <info>2022-01-01</info> (please use strict date format, do not add time)\n"
                 . "- <info>10 days</info>\n"
                 . "- <info>2 weeks</info>\n"
                 . "- <info>6 months</info>\n"
                 . "- <info>1 year</info>\n"
+                . "\<USERTYPE>specify type of user to clean : \n"
+                . "- <info>admin</info> \n"
+                . "- <info>appowner</info \n>"
+                . "- <info>ghost</info> \n"
                 . "\<LIST> sepcifies the user column to be listed, set one of:\n"
                 . "- <info>id</info>\n"
                 . "- <info>login</info>\n"
@@ -74,6 +91,7 @@ class CleanUsersCommand extends Command
         $dry = false;
         $show_sql = false;
         $yes = false;
+        $this->setDelivererLocator(new LazyLocator($this->container, 'notification.deliverer'));
 
         // sanity check options
         //
@@ -105,13 +123,48 @@ class CleanUsersCommand extends Command
         }
         */
 
+        $cnx = $this->container->getApplicationBox()->get_connection();
+
+        $mode = $input->getOption('mode');
+
+        if (!in_array($mode, ['cleanup', 'purge'])) {
+            $output->writeln(sprintf("<error>bad \"mode\" value '%s'</error> (see possible values with --help)", $mode));
+
+            return 1;
+        }
+
         // --older_than
 
         $older_than = str_replace(['-', '/', ' '], '-', $input->getOption('older_than'));
-        if($older_than === "") {
+        if($mode == "cleanup" && $older_than === "" ) {
             $output->writeln("<error>set '--older_than' option.</error>");
             return 1;
         }
+
+        if ($mode == "purge") {
+            $sqlCountDeleted = "SELECT count(*) AS n FROM Users WHERE deleted = 1";
+            $stmt = $cnx->prepare($sqlCountDeleted);
+            $stmt->execute();
+            $nDeleted = $stmt->fetchColumn(0);
+            $stmt->closeCursor();
+
+            $helper = $this->getHelper('question');
+            $question = new ConfirmationQuestion(sprintf("Confirm deletion of %s deleted user(s) [y/n] : ", $nDeleted), false);
+
+            if (!$helper->ask($input, $output, $question)) {
+                return 0;
+            }
+
+            $sqlDelete = "DELETE FROM Users WHERE deleted = 1";
+            $stmt = $cnx->prepare($sqlDelete);
+            $stmt->execute();
+            $stmt->closeCursor();
+
+            $output->writeln("Deleted users removed!");
+
+            return 0;
+        }
+
         $matches = [];
         preg_match("/(\d{4}-\d{2}-\d{2})|(\d+)-(day|week|month|year)s?/i", $older_than, $matches);
         $n = count($matches);
@@ -129,12 +182,31 @@ class CleanUsersCommand extends Command
             $output->writeln("<error>invalid value form '--older_than' option.</error> (see possible values with --help)");
             return 1;
         }
-        $clauses[] = "`admin`=0";                   // dont delete super admins
+
+        $sql_where_u = 1;
+        $sql_where_ub = 1;
+
+        if ($input->getOption('usertype') == 'admin') {
+            $clauses[] = "`admin`=1";
+        } else {
+            $clauses[] = "`admin`=0";  // dont delete super admins
+        }
+
+        if ($input->getOption('usertype') == 'appowner') {
+            $clauses[] = "`ApiAccounts`.`id` IS NOT NULL";
+        } else {
+            $clauses[] = "ISNULL(`ApiAccounts`.`id`)";
+        }
+
+        if ($input->getOption('usertype') == 'ghost') {
+            $sql_where_u = "`u`.`bids` IS NULL";
+            $sql_where_ub = "`ub`.`sbids` IS NULL";
+        }
+
         $clauses[] = "`deleted`=0";                 // dont delete twice
         $clauses[] = "ISNULL(`model_of`)";          // dont delete models
         $clauses[] = "`login`!='autoregister'";     // dont delete "autoregister"
         $clauses[] = "`login`!='guest'";            // dont delete "guest"
-        $clauses[] = "ISNULL(`ApiAccounts`.`id`)";      // dont delete api service accounts
 
         // --dry
 
@@ -161,21 +233,6 @@ class CleanUsersCommand extends Command
         //
         $sql_where = join(") AND (", $clauses);
 
-        $cnx = $this->container->getApplicationBox()->get_connection();
-
-        $sql_count = "SELECT COUNT(`Users`.`id`) AS n FROM (`Users` LEFT JOIN `ApiAccounts` ON `ApiAccounts`.`user_id`=`Users`.`id`) WHERE (" . $sql_where . ")";
-        if($show_sql) {
-            $output->writeln(sprintf("sql: \"<info>%s</info>\"", $sql_count));
-        }
-        $stmt = $cnx->prepare($sql_count);
-        $stmt->execute();
-        $n = $stmt->fetchColumn(0);
-        $stmt->closeCursor();
-        if(!$list) {
-            $output->writeln(sprintf("Acting on %s users.", $n));
-        }
-
-
         /** @var UserManipulator $userManipulator */
         $userManipulator = $this->container['manipulator.user'];
         /** @var UserRepository $userRepository */
@@ -183,32 +240,40 @@ class CleanUsersCommand extends Command
         /** @var BasketRepository $basketRepository */
         $basketRepository = $this->container['repo.baskets'];
 
-        $sql_list = "SELECT u.*, GROUP_CONCAT(`basusr`.`base_id` SEPARATOR ',') AS `bids`\n"
+        $sql_list = "SELECT * FROM \n"
+                . "(SELECT ub.*, GROUP_CONCAT(`basusr`.`base_id` SEPARATOR ',') AS `bids`\n"
                 . "FROM\n"
                 . "( SELECT `Users`.`id` AS `usr_id`, `Users`.`login`, `Users`.`email`, `Users`.`last_connection`, GROUP_CONCAT(`sbasusr`.`sbas_id` SEPARATOR ',') AS `sbids`\n"
                 . "  FROM (`Users` LEFT JOIN `ApiAccounts` ON `ApiAccounts`.`user_id` = `Users`.`id`) \n"
                 . "  LEFT JOIN `sbasusr` ON `sbasusr`.`usr_id` = `Users`.`id`\n"
+                . "  LEFT JOIN Baskets ON Baskets.user_id = `Users`.`id`\n"
+                . "  LEFT JOIN BasketParticipants ON BasketParticipants.user_id = `Users`.`id`\n"
                 . "  WHERE (" . $sql_where . ")"
                 . "  GROUP BY `sbasusr`.`usr_id`\n"
-                . ") AS u\n"
-                . "LEFT JOIN `basusr` ON `basusr`.`usr_id` = `u`.`usr_id` GROUP BY `basusr`.`usr_id`";
+                . ") AS ub\n"
+                . "LEFT JOIN `basusr` ON `basusr`.`usr_id` = `ub`.`usr_id`"
+                . " WHERE " . $sql_where_ub ."\n"
+                . " GROUP BY `basusr`.`usr_id`) AS u\n"
+                . " WHERE ". $sql_where_u ;
 
         if($show_sql) {
             $output->writeln(sprintf("sql: \"<info>%s</info>\"", $sql_list));
         }
 
+        $stmt = $cnx->prepare($sql_list);
+        $stmt->execute();
+        $rows = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
         if(!$yes && !$list) {
             $helper = $this->getHelper('question');
-            $question = new ConfirmationQuestion(sprintf("Confirm deletion of %s user(s) [y/n] : ", $n), false);
+            $question = new ConfirmationQuestion(sprintf("Confirm deletion of %s user(s) [y/n] : ", count($rows)), false);
 
             if (!$helper->ask($input, $output, $question)) {
                 return 0;
             }
         }
 
-        $stmt = $cnx->prepare($sql_list);
-        $stmt->execute();
-        while( $row = ($stmt->fetch(\PDO::FETCH_ASSOC)) ) {
+        foreach ( $rows as $row ) {
             if( !is_null($user = $userRepository->find($row['usr_id'])) ) {
 
                 if ($list) {
@@ -217,6 +282,16 @@ class CleanUsersCommand extends Command
                 }
                 else {
                     $output->write(sprintf("%s : %s / %s (%s)", $row['usr_id'], $row['login'], $row['email'], $row['last_connection']));
+
+                    if ($input->getOption('send_mail')) {
+                        $receiver = Receiver::fromUser($user);
+                        $mail = MailRequestInactifAccount::create($this->container, $receiver);
+
+                        $servername = $this->container['conf']->get('servername');
+                        $mail->setButtonUrl('http://'.$servername.'/login');
+
+                        $this->deliver($mail);
+                    }
 
                     if (!$dry) {
                         $acl = $this->container->getAclForUser($user);
@@ -231,6 +306,11 @@ class CleanUsersCommand extends Command
 
                         // revoke sbas rights
                         $acl->revoke_unused_sbas_rights();
+
+                        if ($input->getOption('remove_basket')) {
+                            $baskets = $basketRepository->findBy(['user' => $user]);
+                            $this->getBasketManipulator()->removeBaskets($baskets);
+                        }
 
                         // delete user
                         $userManipulator->delete($user);
@@ -267,6 +347,14 @@ class CleanUsersCommand extends Command
 */
 
         return 0;
+    }
+
+    /**
+     * @return BasketManipulator
+     */
+    private function getBasketManipulator()
+    {
+        return $this->container['manipulator.basket'];
     }
 
 }
