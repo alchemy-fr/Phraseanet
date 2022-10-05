ef736b3f9de3eb83d9241599f56403ca57403b8b
Moctar <diouf@alchemy.fr>
2022-10-05 12:30:10 +0200

    PHRAS-3318 - Fix phraseanet-saml-sp for kubernets use

--- docker/phraseanet/saml-sp/root/bootstrap/entrypoint.sh
+++ docker/phraseanet/saml-sp/root/bootstrap/entrypoint.sh
@@ -4,8 +4,12 @@ set -e
 
 echo `date +"%Y-%m-%d %H:%M:%S"` " - Starting simplesamlphp sp entrypoint."
 
+if [[ -n $SAML_ALLOW_DEBUG || $SAML_ALLOW_DEBUG == "true" ]]; then
+echo "SAML DEBUG IS ENABLED - TO ADD DEFAULT CONFIG"
 cp -Rf /var/www/simplesamlphp-1.10.0/config-templates/* /var/www/simplesamlphp-1.10.0/config/
 cp -Rf /bootstrap/conf.d/phrasea.*  /var/www/simplesamlphp-1.10.0/cert/
+
+fi
 
 envsubst < "/bootstrap/php.ini.sample" > /usr/local/etc/php/php.ini
 envsubst \$SAML_PHRASEANET_HOST < "/bootstrap/config/config.php" > /var/www/simplesamlphp-1.10.0/config/config.php
