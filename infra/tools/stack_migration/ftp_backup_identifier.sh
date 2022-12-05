6916c180f4022a02bd8e89a8d61d40e27e4a09b7
Nicolas Maillat <maillat@alchemy.fr>
2022-12-05 19:38:29 +0100

    Update ftp_backup_identifier.sh

--- infra/tools/stack_migration/ftp_backup_identifier.sh
+++ infra/tools/stack_migration/ftp_backup_identifier.sh
@@ -1,19 +1,20 @@
 #bin/bash
 
-APP_ROOT="/var/alchemy/Phraseanet"
+# APP_ROOT="/var/alchemy/Phraseanet"
+
 FTP_REPO="ftp"
 BACKUP_REPO="backup"
 
 cd "/var/alchemy/Phraseanet"
 
 if [ ! -z "$STACK_NAME" ]; then
-        touch $FTP_REPO"/ftp_enabled_"$STACK_NAME
-        echo "File created in : "$FTP_REPO"/"$FTP_REPO"/ftp_enabled_"$STACK_NAME
+        touch $FTP_REPO"/ftp_enable_"$STACK_NAME
+        echo "File created in : "$FTP_REPO"/ftp_enable_"$STACK_NAME
 fi
 
 if [ ! -z "$STACK_NAME" ]; then
         touch $BACKUP_REPO"/backup_repo_"$STACK_NAME
-        echo "File created in : "$APP_ROOT"/"$BACKUP_REPO"/backup_repo_"$STACK_NAME
+        echo "File created in : "$BACKUP_REPO"/backup_repo_"$STACK_NAME
 
 fi
 
