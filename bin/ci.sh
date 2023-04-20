4b17e5f24d83260f99b3964b768afdc8b841b14e
aynsix <asr@esokia-webagency.com>
2023-04-20 16:15:46 +0300

    test

---
+++ bin/ci.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+echo "check install"
+runuser app -c PHRASEANET_BASE_URL=http://127.0.0.1 docker/phraseanet/setup/auto-install.sh
+
+echo "init test"
+bin/developer ini:setup-tests-dbs -v
+bin/developer phraseanet:regenerate-sqlite -v
+bin/developer phraseanet:generate-js-fixtures -v
+
+echo "do unit test"
+vendor/phpunit/phpunit/phpunit  --exclude-group legacy
+vendor/phpunit/phpunit/phpunit  --group legacy --exclude-group web
+vendor/phpunit/phpunit/phpunit --group web
