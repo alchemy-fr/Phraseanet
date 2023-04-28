09d435028945d5f0e6c2d8369c754f082eb397f7
aynsix <asr@esokia-webagency.com>
2023-04-28 09:57:37 +0300

    test

---
+++ docker/phraseanet/setup/init-test-install.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -e
+
+curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
+
+php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
+
+composer install --ignore-platform-reqs --no-interaction
+
+
+
+if [ -z "$PHRASEANET_ADMIN_ACCOUNT_EMAIL" ]; then
+    echo "PHRASEANET_ADMIN_ACCOUNT_EMAIL, Phraseanet admin account  var is not set."
+    exit 1
+fi
+
+if [ -z "$PHRASEANET_ADMIN_ACCOUNT_PASSWORD " ]; then
+    echo "$PHRASEANET_ADMIN_ACCOUNT_PASSWORD,  Phaseanet admin password var is not set."
+    exit 1
+fi
+
+FILE=config/configuration.yml
+
+while [[ ! -e "$FILE" ]]
+do
+sleep 10
+/var/alchemy/Phraseanet/bin/setup system:install \
+    --email=$PHRASEANET_ADMIN_ACCOUNT_EMAIL \
+    --password=$PHRASEANET_ADMIN_ACCOUNT_PASSWORD \
+    --db-host=$PHRASEANET_DB_HOST \
+    --db-port=$PHRASEANET_DB_PORT \
+    --db-user=$PHRASEANET_DB_USER \
+    --db-password=$PHRASEANET_DB_PASSWORD \
+    --db-template=$INSTALL_DB_TEMPLATE \
+    --appbox=$INSTALL_APPBOX \
+    --databox=$INSTALL_DATABOX \
+    --server-name=$PHRASEANET_BASE_URL \
+    --download-path=$PHRASEANET_DOWNLOAD_DIR \
+    --lazaret-path=$PHRASEANET_LAZARET_DIR \
+    --caption-path=$PHRASEANET_CAPTION_DIR \
+    --worker-tmp-files=$PHRASEANET_WORKER_TMP \
+    --data-path=/var/alchemy/Phraseanet/datas -y
+done
+
+/var/alchemy/Phraseanet/bin/setup system:config set workers.queue.worker-queue.registry alchemy_worker.queue_registry
+
+/var/alchemy/Phraseanet/bin/console searchengine:index -dc --force
+
+/var/alchemy/Phraseanet/bin/developer ini:setup-tests-dbs -v
+
+/var/alchemy/Phraseanet/bin/developer phraseanet:regenerate-sqlite -v
+
+/var/alchemy/Phraseanet/bin/developer phraseanet:generate-js-fixtures -v
