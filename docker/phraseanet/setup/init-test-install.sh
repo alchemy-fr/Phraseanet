75cc3584fb4fcd44c92fcd54fe4a9b3cb4242713
aynsix <asr@esokia-webagency.com>
2023-07-04 09:57:57 +0300

    fix test install

--- docker/phraseanet/setup/init-test-install.sh
+++ docker/phraseanet/setup/init-test-install.sh
@@ -8,33 +8,21 @@ php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
 composer install --ignore-platform-reqs --no-interaction
 
-
-
-if [ -z "$PHRASEANET_ADMIN_ACCOUNT_EMAIL" ]; then
-    echo "PHRASEANET_ADMIN_ACCOUNT_EMAIL, Phraseanet admin account  var is not set."
-    exit 1
-fi
-
-if [ -z "$PHRASEANET_ADMIN_ACCOUNT_PASSWORD " ]; then
-    echo "$PHRASEANET_ADMIN_ACCOUNT_PASSWORD,  Phaseanet admin password var is not set."
-    exit 1
-fi
-
 FILE=config/configuration.yml
 
 while [[ ! -e "$FILE" ]]
 do
 sleep 10
 /var/alchemy/Phraseanet/bin/setup system:install \
-    --email=$PHRASEANET_ADMIN_ACCOUNT_EMAIL \
-    --password=$PHRASEANET_ADMIN_ACCOUNT_PASSWORD \
-    --db-host=$PHRASEANET_DB_HOST \
-    --db-port=$PHRASEANET_DB_PORT \
-    --db-user=$PHRASEANET_DB_USER \
-    --db-password=$PHRASEANET_DB_PASSWORD \
-    --db-template=$INSTALL_DB_TEMPLATE \
-    --appbox=$INSTALL_APPBOX \
-    --databox=$INSTALL_DATABOX \
+    --email=test@test.fr \
+    --password=test \
+    --db-host=db \
+    --db-port=3306 \
+    --db-user=root \
+    --db-password=root \
+    --db-template=DublinCore \
+    --appbox=ab_master \
+    --databox=db_databox1 \
     --server-name=$PHRASEANET_BASE_URL \
     --download-path=$PHRASEANET_DOWNLOAD_DIR \
     --lazaret-path=$PHRASEANET_LAZARET_DIR \
