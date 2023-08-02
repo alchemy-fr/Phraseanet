74757caa3b14aa2a9e1e02bd688c77ad47543730
jygaulier <gaulier@alchemy.fr>
2023-08-02 15:39:38 +0200

    PHRAS-3870_record-action-example (#4334)
    * select by id or name ; add / enhance clauses (date, number, ...) ; change example to match default db ; add doc
    
    * fix "in code" example
    
    * big refacto ; bc break on syntax ; add "compute" ; allow b/c work:run-serv without payload
    
    * exception if not settings/version=2
    
    ---------
    
    Co-authored-by: Nicolas Maillat <maillat@alchemy.fr>

--- lib/Alchemy/Phrasea/WorkerManager/Command/WorkerRunServiceCommand.php
+++ lib/Alchemy/Phrasea/WorkerManager/Command/WorkerRunServiceCommand.php
@@ -4,6 +4,7 @@ namespace Alchemy\Phrasea\WorkerManager\Command;
 
 use Alchemy\Phrasea\Command\Command;
 use Alchemy\Phrasea\WorkerManager\Worker\Resolver\WorkerResolverInterface;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -15,8 +16,8 @@ class WorkerRunServiceCommand extends Command
         parent::__construct('worker:run-service');
 
         $this->setDescription('Execute a service')
-            ->addArgument('type')
-            ->addArgument('body')
+            ->addArgument('type', InputArgument::REQUIRED)
+            ->addArgument('body', InputArgument::OPTIONAL)
             ->addOption('preserve-payload', 'p', InputOption::VALUE_NONE, 'Preserve temporary payload file');
 
         return $this;
@@ -28,20 +29,25 @@ class WorkerRunServiceCommand extends Command
         $workerResolver = $this->container['alchemy_worker.type_based_worker_resolver'];
 
         $type = $input->getArgument('type');
-        $body = file_get_contents($input->getArgument('body'));
+        $body = $input->getArgument('body');
 
-        if ($body === false) {
-            $output->writeln('Unable to read payload file');
 
-            return;
-        }
+        $body = [];
+        if($input->getArgument('body')) {
+            $body = @file_get_contents($input->getArgument('body'));
 
-        $body = json_decode($body, true);
+            if ($body === false) {
+                $output->writeln(sprintf('<error>Unable to read payload file %s</error>', $input->getArgument('body')));
 
-        if (json_last_error() !== JSON_ERROR_NONE) {
-            $output->writeln('<error>Invalid message body</error>');
+                return;
+            }
 
-            return;
+            $body = json_decode($body, true);
+            if (json_last_error() !== JSON_ERROR_NONE) {
+                $output->writeln('<error>Invalid message body</error>');
+
+                return;
+            }
         }
 
         $worker = $workerResolver->getWorker($type, $body);
@@ -49,7 +55,7 @@ class WorkerRunServiceCommand extends Command
         $worker->process($body);
 
         if (! $input->getOption('preserve-payload')) {
-            unlink($input->getArgument('body'));
+            @unlink($input->getArgument('body'));
         }
 
     }
--- lib/Alchemy/Phrasea/WorkerManager/Controller/AdminConfigurationController.php
+++ lib/Alchemy/Phrasea/WorkerManager/Controller/AdminConfigurationController.php
@@ -6,6 +6,7 @@ use Alchemy\Phrasea\Application as PhraseaApplication;
 use Alchemy\Phrasea\Controller\Controller;
 use Alchemy\Phrasea\Model\Entities\WorkerRunningJob;
 use Alchemy\Phrasea\Model\Repositories\WorkerRunningJobRepository;
+use Alchemy\Phrasea\Plugin\Exception\JsonValidationException;
 use Alchemy\Phrasea\SearchEngine\Elastic\ElasticsearchOptions;
 use Alchemy\Phrasea\Twig\PhraseanetExtension;
 use Alchemy\Phrasea\WorkerManager\Event\PopulateIndexEvent;
@@ -17,8 +18,9 @@ use Alchemy\Phrasea\WorkerManager\Form\WorkerSearchengineType;
 use Alchemy\Phrasea\WorkerManager\Form\WorkerValidationReminderType;
 use Alchemy\Phrasea\WorkerManager\Queue\AMQPConnection;
 use Alchemy\Phrasea\WorkerManager\Queue\MessagePublisher;
-use Alchemy\Phrasea\WorkerManager\Worker\RecordsActionsWorker;
+use Alchemy\Phrasea\WorkerManager\Worker\RecordsActionsWorker\RecordsActionsWorker;
 use Doctrine\ORM\OptimisticLockException;
+use Exception;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Form\Form;
 use Symfony\Component\Form\FormInterface;
@@ -117,7 +119,7 @@ class AdminConfigurationController extends Controller
         if ($timeFilter != null) {
             try {
                 $dateTimeFilter = (new \DateTime())->sub(new \DateInterval($timeFilter));
-            } catch (\Exception $e) {
+            } catch (Exception $e) {
             }
         }
 
@@ -509,28 +511,32 @@ class AdminConfigurationController extends Controller
 
     public function recordsActionsFacilityAction(PhraseaApplication $app, Request $request)
     {
-        $ret = ['tasks' => []];
-        $job = new RecordsActionsWorker($app);
-        switch ($request->get('ACT')) {
-            case 'PLAYTEST':
-                $sxml = simplexml_load_string($request->get('xml'));
-                if (isset($sxml->tasks->task)) {
-                    foreach ($sxml->tasks->task as $sxtask) {
-                        $ret['tasks'][] = $job->calcSQL($app, $sxtask, true);
+        $ret = [
+            'error' => null,
+            'tasks' => []
+        ];
+        try {
+            $job = new RecordsActionsWorker($app);
+            switch ($request->get('ACT')) {
+                case 'PLAYTEST':
+                case 'CALCTEST':
+                case 'CALCSQL':
+                    $sxml = simplexml_load_string($request->get('xml'));
+                    if ((string)$sxml['version'] !== '2') {
+                        throw new JsonValidationException(sprintf("bad settings version (%s), should be \"2\"", (string)$sxml['version']));
                     }
-                }
-                break;
-            case 'CALCTEST':
-            case 'CALCSQL':
-                $sxml = simplexml_load_string($request->get('xml'));
-                if (isset($sxml->tasks->task)) {
-                    foreach ($sxml->tasks->task as $sxtask) {
-                        $ret['tasks'][] = $job->calcSQL($app, $sxtask, false);
+                    if (isset($sxml->tasks->task)) {
+                        foreach ($sxml->tasks->task as $sxtask) {
+                            $ret['tasks'][] = $job->calcSQL($sxtask, $request->get('ACT') === 'PLAYTEST');
+                        }
                     }
-                }
-                break;
-            default:
-                throw new NotFoundHttpException('Route not found.');
+                    break;
+                default:
+                    throw new NotFoundHttpException('Route not found.');
+            }
+        }
+        catch (Exception $e) {
+            $ret['error'] = $e->getMessage();
         }
 
         return $app->json($ret);
@@ -557,73 +563,134 @@ class AdminConfigurationController extends Controller
     -->
     <tasks>
 
-        <comment> keep offline (sb4 = 1) all docs before their "go online" date and after credate (record column) </comment>
+        <!-- SANITY CHECK ON DOCUMENT SIZE workflow
 
-        <task active="0" name="stay offline" action="update" databoxId="1">
-            <from>
-                <date direction="before" field="GO_ONLINE"/>
-                <date direction="after" field="#credate" />
-            </from>
-            <to>
-                <status mask="x1xxxx"/>
-            </to>
+        - trash records with documents > 10Mo
+        -->
+
+        <!-- trash jpeg files > 10Mo -->
+        <task active="0" name="reject too big files " action="update" databoxId="db_databox1">
+            <if>
+                <number field="#filesize" compare=">" value="10485760" />
+            </if>
+            <then>
+                <coll id="_TRASH_" />
+            </then>
         </task>
 
 
-        <comment> Put online (sb4 = 0) all docs from 'public' collection and between the online date and the date of archiving </comment>
 
-        <task active="0" name="go online" action="update" databoxId="1">
-            <from>
-                <comment> 5, 6, 7 are "public" collections </comment>
-                <coll compare="=" id="5,6,7"/>
-                <date direction="after" field="GO_ONLINE"/>
-                <date direction="before" field="TO_ARCHIVE"/>
-            </from>
-            <to>
-                <status mask="x0xxxx"/>
-            </to>
+        <!-- EXPIRATION workflow
+
+        from "test" collection :
+        - records having Source = "internal" will expire after 1 month
+        - other records will expire after 10 days
+        - records go to "Public" collection
+        - we want a "last days" status-bit to be set 2 days before expiration
+        - Expired documents from "Public" collection will go to trash
+        -->
+
+
+        <!-- set the ExpireDate to (create + 1 month) for source="internal, go public -->
+        <task active="0" name="compute expiring date for internal" action="update" databoxId="db_databox1">
+            <if>
+                <coll compare="=" id="test" />
+                <text field="Source" compare="=" value="internal" />
+                <is_unset field="ExpireDate" />
+            </if>
+            <then>
+                <compute_date direction="after" field="#credate" delta="+1 month" computed="exp" />
+                <set_field field="ExpireDate" value="$exp" />
+                <coll id="Public" />
+            </then>
+        </task>
+
+        <!-- set the ExpireDate to (create + 10 days) for others -->
+        <task active="0" name="compute expiring date for others" action="update" databoxId="db_databox1">
+            <if>
+                <coll compare="=" id="test" />
+                <text field="Source" compare="!=" value="internal" />
+                <is_unset field="ExpireDate" />
+            </if>
+            <then>
+                <compute_date direction="after" field="#credate" delta="+10 days" computed="exp" />
+                <set_field field="ExpireDate" value="$exp" />
+                <coll id="Public" />
+            </then>
+        </task>
+
+        <!-- if set the "last days" sb 2 days before expiration-->
+        <task active="0" name="will expire in 2 days" action="update" databoxId="db_databox1">
+            <if>
+                <date direction="after" field="ExpireDate" delta="-2 days" />
+            </if>
+            <then>
+                <status mask="1xxxxxx"/>
+            </then>
+        </task>
+
+        <!-- if Public, move to trash after expiration -->
+        <task active="0"  name="expire" action="update" databoxId="db_databox1">
+            <if>
+                <coll compare="=" id="Public" />
+                <date direction="after" field="ExpireDate" />
+            </if>
+            <then>
+                <coll id="_TRASH_" />
+            </then>
         </task>
 
 
-        <comment> Warn 10 days before archiving (raise sb5) </comment>
 
-        <task active="0" name="almost the end" action="update" databoxId="1">
-            <from>
-                <coll compare="=" id="5,6,7"/>
-                <date direction="after" field="TO_ARCHIVE" delta="-10"/>
-            </from>
-            <to>
-                <status mask="1xxxxx"/>
-            </to>
+        <!-- EMPTY TRASH workflow -->
+
+        <task active="0" name="clean trash" action="delete" databoxId="db_databox1">
+            <!-- Delete the records that are in the trash collection, unmodified from 3 months -->
+            <if>
+                <coll compare="=" id="_TRASH_"/>
+                <date direction="after" field="#moddate" delta="+3 months" />
+            </if>
         </task>
 
 
-        <comment> Move to 'archive' collection </comment>
 
-        <task active="0" name="archivage" action="update" databoxId="1">
-            <from>
-                <coll compare="=" id="5,6,7"/>
-                <date direction="after" field="TO_ARCHIVE" />
-            </from>
-            <to>
-                <comment> reset status of archived documents </comment>
-                <status mask="00xxxx"/>
-                <comment> 666 is the "archive" collection </comment>
-                <coll id="666" />
-            </to>
+        <!-- SANITY CHECK ON FIELDS workflow
+
+        we want a status to show if "Title" and "Description" are filled
+        -->
+
+        <!-- set sb "caption filled" (sb4=1) when both title and Description are set -->
+        <task active="0" name="Title and Description set" action="update" databoxId="db_databox1">
+            <if>
+                <is_set field="Title"/>
+                <is_set field="Description"/>
+            </if>
+            <then>
+                <status mask="1xxxx"/>
+            </then>
         </task>
 
-
-        <comment> Delete the documents that are in the trash collection unmodified from 3 months </comment>
-
-        <task active="0" name="trash" action="delete" databoxId="1">
-            <from>
-                <coll compare="=" id="666"/>
-                <date direction="after" field="#moddate" delta="+90" />
-            </from>
+        <!-- reset sb "caption filled" (sb4=0) when title is not set -->
+        <task active="0" name="Title not set" action="update" databoxId="db_databox1">
+            <if>
+                <is_unset field="Title"/>
+            </if>
+            <then>
+                <status mask="0xxxx"/>
+            </then>
         </task>
+
+        <!-- reset sb "caption filled" (sb4=0) when caption is not set -->
+        <task active="0" name="Description not set" action="update" databoxId="db_databox1">
+            <if>
+                <is_unset field="Description"/>
+            </if>
+            <then>
+                <status mask="0xxxx"/>
+            </then>
+        </task>
+
     </tasks>
-
 </tasksettings>
 EOF;
     }
