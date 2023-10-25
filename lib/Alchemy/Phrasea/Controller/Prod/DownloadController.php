fd7d09c88f2cc0306fb119bb73ea6731a56ea43d
jygaulier <gaulier@alchemy.fr>
2023-10-25 18:26:00 +0200

    conf.export_stamp_choice now supports 'manage_collection' and 'manage_databox' (and bc true) to restrict right to remove stamp.

--- lib/Alchemy/Phrasea/Controller/Prod/DownloadController.php
+++ lib/Alchemy/Phrasea/Controller/Prod/DownloadController.php
@@ -15,6 +15,7 @@ use Alchemy\Phrasea\Core\Configuration\PropertyAccess;
 use Alchemy\Phrasea\Core\Event\DownloadAsyncEvent;
 use Alchemy\Phrasea\Core\Event\ExportEvent;
 use Alchemy\Phrasea\Core\PhraseaEvents;
+use Alchemy\Phrasea\Filesystem\PhraseanetFilesystem;
 use Alchemy\Phrasea\Model\Manipulator\TokenManipulator;
 use set_export;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -50,11 +51,13 @@ class DownloadController extends Controller
 
         $list = $download->prepare_export(
             $this->getAuthenticatedUser(),
-            $this->app['filesystem'],
+            $this->getFilesystem(),
             $subdefs,
             $request->request->get('type') === 'title' ? true : false,
             $request->request->get('businessfields'),
-            $request->request->get('stamp_choice') === "NO_STAMP" ? \set_export::NO_STAMP : \set_export::STAMP_SYNC
+            \set_export::STAMP_SYNC,
+            $request->request->get('stamp_choice') === "REMOVE_STAMP",
+            false
         );
 
         $list['export_name'] = sprintf('%s.zip', $download->getExportName());
@@ -211,5 +214,12 @@ class DownloadController extends Controller
     protected function getSession()
     {
         return $this->app['session'];
+    }
+    /**
+     * @return PhraseanetFilesystem
+     */
+    private function getFilesystem()
+    {
+        return $this->app['filesystem'];
     }
 }
