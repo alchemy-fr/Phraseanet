791528255b3327375a504e54698ed50a29475f59
jygaulier <gaulier@alchemy.fr>
2024-06-20 12:56:09 +0200

    wip do not merge

---
+++ lib/Alchemy/Phrasea/Core/Event/Subscriber/ExpiringRightsSubscriber.php
@@ -0,0 +1,223 @@
+<?php
+
+namespace Alchemy\Phrasea\Core\Event\Subscriber;
+
+use Alchemy\Phrasea\Application;
+use Alchemy\Phrasea\Application\Helper\DataboxLoggerAware;
+use Alchemy\Phrasea\Core\Event\ExportEvent;
+use Alchemy\Phrasea\Core\Event\RecordEdit;
+use Alchemy\Phrasea\Core\PhraseaEvents;
+use appbox;
+use databox;
+use Doctrine\DBAL\DBALException;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class ExpiringRightsSubscriber implements EventSubscriberInterface
+{
+    use DataboxLoggerAware;
+
+    /** @var  Application */
+    private $app;
+
+    /**
+     * @var callable
+     */
+    private $appboxLocator;
+
+    public function __construct(Application $app, callable $appboxLocator)
+    {
+        $this->app                 = $app;
+        $this->appboxLocator       = $appboxLocator;
+    }
+
+    private function getJobsByDb($sbas_id)
+    {
+        static $jobsByDb = [];
+        static $jobsByName = null;
+
+        if($jobsByName === null) {
+            $jobsByName = [];
+            foreach ($this->app['conf']->get(['workers', 'expiring-rights', 'jobs'], []) as $jobname => &$job) {
+                // nb: we must include inactive jobs so every download is recorded
+                $job['_c'] = $job['collection'];
+                unset($job['collection']);
+                $jobsByName[$jobname] = $job;
+            }
+        }
+
+        $sbas_id = (int)$sbas_id;
+        if(!array_key_exists($sbas_id, $jobsByDb)) {
+            $jobsByDb[$sbas_id] = [];
+            /** @var appbox $abox */
+            $abox = $this->app['phraseanet.appbox'];
+            $dbox = $abox->get_databox($sbas_id);
+            foreach($jobsByName as $jobname => &$job) {
+                if($job['databox'] === $sbas_id || $job['databox'] === $dbox->get_dbname()) {
+                    // patch the collections filter to have id's and names
+                    if(!array_key_exists('collection', $job)) {
+                        $job['collection'] = [];
+                        foreach ($dbox->get_collections() as $coll) {
+                            if(empty($job['_c']) ||
+                                in_array($coll->get_coll_id(), $job['_c'], true) ||
+                                in_array($coll->get_name(), $job['_c'], true))
+                            {
+                                $job['collection'][] = $coll->get_coll_id();
+                                $job['collection'][] = $coll->get_name();
+                            }
+                        }
+                    }
+                    $jobsByDb[$sbas_id][$jobname] = &$jobsByName[$jobname];
+                }
+            }
+        }
+
+        return array_key_exists($sbas_id, $jobsByDb) ? $jobsByDb[$sbas_id] : [];
+    }
+
+    /**
+     * when someone downloads, if the base has a declared "expire_field"
+     * insert an entry in our table
+     *
+     * @param ExportEvent $event
+     * @param string $eventName
+     */
+    public function onExportCreate(ExportEvent $event, $eventName)
+    {
+        $user_id = $event->getUser()->getId();
+        $email = $event->getUser()->getEmail();
+        /** @var appbox $abox */
+        $abox = $this->getApplicationBox();
+        $stmt = null;
+
+        foreach(explode(';', $event->getList()) as $sbid_rid) {
+            list($sbas_id, $record_id) = explode('_', $sbid_rid);
+
+            try {
+                /** @var databox $dbox */
+                $dbox = $abox->get_databox($sbas_id);
+            }
+            catch (\Exception $e) {
+                continue;
+            }
+
+            foreach($this->getJobsByDb($sbas_id) as $jobname => $job) {
+                if($job['target'] !== "downloaders") {
+                    continue;
+                }
+
+                $record = $dbox->get_record($record_id);
+
+                // get the expire_field unique value
+                $expire_value = null;
+                $expire_field = $job['expire_field'];
+                $fields = $record->getCaption([$expire_field]);
+                if (array_key_exists($expire_field, $fields) && count($fields[$expire_field]) === 1) {
+                    try {
+                        $expire_value = (new \DateTime($fields[$expire_field][0]))->format('Y-m-d');   // drop hour, minutes...
+                    }
+                    catch (\Exception $e) {
+                        // bad date format ? set null
+                    }
+                }
+
+                try {
+                    // insert
+                    if(!$stmt) {
+                        // first sql
+                        $sql = "INSERT INTO `ExpiringRights` (job, downloaded, user_id, email, sbas_id, base, collection, record_id, title, expire)\n"
+                            . " VALUES (:job, NOW(), :user_id, :email, :sbas_id, :base, :collection, :record_id, :title, :expire)";
+                        $stmt = $abox->get_connection()->prepare($sql);
+                    }
+                    try {
+                        $stmt->execute([
+                            ':job'        => $jobname,
+                            ':user_id'    => $user_id,
+                            ':email'      => $email,
+                            ':sbas_id'    => $sbas_id,
+                            ':base'       => $dbox->get_viewname(),   // fallback of get_label() because we may not know the current language
+                            ':collection' => $record->getCollection()->get_name(),
+                            ':record_id'  => $record_id,
+                            ':title'      => $record->get_title(),
+                            ':expire'     => $expire_value
+                        ]);
+                    }
+                    catch (\Exception $e) {
+                        // duplicate (same job+user+sbas_id+record_id) ?
+                    }
+                }
+                catch (\Exception $e) {
+                    // no-op
+                }
+
+            }
+        }
+        if($stmt) {
+            $stmt->closeCursor();
+        }
+    }
+
+    /**
+     * when the "expire_field" is edited, update the new value for every downloaded record
+     *
+     * @param RecordEdit $event
+     * @param string $eventName
+     * @throws DBALException
+     */
+    public function onRecordEdit(RecordEdit $event, $eventName)
+    {
+        $record = $event->getRecord();
+        $sbas_id = $record->getDataboxId();
+
+        // get settings for this databox
+        foreach($this->getJobsByDb($sbas_id) as $job) {
+            if($job['target'] !== "downloaders") {
+                continue;
+            }
+            $expire_field = $job['expire_field'];
+            $expire_value = null;
+            $record_id = $record->getRecordId();
+            $fields = $record->getCaption([$expire_field]);
+            if (array_key_exists($expire_field, $fields) && count($fields[$expire_field]) > 0) {
+                $expire_value = $fields[$expire_field][0];
+            }
+
+            /** @var appbox $abox */
+            $abox = $this->getApplicationBox();
+            $sql = "UPDATE `ExpiringRights` SET new_expire = :new_expire\n"
+                . " WHERE sbas_id = :sbas_id AND record_id = :record_id AND (IFNULL(expire, 0) != IFNULL(:expire, 0) OR !ISNULL(new_expire))";
+            $stmt = $abox->get_connection()->prepare($sql);
+            $stmt->execute($p = [
+                ':new_expire' => $expire_value,
+                ':sbas_id'    => $sbas_id,
+                ':record_id'  => $record_id,
+                ':expire'     => $expire_value
+            ]);
+            $stmt->closeCursor();
+        }
+    }
+
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            /** @uses onRecordEdit */
+            PhraseaEvents::RECORD_EDIT => 'onRecordEdit',
+            /** @uses onExportCreate */
+            PhraseaEvents::EXPORT_CREATE   => 'onExportCreate',
+        ];
+    }
+
+
+    /**
+     * @return appbox
+     */
+    private function getApplicationBox()
+    {
+        static $appbox = null;
+        if($appbox === null) {
+            $callable = $this->appboxLocator;
+            $appbox = $callable();
+        }
+        return $appbox;
+    }
+}
---
+++ lib/classes/patch/419RC1PHRAS4078.php
@@ -0,0 +1,268 @@
+<?php
+
+use Alchemy\Phrasea\Application;
+use Alchemy\Phrasea\Core\Configuration\PropertyAccess;
+use Doctrine\DBAL\Exception\TableExistsException;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Yaml\Yaml;
+
+class patch_419RC1PHRAS4078 implements patchInterface
+{
+    /** @var string */
+    private $release = '4.1.9-rc1';
+
+    /** @var array */
+    private $concern = [base::APPLICATION_BOX];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get_release()
+    {
+        return $this->release;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDoctrineMigrations()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function require_all_upgrades()
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function concern()
+    {
+        return $this->concern;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply(base $base, Application $app)
+    {
+        $this->app = $app;
+        $this->logger = $app['logger'];
+
+        if ($base->get_base_type() === base::DATA_BOX) {
+            $this->patch_databox($base, $app);
+        } elseif ($base->get_base_type() === base::APPLICATION_BOX) {
+            $this->patch_appbox($base, $app);
+        }
+
+        return true;
+    }
+
+    private function patch_databox(databox $databox, Application $app)
+    {
+    }
+
+    private function patch_appbox(base $appbox, Application $app)
+    {
+        $this->migrateConfig();
+        $this->migrateTable($appbox);
+    }
+
+    const TABLENAME = "_ExpiringRights";
+    const TABLENAME3 = "ExpiringRights";
+    const CONFIG_DIR = "/config/plugins/expirating-rights-plugin/";
+    const CONFIG_FILE = "configuration.yml";
+    /** @var Application $app */
+    private $app;
+    /** @var LoggerInterface $logger */
+    private $logger;
+    private $config;
+
+    private function migrateConfig()
+    {
+        /** @var PropertyAccess $conf */
+        $phrconfig = $this->app['conf'];
+
+        if ($phrconfig->has(['expiring-rights'])) {
+            return;
+        }
+
+        // locate the config for the ExpiringRights plugin (v1 or v3)
+        $config_dir = $this->app['root.path'] . self::CONFIG_DIR;
+        $config_file = $config_dir . self::CONFIG_FILE;
+        $piconfig = ['jobs' => []];
+        if (file_exists($config_file)) {
+            try {
+                $piconfig = Yaml::parse(file_get_contents($config_file));
+            }
+            catch (\Exception $e) {
+                $piconfig = ['jobs' => []];
+            }
+
+            if (array_key_exists('databoxes', $piconfig)) {
+                // migrate the job settings to v3
+                $jobs3 = [];
+                foreach ($piconfig['jobs'] as $job) {
+                    $jobname = $job['job'];
+
+                    // find databox
+                    $found = 0;
+                    foreach ($piconfig['databoxes'] as $db) {
+                        if ($db['databox'] === $job['databox']) {
+                            unset($job['job']);
+                            $jobs3[$jobname] = $job;
+                            $jobs3[$jobname]['target'] = "downloader";
+                            $jobs3[$jobname]['collection'] = array_key_exists('collection', $db) ? $db['collection'] : [];
+                            $jobs3[$jobname]['downloaded'] = array_key_exists('downloaded', $db) ? $db['downloaded'] : [];
+                            $jobs3[$jobname]['expire_field'] = $db['expire_field'];
+                            $found++;
+                        }
+                    }
+
+                    if ($found != 1) {
+                        $msg = sprintf("error migrating job \"%s\": databox not found or not unique", $jobname);
+                        $this->logger->error(sprintf("<error>%s</error>", $msg));
+                    }
+                }
+
+                $piconfig = [
+                    'version' => 3,
+                    'jobs'    => $jobs3
+                ];
+            }
+
+            rename($config_file, $config_file . "_bkp");
+        }
+
+        $phrconfig->set(['plugins', 'expirating-rights-plugin', 'enabled'], false);
+        $phrconfig->set(['expiring-rights'], $piconfig);
+    }
+
+
+    private function migrateTable(appbox $appbox)
+    {
+        // create the table
+        $sql = "CREATE TABLE `ExpiringRights` (\n"
+            . "`id` int(11) unsigned NOT NULL AUTO_INCREMENT,\n"
+            . "`job` char(128) DEFAULT NULL,\n"
+            . "`downloaded` datetime DEFAULT NULL,\n"
+            . "`user_id` int(11) unsigned DEFAULT NULL,\n"
+            . "`email` char(128) DEFAULT NULL,\n"
+            . "`sbas_id` int(11) unsigned DEFAULT NULL,\n"
+            . "`base` char(50) DEFAULT NULL,\n"
+            . "`collection` char(50) DEFAULT NULL,\n"
+            . "`record_id` int(11) unsigned DEFAULT NULL,\n"
+            . "`title` char(200) DEFAULT NULL,\n"
+            . "`expire` datetime DEFAULT NULL,\n"
+            . "`new_expire` datetime DEFAULT NULL,\n"
+            . "`alerted` datetime DEFAULT NULL,\n"
+            . "PRIMARY KEY (`id`),\n"
+            . "KEY `job` (`job`),\n"
+            . "KEY `sbas_id` (`sbas_id`),\n"
+            . "KEY `expire` (`expire`),\n"
+            . "KEY `new_expire` (`new_expire`),\n"
+            . "KEY `alerted` (`alerted`),\n"
+            . "UNIQUE KEY `unique` (job,user_id,sbas_id,record_id)"
+            . ") ENGINE=InnoDB CHARSET=utf8;";
+
+        $stmt = $appbox->get_connection()->prepare($sql);
+        try {
+            $stmt->execute();
+        }
+        catch(TableExistsException $e) {
+            // table v3 already exists, skip migration
+            return;
+        }
+        catch(\Exception $e) {
+            // fatal
+            return;
+        }
+        $stmt->closeCursor();
+
+        // migrate data from v-1 ?
+        try {
+            $appbox->get_connection()->exec("SELECT `id` FROM `_ExpiringRights` LIMIT 1");
+        }
+        catch (\Exception $e) {
+            // failed : no table to copy ?
+            return;
+        }
+
+        /** @var PropertyAccess $conf */
+        $phrconfig = $this->app['conf'];
+
+        if (!$phrconfig->has(['expiring-rights'])) {
+            return;
+        }
+
+        // table v3 was just created, insert from table v-1
+        $sql = "INSERT INTO `ExpiringRights` (\n"
+            . " `job`, \n"
+            . " `alerted`,\n"
+            . " `base`,\n"
+            . " `collection`,\n"
+            . " `downloaded`,\n"
+            . " `email`,\n"
+            . " `expire`,\n"
+            . " `new_expire`,\n"
+            . " `record_id`,\n"
+            . " `sbas_id`,\n"
+            . " `title`,\n"
+            . " `user_id`)\n"
+            . "SELECT\n"
+            . " :job AS `job`,\n"
+            . " MAX(`alerted`),\n"
+            . " `base`,\n"
+            . " `collection`,\n"
+            . " MAX(`downloaded`),\n"
+            . " `email`,\n"
+            . " MAX(`expire`),\n"
+            . " MAX(`new_expire`),\n"
+            . " `record_id`,\n"
+            . " `sbas_id`,\n"
+            . " `title`,\n"
+            . " `user_id`\n"
+            . "FROM `_ExpiringRights` GROUP BY `user_id`, `sbas_id`, `record_id` ORDER BY `id` ASC";
+        $stmtCopy = $appbox->get_connection()->prepare($sql);
+
+        $config = $phrconfig->get(['expiring-rights']);
+        foreach ($config['jobs'] as $jobname => $job) {
+
+            // copy v-1 rows to v3 (add job)
+            $stmtCopy->execute([':job' => $jobname]);
+
+            // fix alerted too early
+            $delta = $job['prior_notice'];
+            if ($delta > 0) {
+                $value = "(`expire`+INTERVAL " . $delta . " DAY)";
+            } elseif ($delta < 0) {
+                $value = "(`expire`-INTERVAL " . -$delta . " DAY)";
+            } else {
+                $value = "`expire`";
+            }
+            $sql = "UPDATE `ExpiringRights` SET `alerted`=NULL WHERE `job` = :job AND `alerted` < " . $value;
+            $stmtEarly = $appbox->get_connection()->prepare($sql);
+            $stmtEarly->execute([':job' => $jobname]);
+            $stmtEarly->closeCursor();
+        }
+        $stmtCopy->closeCursor();
+
+        // fix bad date
+        $sql = "UPDATE `ExpiringRights` SET `expire`=NULL WHERE `expire`='0000-00-00'";
+        $stmt = $appbox->get_connection()->prepare($sql);
+        $stmt->execute([]);
+        $stmt->closeCursor();
+
+        // fix useless new_expire
+        $sql = "UPDATE `ExpiringRights` SET `new_expire`=NULL WHERE `new_expire` = `expire`";
+        $stmt = $appbox->get_connection()->prepare($sql);
+        $stmt->execute([]);
+        $stmt->closeCursor();
+    }
+
+}
