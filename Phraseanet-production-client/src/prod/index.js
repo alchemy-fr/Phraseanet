2d21d2f2ecd623a05eb9e35632d2c5fe8f9fdf1f
Gaspard Jacobson <52928254+gjacobjn@users.noreply.github.com>
2025-10-31 21:28:43 +0100

    Comment out unused jQuery UI datepicker imports
    Comment out jQuery UI datepicker language imports.

--- Phraseanet-production-client/src/prod/index.js
+++ Phraseanet-production-client/src/prod/index.js
@@ -2,12 +2,12 @@ import $ from 'jquery';
 require('./style/main.scss');
 import * as utils from './../components/utils/utils.js';
 import bootstrap from './bootstrap.js';
-require('./../../node_modules/jquery-ui-datepicker-with-i18n/ui/i18n/jquery.ui.datepicker-ar.js');
-require('./../../node_modules/jquery-ui-datepicker-with-i18n/ui/i18n/jquery.ui.datepicker-de.js');
-require('./../../node_modules/jquery-ui-datepicker-with-i18n/ui/i18n/jquery.ui.datepicker-es.js');
-require('./../../node_modules/jquery-ui-datepicker-with-i18n/ui/i18n/jquery.ui.datepicker-fr.js');
-require('./../../node_modules/jquery-ui-datepicker-with-i18n/ui/i18n/jquery.ui.datepicker-nl.js');
-require('./../../node_modules/jquery-ui-datepicker-with-i18n/ui/i18n/jquery.ui.datepicker-en-GB.js');
+//require('./../../node_modules/jquery-ui-datepicker-with-i18n/ui/i18n/jquery.ui.datepicker-ar.js');
+//require('./../../node_modules/jquery-ui-datepicker-with-i18n/ui/i18n/jquery.ui.datepicker-de.js');
+//require('./../../node_modules/jquery-ui-datepicker-with-i18n/ui/i18n/jquery.ui.datepicker-es.js');
+//require('./../../node_modules/jquery-ui-datepicker-with-i18n/ui/i18n/jquery.ui.datepicker-fr.js');
+//require('./../../node_modules/jquery-ui-datepicker-with-i18n/ui/i18n/jquery.ui.datepicker-nl.js');
+//require('./../../node_modules/jquery-ui-datepicker-with-i18n/ui/i18n/jquery.ui.datepicker-en-GB.js');
 
 $.widget.bridge('uitooltip', $.fn.tooltip);
 //window.btn = $.fn.button.noConflict(); // reverts $.fn.button to jqueryui btn
